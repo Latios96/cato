6bf8733f69caea5cfa55f6715cb0ceae9b0d106d
Jan <jan.honsbrok@gmail.com>
2020-11-13 20:49:49 +0100

    generate end message

--- cato/__main__.py
+++ cato/__main__.py
@@ -1,14 +1,21 @@
 import pinject
 
 from cato.config.config_file_parser import JsonConfigParser
+from cato.reporter.end_message_generator import EndMessageGenerator
 from cato.runners.test_suite_runner import TestSuiteRunner
 
 if __name__ == "__main__":
     config_parser = JsonConfigParser()
-    path = r"M:\test\cato-test\testconfig.json"
+    #path = r"M:\test\cato-test\testconfig.json"
+    path = r"M:\workspace\cato\testconfig.json"
     config = config_parser.parse(path)
 
     obj_graph = pinject.new_object_graph()
     test_suite_runner = obj_graph.provide(TestSuiteRunner)
 
-    test_suite_runner.run_test_suites(config)
+    result = test_suite_runner.run_test_suites(config)
+
+    generator = EndMessageGenerator()
+    print()
+    print(generator.generate_end_message(result))
+
---
+++ tests/test_end_message_generator.py
@@ -0,0 +1,23 @@
+import emoji
+
+from cato.domain.test import Test
+from cato.domain.test_execution_result import TestExecutionResult
+from cato.domain.test_result import TestStatus
+from cato.domain.test_suite import TestSuite
+from cato.domain.test_suite_execution_result import TestSuiteExecutionResult
+from cato.reporter.end_message_generator import EndMessageGenerator
+
+
+def test_generate_end_message():
+    test = Test(name="my first test", command="dummy_command")
+    test_suite = TestSuite(name="example", tests=[test])
+    execution_result = TestExecutionResult(test, TestStatus.SUCCESS, [], 1)
+    result = [TestSuiteExecutionResult(test_suite, TestStatus.SUCCESS, [execution_result])]
+    generator = EndMessageGenerator()
+
+    message = generator.generate_end_message(result)
+
+    assert message == emoji.emojize("""Result:
+Ran {} tests
+{}  succeded :white_check_mark:
+{}  failed   :x:""".format(1, 1, 0), use_aliases = True)
