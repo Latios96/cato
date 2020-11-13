bf56bafaecb5a6bf5bef70f2667030f4d63a8038
Jan <jan.honsbrok@gmail.com>
2020-11-13 17:50:22 +0100

    replace variables

--- tests/test_TestRunner.py
+++ tests/test_TestRunner.py
@@ -1,3 +1,5 @@
+import os
+
 from cato.domain.config import Config
 from cato.domain.test import Test
 from cato.domain.test_suite import TestSuite
@@ -19,3 +21,16 @@ def test_should_report_test_start():
 
     reporter.report_start_test.assert_called_with(test)
     command_runner.run.assert_called_with(test.command)
+
+def test_should_replace_placeholder():
+    reporter = mock_safe(Reporter)
+    command_runner = mock_safe(CommandRunner)
+    test_runner = TestRunner(command_runner, reporter)
+    test = Test(name="my first test", command="crayg -s {test_resources}/test.json -o {image_output_png}")
+
+    test_runner.run_test(
+        Config(path="test", test_suites=[]), TestSuite(name="suite", tests=[]), test
+    )
+
+    reporter.report_start_test.assert_called_with(test)
+    command_runner.run.assert_called_with("crayg -s {}/test.json -o my first test.png".format(os.path.join('test', 'suite', 'my first test')))
