2c626a9a32bdd1c0072c1f0e8cb59565409c4621
Jan <jan.honsbrok@gmail.com>
2020-11-14 13:07:42 +0100

    add variable processor

---
+++ cato/runners/variable_processor.py
@@ -0,0 +1,35 @@
+from typing import Dict
+
+from cato.vendor import lucidity
+
+
+class VariableProcessor:
+    def evaluate_variables(
+        self, config, current_suite, test, variables: Dict[str, str]
+    ) -> Dict[str, str]:
+        default_variables = {
+            "test_name": test.name,
+            "suite_name": current_suite.name,
+            "config_path": config.path,
+            "test_resources": "{@config_path}/{@suite_name}/{@test_name}",
+            "image_output_folder": "{@test_resources}/{@suite_name}/{@test_name}",
+            "image_output_no_extension": "{@image_output_folder}/{@test_name}",
+            "image_output_png": "{@image_output_no_extension}.png",
+        }
+        default_variables.update(variables)
+
+        templates = {}
+        for name, str in default_variables.items():
+            template = lucidity.Template(name, str)
+            template.template_resolver = templates
+            templates[name] = template
+
+        formatted = {}
+
+        for name, template in templates.items():
+            formatted[name] = template.format({})
+
+        return formatted
+
+    def format_command(self, command: str, variables: Dict[str, str]):
+        return command.format(**variables)
---
+++ tests/test_variable_processor.py
@@ -0,0 +1,63 @@
+import os
+
+from cato.domain.config import Config
+from cato.domain.test import Test
+from cato.domain.test_suite import TestSuite
+from cato.runners.variable_processor import VariableProcessor
+
+
+def test_evaluate_variables_no_custom_vars():
+    config = Config(path="config_path", test_suites=[], output_folder="test")
+    suite = TestSuite(name="my_test_suite", tests=[])
+    test = Test("test_name", "test_command")
+    variable_processor = VariableProcessor()
+
+    variables = variable_processor.evaluate_variables(config, suite, test, {})
+
+    assert variables == {
+        "test_name": test.name,
+        "suite_name": suite.name,
+        "config_path": config.path,
+        "test_resources": "config_path/my_test_suite/test_name",
+        "image_output_folder": "config_path/my_test_suite/test_name/my_test_suite/test_name",
+        "image_output_no_extension": "config_path/my_test_suite/test_name/my_test_suite/test_name/test_name",
+        "image_output_png": "config_path/my_test_suite/test_name/my_test_suite/test_name/test_name.png",
+    }
+
+
+def test_evaluate_variables_custom_image_output():
+    config = Config(path="config_path", test_suites=[], output_folder="test")
+    suite = TestSuite(name="my_test_suite", tests=[])
+    test = Test("test_name", "test_command")
+    variable_processor = VariableProcessor()
+
+    variables = variable_processor.evaluate_variables(
+        config,
+        suite,
+        test,
+        {
+            "frame": "7",
+            "image_output_png": "{@image_output_folder}/test_name{@frame}.png",
+        },
+    )
+
+    assert variables == {
+        "frame": "7",
+        "test_name": test.name,
+        "suite_name": suite.name,
+        "config_path": config.path,
+        "test_resources": "config_path/my_test_suite/test_name",
+        "image_output_folder": "config_path/my_test_suite/test_name/my_test_suite/test_name",
+        "image_output_no_extension": "config_path/my_test_suite/test_name/my_test_suite/test_name/test_name",
+        "image_output_png": "config_path/my_test_suite/test_name/my_test_suite/test_name/test_name7.png",
+    }
+
+
+def test_format_command():
+    variable_processor = VariableProcessor()
+
+    command = variable_processor.format_command(
+        "{image_output_png}", {"image_output_png": "test"}
+    )
+
+    assert command == "test"
