fab18d20aa613f478b4c24f308e6e40d8b192855
Jan <jan.honsbrok@gmail.com>
2020-11-18 14:45:41 +0100

    add tests for stats_calculator

---
+++ cato/reporter/stats_calculator.py
@@ -0,0 +1,28 @@
+import itertools
+from dataclasses import dataclass
+from typing import List
+
+from cato.domain.test_result import TestStatus
+from cato.domain.test_suite_execution_result import TestSuiteExecutionResult
+
+
+@dataclass
+class Stats:
+    num_tests: int
+    succeded_tests: int
+    failed_tests: int
+
+
+class StatsCalculator:
+    def calculate(self, result: List[TestSuiteExecutionResult]) -> Stats:
+        all_tests = list(itertools.chain(*map(lambda x: x.test_results, result)))
+
+        total_tests = len(all_tests)
+
+        total_tests_succeded = len(
+            list(filter(lambda x: x.result == TestStatus.SUCCESS, all_tests))
+        )
+        total_tests_failed = len(
+            list(filter(lambda x: x.result == TestStatus.FAILED, all_tests))
+        )
+        return Stats(total_tests, total_tests_succeded, total_tests_failed)
---
+++ tests/test_stats_calculator.py
@@ -0,0 +1,41 @@
+from cato.domain.test import Test
+from cato.domain.test_execution_result import TestExecutionResult
+from cato.domain.test_result import TestStatus
+from cato.domain.test_suite import TestSuite
+from cato.domain.test_suite_execution_result import TestSuiteExecutionResult
+from cato.reporter.stats_calculator import StatsCalculator, Stats
+
+
+def test_calculates_succeded_correctly():
+    test = Test(name="my first test", command="dummy_command", variables={})
+    test_suite = TestSuite(name="example", tests=[test])
+    execution_result = TestExecutionResult(
+        test, TestStatus.SUCCESS, [], 1, "this is a message", ""
+    )
+    result = [
+        TestSuiteExecutionResult(test_suite, TestStatus.SUCCESS, [execution_result])
+    ]
+    stats_calculator = StatsCalculator()
+
+    stats = stats_calculator.calculate(result)
+
+    assert stats == Stats(num_tests=1, succeded_tests=1, failed_tests=0)
+
+
+def test_calculates_failed_correctly():
+    test = Test(name="my first test", command="dummy_command", variables={})
+    test_suite = TestSuite(name="example", tests=[test])
+    execution_result1 = TestExecutionResult(
+        test, TestStatus.SUCCESS, [], 1, "this is a message", ""
+    )
+    execution_result2 = TestExecutionResult(
+        test, TestStatus.FAILED, [], 1, "this is a message", ""
+    )
+    result = [
+        TestSuiteExecutionResult(test_suite, TestStatus.SUCCESS, [execution_result1, execution_result2])
+    ]
+    stats_calculator = StatsCalculator()
+
+    stats = stats_calculator.calculate(result)
+
+    assert stats == Stats(num_tests=2, succeded_tests=1, failed_tests=1)
