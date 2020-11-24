2b6edae4731f7df8b80e62dbb5e7d5079f83d8a7
Jan <jan.honsbrok@gmail.com>
2020-11-24 15:14:58 +0100

    validate test name

--- cato/domain/test.py
+++ cato/domain/test.py
@@ -1,12 +1,22 @@
-from dataclasses import dataclass
 from typing import Dict
 
+import attr
 
-@dataclass
+
+@attr.s
 class Test:
-    name: str
-    command: str
-    variables: Dict[str, str]
+    name: str = attr.ib()
+    command: str = attr.ib()
+    variables: Dict[str, str] = attr.ib()
 
     def to_dict(self):
         return {"name": self.name, "command": self.command}
+
+    @name.validator
+    def check(self, attribute, value):
+        if not value:
+            raise ValueError("Test name can not be empty!")
+
+        for c in value:
+            if c in [' ', '/',",", ".", "\\", "\"", "'"]:
+                raise ValueError(f"Test name {value} contains not allowed character: {c}")
---
+++ tests/test_test.py
@@ -0,0 +1,14 @@
+import pytest
+
+from cato.domain.test import Test
+
+
+def test_create_with_empty_name_should_fail():
+    with pytest.raises(ValueError):
+        Test(name="", command="", variables={})
+
+
+@pytest.mark.parametrize("name", [" ", "", "/", ",", "\"", "'", "\\"])
+def test_create_with_unallowed_chars_should_fail(name):
+    with pytest.raises(ValueError):
+        Test(name=name, command="", variables={})
