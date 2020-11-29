003ec87a11d3a196fcfd7aea95c781ce8c9721fe
Jan <jan.honsbrok@gmail.com>
2020-11-29 20:14:30 +0100

    list

--- frontend/src/components/projectrunview/ProjectRunsView.tsx
+++ frontend/src/components/projectrunview/ProjectRunsView.tsx
@@ -1,16 +1,57 @@
 import React, { Component } from "react";
+import Run from "../../models/Run";
+import ago from "s-ago";
+import { ListGroup } from "react-bootstrap";
 interface Props {
   projectId: number;
 }
-interface State {}
+interface State {
+  runs: Run[];
+}
 class ProjectRunsView extends Component<Props, State> {
   constructor(props: Props) {
     super(props);
-    this.state = {};
+    this.state = { runs: [] };
   }
+  componentDidMount() {
+    this.fetchProjects();
+  }
+
   render() {
-    return <div>{this.props.projectId}</div>;
+    return (
+      <div>
+        <ListGroup>
+          {this.state.runs.map((r: Run) => {
+            return (
+              <div>
+                <ListGroup.Item>
+                  <p>Run {r.id}</p>
+                  <p>{this.formatTime(r.started_at)}</p>
+                </ListGroup.Item>
+              </div>
+            );
+          })}
+        </ListGroup>
+      </div>
+    );
   }
+  fetchProjects = () => {
+    fetch("/api/v1/runs/project/" + this.props.projectId)
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          console.log(result);
+          this.setState({ runs: result });
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  };
+  formatTime = (datestr: string) => {
+    var date = new Date(datestr);
+    return ago(date);
+  };
 }
 
 export default ProjectRunsView;
---
+++ frontend/src/models/Run.ts
@@ -0,0 +1,5 @@
+export default interface Run {
+  id: number;
+  project_id: number;
+  started_at: string;
+}
