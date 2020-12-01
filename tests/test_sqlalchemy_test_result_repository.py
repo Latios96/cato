aaececab0a555995f7f28a3285cdc7358feb25d5
Jan <jan.honsbrok@gmail.com>
2020-12-01 16:36:39 +0100

    add tests for find by suite result

--- tests/test_sqlalchemy_test_result_repository.py
+++ tests/test_sqlalchemy_test_result_repository.py
@@ -106,3 +106,40 @@ def test_find_find_by_suite_result_and_test_identifier_not_found(sessionmaker_fi
     assert not repository.find_by_suite_result_and_test_identifier(
         3, TestIdentifier("my_suite", "my_test")
     )
+
+
+def test_find_by_suite_result(sessionmaker_fixture, suite_result):
+    repository = SqlAlchemyTestResultRepository(sessionmaker_fixture)
+    start_time = datetime.datetime.now()
+    end_time = datetime.datetime.now()
+    test_result = TestResult(
+        id=0,
+        suite_result_id=suite_result.id,
+        test_name="my_test_name",
+        test_identifier=TestIdentifier(suite_name="my_suite", test_name="my_test"),
+        test_command="my_command",
+        test_variables={"testkey": "test_value"},
+        machine_info=MachineInfo(cpu_name="cpu", cores=56, memory=8),
+        execution_status=ExecutionStatus.NOT_STARTED,
+        status=TestStatus.SUCCESS,
+        output=["1", "2", "3"],
+        seconds=5,
+        message="sucess",
+        image_output=1,
+        reference_image=3,
+        started_at=start_time,
+        finished_at=end_time,
+    )
+    test_result_save = repository.save(test_result)
+
+    results = repository.find_by_suite_result(suite_result.id)
+
+    assert results == [test_result_save]
+
+
+def test_find_by_suite_result_not_found(sessionmaker_fixture, suite_result):
+    repository = SqlAlchemyTestResultRepository(sessionmaker_fixture)
+
+    results = repository.find_by_suite_result(suite_result.id)
+
+    assert results == []
