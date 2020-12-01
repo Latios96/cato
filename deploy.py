95d798755560f9002a70940e1bf8878f74ca6a10
Jan <jan.honsbrok@gmail.com>
2020-12-01 13:25:04 +0100

    add deploy script

---
+++ deploy.py
@@ -0,0 +1,52 @@
+import os
+import shutil
+import subprocess
+
+import git
+
+
+def check_tag():
+    repo = git.Repo(os.path.dirname(__file__))
+    for tag in repo.tags:
+        if tag.commit == repo.head.commit:
+            return tag
+    return None
+
+
+def main():
+    tag = check_tag()
+    if not tag:
+        print("Current commit is not tagged! Please run bumpversion!")
+    version = tag.name[1:]
+    print(f"Deploy version {version}..")
+
+    print("Building..")
+    subprocess.check_call("gradlew.bat build")
+
+    pip_path = r"M:\cato\venv\Scripts\pip"
+
+    print("Install OpenImageIO..")
+    subprocess.check_call(
+        [
+            pip_path,
+            "install",
+            "-r",
+            os.path.join(os.path.dirname(__file__), "oiio-requirements.txt"),
+        ]
+    )
+
+    print("Install..")
+    wheel_path = os.path.join(
+        os.path.dirname(__file__), "dist", f"cato-{version}-py3-none-any.whl"
+    )
+    subprocess.check_call([pip_path, "install", "--upgrade", wheel_path])
+
+    print("Copy config..")
+    prod_config = r"M:\cato\config.ini"
+    prod_config_target = r"M:\cato\venv\Lib\site-packages\cato\storage\sqlalchemy\config.ini"
+    shutil.copy(prod_config, prod_config_target)
+
+    print("Done.")
+
+if __name__ == "__main__":
+    main()
