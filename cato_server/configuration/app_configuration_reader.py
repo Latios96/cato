5f2a7ee401c00cd92b90951f4562aaafe9089cc9
Jan <jan.honsbrok@gmail.com>
2020-12-02 12:57:13 +0100

    add app configuration reader

---
+++ cato_server/configuration/app_configuration_reader.py
@@ -0,0 +1,33 @@
+import os
+
+import configparser
+from typing import IO
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from cato_server.configuration.app_configuration import AppConfiguration
+from cato_server.configuration.storage_configuration import StorageConfiguration
+
+
+class AppConfigurationReader:
+    def read_file(self, path: str) -> AppConfiguration:
+        if not os.path.exists(path):
+            raise ValueError(f"Supplied config path {path} does not exists!")
+        config = configparser.ConfigParser()
+        config.read(path)
+
+        storage_configuration = self._read_storage_configuration(config)
+
+        return AppConfiguration(
+            port=config.getint("app", "port"),
+            storage_configuration=storage_configuration,
+        )
+
+    def _read_storage_configuration(
+        self, config: configparser.ConfigParser
+    ) -> StorageConfiguration:
+        return StorageConfiguration(
+            database_url=config.get("storage", "database_url"),
+            file_storage_url=config.get("storage", "file_storage_url"),
+        )
---
+++ tests/test_app_configuration_reader.py
@@ -0,0 +1,71 @@
+import configparser
+import os
+
+import pytest
+
+from cato_server.configuration.app_configuration import AppConfiguration
+from cato_server.configuration.app_configuration_reader import AppConfigurationReader
+from cato_server.configuration.storage_configuration import StorageConfiguration
+
+VALID_INI_FILE = """[app]
+port=5000
+[storage]
+database_url=my_database_url
+file_storage_url=my_file_storage_url"""
+
+MISSING_PORT = """[app]
+[storage]
+database_url=my_database_url
+file_storage_url=my_file_storage_url"""
+
+MISSING_DATABASE_URL = """[app]
+port=5000
+[storage]
+file_storage_url=my_file_storage_url"""
+
+MISSING_FILE_STORAGE_URL = """[app]
+port=5000
+[storage]
+database_url=my_database_url"""
+
+
+@pytest.fixture
+def ini_file_creator(tmp_path):
+    def create_ini_file(content):
+        ini_path = os.path.join(str(tmp_path), "config.ini")
+        with open(ini_path, "w") as f:
+            f.write(content)
+        return ini_path
+
+    return create_ini_file
+
+
+def test_read_not_existing_ini_file():
+    reader = AppConfigurationReader()
+    with pytest.raises(ValueError):
+        config = reader.read_file("ini_path")
+
+
+def test_read_valid_file(ini_file_creator):
+    ini_path = ini_file_creator(VALID_INI_FILE)
+    reader = AppConfigurationReader()
+
+    config = reader.read_file(ini_path)
+
+    assert config == AppConfiguration(
+        port=5000,
+        storage_configuration=StorageConfiguration(
+            database_url="my_database_url", file_storage_url="my_file_storage_url"
+        ),
+    )
+
+
+@pytest.mark.parametrize(
+    "invalid_config", [MISSING_PORT, MISSING_DATABASE_URL, MISSING_FILE_STORAGE_URL]
+)
+def test_read_missing_port(invalid_config, ini_file_creator):
+    ini_path = ini_file_creator(invalid_config)
+    reader = AppConfigurationReader()
+
+    with pytest.raises(configparser.NoOptionError):
+        reader.read_file(ini_path)
