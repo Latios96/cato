e3dff79ff5139a211d205a9798b5541ee71bb41f
Jan <jan.honsbrok@gmail.com>
2020-12-02 10:44:07 +0100

    extract constant

--- tests/test_sqlalchemy_deduplicating_storage.py
+++ tests/test_sqlalchemy_deduplicating_storage.py
@@ -3,9 +3,10 @@ import os
 from cato.storage.sqlalchemy.sqlalchemy_deduplicating_file_storage import (
     SqlAlchemyDeduplicatingFileStorage,
 )
-from cato.storage.sqlalchemy.sqlalchemy_simple_file_storage import (
-    SqlAlchemySimpleFileStorage,
-)
+
+TEST_IMAGE_BLACK_PNG = "test_image_black.png"
+
+TEST_IMAGE_WHITE_PNG = "test_image_white.png"
 
 
 def test_save_same_file_should_store_one_file_on_disk(sessionmaker_fixture, tmp_path):
@@ -14,11 +15,11 @@ def test_save_same_file_should_store_one_file_on_disk(sessionmaker_fixture, tmp_
     )
 
     f1 = file_storage.save_file(
-        os.path.join(os.path.dirname(__file__), "test_image_white.png")
+        os.path.join(os.path.dirname(__file__), TEST_IMAGE_WHITE_PNG)
     )
 
     f2 = file_storage.save_file(
-        os.path.join(os.path.dirname(__file__), "test_image_white.png")
+        os.path.join(os.path.dirname(__file__), TEST_IMAGE_WHITE_PNG)
     )
     assert f1.name
     assert f1.id != f2.id
@@ -35,11 +36,11 @@ def test_save_different_files_should_store_two_files_on_disk(
     )
 
     f1 = file_storage.save_file(
-        os.path.join(os.path.dirname(__file__), "test_image_white.png")
+        os.path.join(os.path.dirname(__file__), TEST_IMAGE_WHITE_PNG)
     )
 
     f2 = file_storage.save_file(
-        os.path.join(os.path.dirname(__file__), "test_image_black.png")
+        os.path.join(os.path.dirname(__file__), TEST_IMAGE_BLACK_PNG)
     )
     assert f1.id != f2.id
     assert f1.hash != f2.hash
