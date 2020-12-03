be6ceb2bbc8e281cb0a041923e00f0c29164a508
Jan <jan.honsbrok@gmail.com>
2020-12-03 20:42:50 +0100

    add method to create suite_result in api

--- cato_server/api/suite_results_blueprint.py
+++ cato_server/api/suite_results_blueprint.py
@@ -1,21 +1,68 @@
 import logging
+from http.client import BAD_REQUEST
 
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+from marshmallow import Schema, fields, ValidationError
+from marshmallow.validate import Length, Regexp
 
+from cato.storage.abstract.run_repository import RunRepository
 from cato.storage.abstract.suite_result_repository import SuiteResultRepository
+from cato.storage.domain.suite_result import SuiteResult
 
 logger = logging.getLogger(__name__)
 
 
+def run_id_exists(self, id):
+    return self._run_repository.find_by_id(id) is not None
+
+
 class SuiteResultsBlueprint(Blueprint):
-    def __init__(self, suite_result_repository: SuiteResultRepository):
+    def __init__(
+        self,
+        suite_result_repository: SuiteResultRepository,
+        run_repository: RunRepository,
+    ):
         super(SuiteResultsBlueprint, self).__init__("suite-results", __name__)
         self._suite_result_repository = suite_result_repository
+        self._run_repository = run_repository
 
         self.route("/suite_results/run/<run_id>", methods=["GET"])(
             self.suite_result_by_run
         )
+        self.route("/suite_results", methods=["POST"])(self.create_suite_result)
 
     def suite_result_by_run(self, run_id):
         suite_results = self._suite_result_repository.find_by_run_id(run_id)
         return jsonify(suite_results)
+
+    def create_suite_result(self):
+        class CreateSuiteResultSchema(Schema):
+            run_id = fields.Integer(required=True, validate=self._run_id_exists)
+            suite_name = fields.Str(
+                required=True, validate=[Length(min=1), Regexp(r"^[A-Za-z0-9_\-]+$")]
+            )
+            suite_variables = fields.Dict(required=True, validate=self._is_str_str_dict)
+
+        schema = CreateSuiteResultSchema()
+        request_json = request.get_json()
+        errors = schema.validate(request_json)
+        if errors:
+            return jsonify(errors), BAD_REQUEST
+        suite_result = SuiteResult(
+            id=0,
+            run_id=request_json["run_id"],
+            suite_name=request_json["suite_name"],
+            suite_variables=request_json["suite_variables"],
+        )
+        suite_result = self._suite_result_repository.save(suite_result)
+        logger.info("Created SuiteResult %s", suite_result)
+        return jsonify(suite_result), 201
+
+    def _run_id_exists(self, id):
+        if self._run_repository.find_by_id(id) is None:
+            raise ValidationError(f"No run with id {id} exists.")
+
+    def _is_str_str_dict(self, the_dict):
+        for key, value in the_dict.items():
+            if not isinstance(key, str) or not isinstance(value, str):
+                raise ValidationError(f"Not a mapping of str->str: {key}={value}")
--- tests/test_suite_results_blueprint.py
+++ tests/test_suite_results_blueprint.py
@@ -1,3 +1,10 @@
+import json
+
+import pytest
+
+from cato.storage.domain.suite_result import SuiteResult
+
+
 def test_get_run_by_project_id_should_return(client, suite_result, run):
     url = "/api/v1/suite_results/run/{}".format(run.id)
 
@@ -21,3 +28,66 @@ def test_get_run_by_project_id_should_return_empty_list(client):
 
     assert rv.status_code == 200
     assert rv.get_json() == []
+
+
+@pytest.mark.parametrize(
+    "data,errors",
+    [
+        (
+            {},
+            {
+                "run_id": ["Missing data for required field."],
+                "suite_name": ["Missing data for required field."],
+                "suite_variables": ["Missing data for required field."],
+            },
+        ),
+        (
+            {"run_id": 2},
+            {
+                "run_id": ["No run with id 2 exists."],
+                "suite_name": ["Missing data for required field."],
+                "suite_variables": ["Missing data for required field."],
+            },
+        ),
+        (
+            {"run_id": 1, "suite_name": "yrsdt*$%$$"},
+            {
+                "suite_name": ["String does not match expected pattern."],
+                "suite_variables": ["Missing data for required field."],
+            },
+        ),
+        (
+            {
+                "run_id": 1,
+                "suite_name": "suite_name",
+                "suite_variables": {"Test": 6546565},
+            },
+            {
+                "suite_variables": ["Not a mapping of str->str: Test=6546565"],
+            },
+        ),
+    ],
+)
+def test_create_suite_result_invalid_data_should_not_create(data, errors, client, run):
+    url = "/api/v1/suite_results"
+
+    rv = client.post(url, json=data)
+
+    assert rv.status_code == 400
+    assert rv.get_json() == errors
+
+
+def test_create_suite_result_should_create(client, run):
+    url = "/api/v1/suite_results"
+
+    data = dict(run_id=run.id, suite_name="my_suite", suite_variables={"key": "value"})
+
+    rv = client.post(url, json=data)
+
+    assert rv.status_code == 201
+    assert rv.get_json() == {
+        "id": 1,
+        "run_id": 1,
+        "suite_name": "my_suite",
+        "suite_variables": {"key": "value"},
+    }
