bfe315d60183704cd38b7d3ae6c04e3441fc98cc
Jan <jan.honsbrok@gmail.com>
2020-12-05 11:08:23 +0100

    remove tests which are covered by validator tests

--- tests/test_projects_blueprint.py
+++ tests/test_projects_blueprint.py
@@ -30,68 +30,18 @@ def test_get_project_should_return_none(client, project):
     assert rv.status_code == 404
 
 
-@pytest.mark.parametrize(
-    "project_name",
-    [
-        "my_project_name",
-        "my-project_name",
-        "my-project_name222",
-        "22",
-        "My-Project-Name",
-    ],
-)
-def test_create_project(client, project_name):
-    rv = client.post(API_V_PROJECTS, json={"name": project_name})
+def test_create_project_success(client):
+    rv = client.post(API_V_PROJECTS, json={"name": "test"})
 
-    assert rv.get_json() == {"id": 1, "name": project_name}
+    assert rv.get_json() == {"id": 1, "name": "test"}
     assert rv.status_code == 201
 
 
-def test_create_project_same_name_should_fail(client):
-    client.post(API_V_PROJECTS, json={"name": "test"})
-    rv = client.post(API_V_PROJECTS, json={"name": "test"})
+def test_create_project_fail(client):
+    rv = client.post(API_V_PROJECTS, json={"name": "$test"})
 
-    assert rv.get_json() == {"name": ['Project with name "test" already exists!']}
+    assert rv.get_json() == {"name": ["String does not match expected pattern."]}
     assert rv.status_code == 400
-
-
-def test_create_project_no_name(client):
-    rv = client.post(API_V_PROJECTS, json={"xfcgvy": "my_project_name"})
-
-    assert rv.status_code == 400
-    assert rv.get_json() == {
-        "name": ["Missing data for required field."],
-        "xfcgvy": ["Unknown field."],
-    }
-
-
-@pytest.mark.parametrize(
-    "invalid_project_name,error_messages",
-    [
-        ("my invalid name", ["String does not match expected pattern."]),
-        ("myinvalid%name", ["String does not match expected pattern."]),
-        ("myinvalid$name", ["String does not match expected pattern."]),
-        ("myinvalid§*+*name", ["String does not match expected pattern."]),
-        ("myinvalid/name", ["String does not match expected pattern."]),
-        ("myinvalid\\name", ["String does not match expected pattern."]),
-        ("my invalid&name", ["String does not match expected pattern."]),
-        ("my invalid name", ["String does not match expected pattern."]),
-        (
-            "",
-            [
-                "Shorter than minimum length 1.",
-                "String does not match expected pattern.",
-            ],
-        ),
-    ],
-)
-def test_create_project_invalid_name(client, invalid_project_name, error_messages):
-    rv = client.post(API_V_PROJECTS, json={"name": invalid_project_name})
-
-    assert rv.status_code == 400
-    assert rv.get_json() == {
-        "name": error_messages,
-    }
 
 
 def test_get_project_by_name_should_get(client, project):
