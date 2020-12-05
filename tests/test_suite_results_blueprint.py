de3efa12814ad338158a644a9ec415fceaa95dd1
Jan <jan.honsbrok@gmail.com>
2020-12-05 11:51:52 +0100

    remove duplicated tests

--- tests/test_suite_results_blueprint.py
+++ tests/test_suite_results_blueprint.py
@@ -30,67 +30,6 @@ def test_get_run_by_project_id_should_return_empty_list(client):
     assert rv.get_json() == []
 
 
-@pytest.mark.parametrize(
-    "data,errors",
-    [
-        (
-            {},
-            {
-                "run_id": ["Missing data for required field."],
-                "suite_name": ["Missing data for required field."],
-                "suite_variables": ["Missing data for required field."],
-            },
-        ),
-        (
-            {"run_id": 2},
-            {
-                "run_id": ["No run with id 2 exists."],
-                "suite_name": ["Missing data for required field."],
-                "suite_variables": ["Missing data for required field."],
-            },
-        ),
-        (
-            {"run_id": 1, "suite_name": "yrsdt*$%$$"},
-            {
-                "suite_name": ["String does not match expected pattern."],
-                "suite_variables": ["Missing data for required field."],
-            },
-        ),
-        (
-            {
-                "run_id": 1,
-                "suite_name": "suite_name",
-                "suite_variables": {"Test": 6546565},
-            },
-            {
-                "suite_variables": ["Not a mapping of str->str: Test=6546565"],
-            },
-        ),
-    ],
-)
-def test_create_suite_result_invalid_data_should_not_create(data, errors, client, run):
-    url = "/api/v1/suite_results"
-
-    rv = client.post(url, json=data)
-
-    assert rv.status_code == 400
-    assert rv.get_json() == errors
-
-
-def test_create_suite_result_with_same_name_should_error(client, run):
-    url = "/api/v1/suite_results"
-
-    data = dict(run_id=run.id, suite_name="my_suite", suite_variables={"key": "value"})
-
-    rv = client.post(url, json=data)
-    rv = client.post(url, json=data)
-
-    assert rv.status_code == 400
-    assert rv.get_json() == [
-        {"run_id": "A test suite with name my_suite already exists for run id 1"}
-    ]
-
-
 def test_create_suite_result_should_create(client, run):
     url = "/api/v1/suite_results"
 
@@ -105,3 +44,14 @@ def test_create_suite_result_should_create(client, run):
         "suite_name": "my_suite",
         "suite_variables": {"key": "value"},
     }
+
+
+def test_create_suite_result_should_not_create(client, run):
+    url = "/api/v1/suite_results"
+
+    data = dict(run_id=42, suite_name="my_suite", suite_variables={"key": "value"})
+
+    rv = client.post(url, json=data)
+
+    assert rv.status_code == 400
+    assert rv.get_json() == {"run_id": ["No run with id 42 exists."]}
