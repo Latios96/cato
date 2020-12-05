8900867c4d391516bcfb3cab074e643b9472f1e8
Jan <jan.honsbrok@gmail.com>
2020-12-05 20:27:59 +0100

    add test result validator

---
+++ cato_server/api/validators/test_result_validators.py
@@ -0,0 +1,54 @@
+from typing import List, Dict
+
+from cato.domain.test_identifier import TestIdentifier
+from cato.storage.abstract.abstract_file_storage import AbstractFileStorage
+from cato.storage.abstract.suite_result_repository import SuiteResultRepository
+from cato_server.api.schemas.test_result_schemas import CreateTestResultSchema
+from cato_server.api.validators.basic import SchemaValidator
+
+
+class CreateTestResultValidator(SchemaValidator):
+    def __init__(
+        self,
+        suite_result_repository: SuiteResultRepository,
+        file_storage: AbstractFileStorage,
+    ):
+        super(CreateTestResultValidator, self).__init__(CreateTestResultSchema())
+        self._suite_result_repository = suite_result_repository
+        self._file_storage = file_storage
+
+    def validate(self, data: Dict) -> Dict[str, List[str]]:
+        errors = super(CreateTestResultValidator, self).validate(data)
+
+        suite_result_id = data.get("suite_result_id")
+        suite_result = self._suite_result_repository.find_by_id(suite_result_id)
+        if suite_result_id and not suite_result:
+            self.add_error(
+                errors,
+                "suite_result_id",
+                f"No suite result exists for id {suite_result_id}.",
+            )
+
+        test_identifier = data.get("test_identifier")
+        if test_identifier and suite_result:
+            suite_name = TestIdentifier.from_string(test_identifier).suite_name
+            if not suite_name == suite_result.suite_name:
+                self.add_error(
+                    errors,
+                    "test_identifier",
+                    f"Suite name in test identifier {test_identifier} does not match suite name {suite_result.suite_name} of linked suite result.",
+                )
+
+        image_output = data.get("image_output")
+        if image_output and not self._file_storage.find_by_id(image_output):
+            self.add_error(
+                errors, "image_output", f"No file exists for id {image_output}."
+            )
+
+        reference_image = data.get("reference_image")
+        if reference_image and not self._file_storage.find_by_id(reference_image):
+            self.add_error(
+                errors, "reference_image", f"No file exists for id {reference_image}."
+            )
+
+        return errors
---
+++ tests/test_test_result_validators.py
@@ -0,0 +1,106 @@
+import pytest
+
+from cato.storage.abstract.abstract_file_storage import AbstractFileStorage
+from cato.storage.abstract.suite_result_repository import SuiteResultRepository
+from cato.storage.domain.suite_result import SuiteResult
+from cato_server.api.validators.test_result_validators import CreateTestResultValidator
+from tests.utils import mock_safe
+
+
+class TestCreateTestResultValidator:
+    def test_success(self):
+        suite_result_repo = mock_safe(SuiteResultRepository)
+        suite_result_repo.find_by_id.return_value = SuiteResult(
+            id=1, run_id=1, suite_name="my_suite", suite_variables={}
+        )
+        file_storage = mock_safe(AbstractFileStorage)
+        file_storage.find_by_id = lambda x: x in [1, 2]
+        validator = CreateTestResultValidator(suite_result_repo, file_storage)
+
+        errors = validator.validate(
+            {
+                "suite_result_id": 1,
+                "test_name": "my_test_name",
+                "test_identifier": "my_suite/test_identifier",
+                "test_command": "my_command",
+                "test_variables": {"key": "value"},
+                "machine_info": {"cpu_name": "Intel", "cores": 8, "memory": 24},
+                "execution_status": "NOT_STARTED",
+                "image_output": 1,
+                "reference_image": 2,
+            }
+        )
+
+        assert errors == {}
+
+    @pytest.mark.parametrize(
+        "data,expected_errors",
+        [
+            (
+                {
+                    "suite_result_id": 5,
+                    "test_name": "my_test_name",
+                    "test_identifier": "my_suite/test_identifier",
+                    "test_command": "my_command",
+                    "test_variables": {"key": "value"},
+                    "machine_info": {"cpu_name": "Intel", "cores": 8, "memory": 24},
+                    "execution_status": "NOT_STARTED",
+                    "image_output": 1,
+                    "reference_image": 2,
+                },
+                {"suite_result_id": ["No suite result exists for id 5."]},
+            ),
+            (
+                {
+                    "suite_result_id": 1,
+                    "test_name": "my_test_name",
+                    "test_identifier": "foo/test_identifier",
+                    "test_command": "my_command",
+                    "test_variables": {"key": "value"},
+                    "machine_info": {"cpu_name": "Intel", "cores": 8, "memory": 24},
+                    "execution_status": "NOT_STARTED",
+                    "image_output": 1,
+                    "reference_image": 2,
+                },
+                {
+                    "test_identifier": [
+                        "Suite name in test identifier foo/test_identifier does "
+                        "not match suite name my_suite of linked suite result."
+                    ]
+                },
+            ),
+            (
+                {
+                    "suite_result_id": 1,
+                    "test_name": "my_test_name",
+                    "test_identifier": "my_suite/test_identifier",
+                    "test_command": "my_command",
+                    "test_variables": {"key": "value"},
+                    "machine_info": {"cpu_name": "Intel", "cores": 8, "memory": 24},
+                    "execution_status": "NOT_STARTED",
+                    "image_output": 4,
+                    "reference_image": 5,
+                },
+                {
+                    "image_output": ["No file exists for id 4."],
+                    "reference_image": ["No file exists for id 5."],
+                },
+            ),
+        ],
+    )
+    def test_failure(self, data, expected_errors):
+        suite_result_repo = mock_safe(SuiteResultRepository)
+        suite_result_repo.find_by_id = (
+            lambda x: SuiteResult(
+                id=1, run_id=1, suite_name="my_suite", suite_variables={}
+            )
+            if x == 1
+            else None
+        )
+        file_storage = mock_safe(AbstractFileStorage)
+        file_storage.find_by_id = lambda x: x in [1, 2]
+        validator = CreateTestResultValidator(suite_result_repo, file_storage)
+
+        errors = validator.validate(data)
+
+        assert errors == expected_errors
