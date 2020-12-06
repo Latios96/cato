cb7e9baa51980db0fa2841389469f6cf2b41f39a
Jan <jan.honsbrok@gmail.com>
2020-12-06 22:31:32 +0100

    add tests for run class mapper

---
+++ tests/test_run_class_mapper.py
@@ -0,0 +1,24 @@
+import datetime
+
+from cato.domain.run import Run
+from cato.mappers.run_class_mapper import RunClassMapper
+
+
+def test_map_from_dict():
+    started_at = datetime.datetime.now()
+    mapper = RunClassMapper()
+
+    result = mapper.map_from_dict(
+        {"id": 1, "project_id": 1, "started_at": started_at.isoformat()}
+    )
+
+    assert result == Run(id=1, project_id=1, started_at=started_at)
+
+
+def test_map_to_dict():
+    started_at = datetime.datetime.now()
+    mapper = RunClassMapper()
+
+    result = mapper.map_to_dict(Run(id=1, project_id=1, started_at=started_at))
+
+    assert result == {"id": 1, "project_id": 1, "started_at": started_at.isoformat()}
