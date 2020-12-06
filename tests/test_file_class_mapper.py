4c944f97dc4f39abbd7adac5261f335559e3ada7
Jan <jan.honsbrok@gmail.com>
2020-12-06 22:27:49 +0100

    add tests for file class mapper

---
+++ tests/test_file_class_mapper.py
@@ -0,0 +1,18 @@
+from cato.mappers.file_class_mapper import FileClassMapper
+from cato.storage.domain.File import File
+
+
+def test_map_from_dict():
+    mapper = FileClassMapper()
+
+    result = mapper.map_from_dict({"id": 1, "name": "file_name", "hash": "the_hash"})
+
+    assert result == File(id=1, name="file_name", hash="the_hash")
+
+
+def test_map_to_dict():
+    mapper = FileClassMapper()
+
+    result = mapper.map_to_dict(File(id=1, name="file_name", hash="the_hash"))
+
+    assert result == {"id": 1, "name": "file_name", "hash": "the_hash"}
