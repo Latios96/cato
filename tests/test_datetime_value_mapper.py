c21d90e9782d7c56b4c5a3242355643f8dbf8182
Jan <jan.honsbrok@gmail.com>
2020-12-06 19:31:21 +0100

    add datetime value mapper

---
+++ tests/test_datetime_value_mapper.py
@@ -0,0 +1,28 @@
+from cato.mappers.datetime_value_mapper import DateTimeValueMapper
+import datetime
+
+
+def test_map_from():
+    date = datetime.datetime.now()
+    wrapper = DateTimeValueMapper()
+
+    result = wrapper.map_from(date.isoformat())
+
+    assert result == date
+
+
+def test_map_from_empty():
+    wrapper = DateTimeValueMapper()
+
+    result = wrapper.map_from(None)
+
+    assert result is None
+
+
+def test_map_to():
+    date = datetime.datetime.now()
+    wrapper = DateTimeValueMapper()
+
+    result = wrapper.map_to(date)
+
+    assert result == date.isoformat()
