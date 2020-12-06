ff9537d5f08e053f0084a22ba337b4d39ad77139
Jan <jan.honsbrok@gmail.com>
2020-12-06 19:42:03 +0100

    add test status value mapper

---
+++ cato/mappers/test_status_value_mapper.py
@@ -0,0 +1,15 @@
+from typing import Optional
+
+from cato.domain.test_result import TestStatus
+from cato.mappers.abstract_value_mapper import AbstractValueMapper
+
+
+class TestStatusValueMapper(AbstractValueMapper[TestStatus, str]):
+    def map_from(self, status: Optional[str]) -> Optional[TestStatus]:
+        if not status:
+            return None
+        values = {"SUCCESS": TestStatus.SUCCESS, "FAILED": TestStatus.FAILED}
+        return values[status]
+
+    def map_to(self, status: TestStatus) -> str:
+        return status.name
---
+++ tests/test_test_status_value_mapper.py
@@ -0,0 +1,42 @@
+import pytest
+
+from cato.domain.test_result import TestStatus
+from cato.mappers.test_status_value_mapper import TestStatusValueMapper
+
+
+@pytest.mark.parametrize(
+    "value,expected",
+    [
+        ("SUCCESS", TestStatus.SUCCESS),
+        ("FAILED", TestStatus.FAILED),
+    ],
+)
+def test_map_from(value, expected):
+    wrapper = TestStatusValueMapper()
+
+    result = wrapper.map_from(value)
+
+    assert result == expected
+
+
+def test_map_from_empty():
+    wrapper = TestStatusValueMapper()
+
+    result = wrapper.map_from(None)
+
+    assert result is None
+
+
+@pytest.mark.parametrize(
+    "value,expected",
+    [
+        (TestStatus.SUCCESS, "SUCCESS"),
+        (TestStatus.FAILED, "FAILED"),
+    ],
+)
+def test_map_to(value, expected):
+    wrapper = TestStatusValueMapper()
+
+    result = wrapper.map_to(value)
+
+    assert result == expected
