06e8c55a3e5b05b64dd9ee6cb4ddc49735e91cb3
Jan <jan.honsbrok@gmail.com>
2020-12-06 19:36:12 +0100

    add execution status value mapper

---
+++ cato/mappers/execution_status_value_mapper.py
@@ -0,0 +1,18 @@
+from typing import Optional
+
+from cato.mappers.abstract_value_mapper import AbstractValueMapper
+from cato.storage.domain.execution_status import ExecutionStatus
+
+
+class ExecutionStatusValueMapper(AbstractValueMapper[ExecutionStatus, str]):
+    def map_from(self, status: Optional[str]) -> Optional[ExecutionStatus]:
+        if not status:
+            return None
+        return {
+            "NOT_STARTED": ExecutionStatus.NOT_STARTED,
+            "RUNNING": ExecutionStatus.RUNNING,
+            "FINISHED": ExecutionStatus.FINISHED,
+        }[status]
+
+    def map_to(self, status: ExecutionStatus) -> str:
+        return status.name
---
+++ tests/test_execution_status_value_mapper.py
@@ -0,0 +1,44 @@
+import pytest
+
+from cato.mappers.execution_status_value_mapper import ExecutionStatusValueMapper
+from cato.storage.domain.execution_status import ExecutionStatus
+
+
+@pytest.mark.parametrize(
+    "value,expected",
+    [
+        ("NOT_STARTED", ExecutionStatus.NOT_STARTED),
+        ("RUNNING", ExecutionStatus.RUNNING),
+        ("FINISHED", ExecutionStatus.FINISHED),
+    ],
+)
+def test_map_from(value, expected):
+    wrapper = ExecutionStatusValueMapper()
+
+    result = wrapper.map_from(value)
+
+    assert result == expected
+
+
+def test_map_from_empty():
+    wrapper = ExecutionStatusValueMapper()
+
+    result = wrapper.map_from(None)
+
+    assert result is None
+
+
+@pytest.mark.parametrize(
+    "value,expected",
+    [
+        (ExecutionStatus.NOT_STARTED, "NOT_STARTED"),
+        (ExecutionStatus.RUNNING, "RUNNING"),
+        (ExecutionStatus.FINISHED, "FINISHED"),
+    ],
+)
+def test_map_to(value, expected):
+    wrapper = ExecutionStatusValueMapper()
+
+    result = wrapper.map_to(value)
+
+    assert result == expected
