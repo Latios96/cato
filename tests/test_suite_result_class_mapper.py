61c0566a68db7ba466424cd0c577cfb19f6663f9
Jan <jan.honsbrok@gmail.com>
2020-12-06 22:33:37 +0100

    add tests for suite result class mapper

---
+++ tests/test_suite_result_class_mapper.py
@@ -0,0 +1,36 @@
+from cato.mappers.suite_result_class_mapper import SuiteResultClassMapper
+from cato.storage.domain.suite_result import SuiteResult
+
+
+def test_map_from_dict():
+    mapper = SuiteResultClassMapper()
+
+    result = mapper.map_from_dict(
+        {
+            "id": 1,
+            "run_id": 1,
+            "suite_name": "suite_name",
+            "suite_variables": {"key": "value"},
+        }
+    )
+
+    assert result == SuiteResult(
+        id=1, run_id=1, suite_name="suite_name", suite_variables={"key": "value"}
+    )
+
+
+def test_map_to_dict():
+    mapper = SuiteResultClassMapper()
+
+    result = mapper.map_to_dict(
+        SuiteResult(
+            id=1, run_id=1, suite_name="suite_name", suite_variables={"key": "value"}
+        )
+    )
+
+    assert result == {
+        "id": 1,
+        "run_id": 1,
+        "suite_name": "suite_name",
+        "suite_variables": {"key": "value"},
+    }
