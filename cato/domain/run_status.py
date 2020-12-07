314c0165b965b851c3e2347a7a9ee431cc79dea9
Jan <jan.honsbrok@gmail.com>
2020-12-07 21:37:48 +0100

    run status can bot be finished

--- cato/domain/run_status.py
+++ cato/domain/run_status.py
@@ -4,6 +4,5 @@ from enum import Enum
 class RunStatus(str, Enum):
     NOT_STARTED = "NOT_STARTED"
     RUNNING = "RUNNING"
-    FINISHED = "FINISHED"
     SUCCESS = "SUCCESS"
     FAILED = "FAILED"
