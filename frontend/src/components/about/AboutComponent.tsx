978144b6fafeb73a14e44fa51425e49531d54097
Jan <jan.honsbrok@gmail.com>
2020-12-07 22:02:31 +0100

    Bump version: 0.15.2 → 0.16.0

--- frontend/src/components/about/AboutComponent.tsx
+++ frontend/src/components/about/AboutComponent.tsx
@@ -3,7 +3,7 @@ import styles from "./AboutComponent.module.css";
 
 interface Props {}
 
-const FRONTEND_VERSION = "0.15.2";
+const FRONTEND_VERSION = "0.16.0";
 
 interface State {
   backendVersion: string;
