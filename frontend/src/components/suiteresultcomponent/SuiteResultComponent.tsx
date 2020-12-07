f286a0cb0ea86faeda963e52717b26962051cff9
Jan <jan.honsbrok@gmail.com>
2020-12-07 17:06:37 +0100

    fix test identifier display

--- frontend/src/components/suiteresultcomponent/SuiteResultComponent.tsx
+++ frontend/src/components/suiteresultcomponent/SuiteResultComponent.tsx
@@ -78,7 +78,7 @@ class SuiteResultComponent extends Component<Props, State> {
         <Card>
           <Card.Body>
             <Card.Title>
-              {result.test_identifier} {this.emojinizeStatus(result.status)}
+              {result.test_identifier.split("/")[0]} / {result.test_identifier.split("/")[1]} {this.emojinizeStatus(result.status)}
             </Card.Title>
             {result.execution_status === "FINISHED" ? (
               <FinishedTestResultComponent result={result} />
