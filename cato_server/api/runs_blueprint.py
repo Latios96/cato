aa9e0b38789e7daa8cbf87bdd1e8b8ca1b99a15f
Jan <jan.honsbrok@gmail.com>
2020-12-07 20:29:15 +0100

    add run status to api

--- cato_server/api/runs_blueprint.py
+++ cato_server/api/runs_blueprint.py
@@ -1,27 +1,36 @@
 import logging
-from dateutil.parser import parse
 from http.client import BAD_REQUEST
 
-from flask import Blueprint, jsonify, request
+from dateutil.parser import parse
+from flask import Blueprint, jsonify, request, abort
 
 from cato.domain.run import Run
+from cato.reporter.run_status_calculator import RunStatusCalculator
+from cato_server.api.validators.run_validators import CreateRunValidator
+from cato_server.storage.abstract.abstract_test_result_repository import (
+    TestResultRepository,
+)
 from cato_server.storage.abstract.project_repository import ProjectRepository
 from cato_server.storage.abstract.run_repository import RunRepository
-from cato_server.api.validators.run_validators import CreateRunValidator
 
 logger = logging.getLogger(__name__)
 
 
 class RunsBlueprint(Blueprint):
     def __init__(
-        self, run_repository: RunRepository, project_repository: ProjectRepository
+        self,
+        run_repository: RunRepository,
+        project_repository: ProjectRepository,
+        test_result_repository: TestResultRepository,
     ):
         super(RunsBlueprint, self).__init__("runs", __name__)
         self._run_repository = run_repository
         self._project_repository = project_repository
+        self._test_result_repository = test_result_repository
 
         self.route("/runs/project/<project_id>", methods=["GET"])(self.run_by_project)
         self.route("/runs", methods=["POST"])(self.create_run)
+        self.route("/runs/<int:run_id>/status", methods=["GET"])(self.status)
 
     def run_by_project(self, project_id):
         runs = self._run_repository.find_by_project_id(project_id)
@@ -41,3 +50,11 @@ class RunsBlueprint(Blueprint):
         run = self._run_repository.save(run)
         logger.info("Created run %s", run)
         return jsonify(run), 201
+
+    def status(self, run_id):
+        test_results = self._test_result_repository.find_by_run_id(run_id)
+
+        if not test_results:
+            abort(404)
+
+        return {"status": RunStatusCalculator().calculate(test_results).name}
--- tests/test_runs_blueprint.py
+++ tests/test_runs_blueprint.py
@@ -47,3 +47,16 @@ def test_create_run_failure(client):
 
     assert rv.status_code == 400
     assert rv.get_json() == {"project_id": ["No project with id 2 exists!"]}
+
+
+def test_get_status(client, run, test_result):
+    rv = client.get(f"/api/v1/runs/{run.id}/status")
+
+    assert rv.status_code == 200
+    assert rv.get_json() == {"status": "NOT_STARTED"}
+
+
+def test_get_status_404(client, run):
+    rv = client.get(f"/api/v1/runs/{run.id}/status")
+
+    assert rv.status_code == 404
