c8fd937d19b985b447433692c643ecec220c4b0c
Jan <jan.honsbrok@gmail.com>
2020-12-12 19:34:24 +0100

    add image channel class mapper

---
+++ cato/mappers/image_channel_class_mapper.py
@@ -0,0 +1,22 @@
+from typing import Dict
+
+from cato.domain.image import ImageChannel
+from cato.mappers.abstract_class_mapper import AbstractClassMapper, T
+
+
+class ImageChannelClassMapper(AbstractClassMapper[ImageChannel]):
+    def map_from_dict(self, json_data: Dict) -> ImageChannel:
+        return ImageChannel(
+            id=json_data["id"],
+            image_id=json_data["image_id"],
+            name=json_data["name"],
+            file_id=json_data["file_id"],
+        )
+
+    def map_to_dict(self, test_result: ImageChannel) -> Dict:
+        return {
+            "id": test_result.id,
+            "image_id": test_result.image_id,
+            "name": test_result.name,
+            "file_id": test_result.file_id,
+        }
---
+++ tests/test_image_channel_class_mapper.py
@@ -0,0 +1,22 @@
+from cato.domain.image import ImageChannel
+from cato.mappers.image_channel_class_mapper import ImageChannelClassMapper
+
+
+def test_from_dict_with_id():
+    mapper = ImageChannelClassMapper()
+
+    result = mapper.map_from_dict(
+        {"id": 1, "image_id": 2, "name": "test.exr", "file_id": 3}
+    )
+
+    assert result == ImageChannel(id=1, image_id=2, name="test.exr", file_id=3)
+
+
+def test_to_dict():
+    mapper = ImageChannelClassMapper()
+
+    result = mapper.map_to_dict(
+        ImageChannel(id=1, image_id=2, name="test.exr", file_id=3)
+    )
+
+    assert result == {"id": 1, "image_id": 2, "name": "test.exr", "file_id": 3}
