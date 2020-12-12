8c9f8a8fcde65f50005c3df5952e5bcf4a5cdab8
Jan <jan.honsbrok@gmail.com>
2020-12-12 15:26:18 +0100

    fix migration

--- alembic/versions/37ee8dba5560_add_image_channel_table.py
+++ alembic/versions/37ee8dba5560_add_image_channel_table.py
@@ -23,6 +23,8 @@ def upgrade():
         sa.Column("name", sa.String, nullable=False),
         sa.Column("file_entity_id", sa.INTEGER, sa.ForeignKey("file_entity.id")),
     )
+    with op.batch_alter_table("image_entity") as batch_op:
+        batch_op.drop_column("channels")
 
 
 def downgrade():
