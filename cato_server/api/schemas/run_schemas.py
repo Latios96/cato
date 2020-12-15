f7bc188f71ab38034a77065d6ed987786e0dcba9
Jan <jan.honsbrok@gmail.com>
2020-12-15 16:59:41 +0100

    outline run creation in one go

--- cato_server/api/schemas/run_schemas.py
+++ cato_server/api/schemas/run_schemas.py
@@ -1,6 +1,13 @@
 from marshmallow import Schema, fields, EXCLUDE
+from marshmallow.validate import Length
+from marshmallow_enum import EnumField
 
-from cato_server.api.schemas.general import ID_FIELD
+from cato_server.api.schemas.general import (
+    ID_FIELD,
+    NAME_FIELD, VARIABLES_FIELD, MachineInfoSchema,
+)
+from cato_server.api.schemas.test_result_schemas import is_test_identifier
+from cato_server.domain.execution_status import ExecutionStatus
 
 
 class CreateRunSchema(Schema):
@@ -9,3 +16,34 @@ class CreateRunSchema(Schema):
 
     project_id = ID_FIELD
     started_at = fields.DateTime()
+
+
+class TestForRunCreation(Schema):
+    class Meta:
+        unknown = EXCLUDE
+
+    test_name = NAME_FIELD
+    test_identifier = fields.String(
+        required=True, validate=[Length(min=1), is_test_identifier]
+    )
+    test_command = fields.String(required=True, validate=[Length(1)])
+    test_variables = VARIABLES_FIELD
+    machine_info = fields.Nested(MachineInfoSchema, required=True)
+    execution_status = EnumField(ExecutionStatus, required=True)
+
+
+class TestSuiteForRunCreation(Schema):
+    class Meta:
+        unknown = EXCLUDE
+
+    suite_name = NAME_FIELD
+    suite_variables = VARIABLES_FIELD
+    tests = fields.List(fields.Nested(TestForRunCreation), required=True)
+
+
+class CreateFullRunDto(Schema):
+    class Meta:
+        unknown = EXCLUDE
+
+    project_id = ID_FIELD
+    test_suites = fields.List(fields.Nested(TestSuiteForRunCreation), required=True)
