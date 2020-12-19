89f06b85e169bc44ebde1e373bffe57ffd9a39c0
Jan <jan.honsbrok@gmail.com>
2020-12-19 20:40:06 +0100

    display suites in tab

--- frontend/src/components/runsummary/internal/RunSummaryTabComponent.tsx
+++ frontend/src/components/runsummary/internal/RunSummaryTabComponent.tsx
@@ -1,6 +1,8 @@
-import { useHistory } from "react-router-dom";
-import { Tab, Tabs } from "react-bootstrap";
+import { Link, useHistory } from "react-router-dom";
+import { ListGroup, Tab, Tabs } from "react-bootstrap";
 import React, { useEffect, useState } from "react";
+import SuiteResult from "../../../models/SuiteResult";
+import styles from "./RunSummaryTabComponent.module.scss";
 
 interface Props {
   projectId: number;
@@ -10,10 +12,23 @@ interface Props {
 
 export default function RunSummaryTabComponent(props: Props) {
   const [currentTab, setCurrentTab] = useState(props.currentTab);
+  const [suites, setSuites] = useState<SuiteResult[]>([]);
   let history = useHistory();
+
   useEffect(() => {
     setCurrentTab(props.currentTab);
-  }, [props.currentTab]);
+    fetch(`/api/v1/suite_results/run/${props.runId}`)
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setSuites(result);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  }, [props.currentTab, props.runId]);
+
   return (
     <Tabs
       id="controlled-tab-example"
@@ -23,10 +38,34 @@ export default function RunSummaryTabComponent(props: Props) {
       }
     >
       <Tab eventKey="suites" title="Suites">
-        Suites
+        <div className={styles.tabContent}>
+          <ListGroup>
+            {suites.map((suite) => {
+              return (
+                <Link to={"test"}>
+                  <ListGroup.Item className={styles.listEntry}>
+                    {suite.suite_name}
+                  </ListGroup.Item>
+                </Link>
+              );
+            })}
+          </ListGroup>
+        </div>
       </Tab>
       <Tab eventKey="tests" title="Tests">
-        Tests
+        <div className={styles.tabContent}>
+          <ListGroup>
+            {suites.map((suite) => {
+              return (
+                <Link to={"test"}>
+                  <ListGroup.Item className={styles.listEntry}>
+                    {suite.suite_name}
+                  </ListGroup.Item>
+                </Link>
+              );
+            })}
+          </ListGroup>
+        </div>
       </Tab>
     </Tabs>
   );
