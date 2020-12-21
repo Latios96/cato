70393bc063d94c36492e55cdbdf709051e2c8df9
Jan <jan.honsbrok@gmail.com>
2020-12-21 16:44:32 +0100

    add TestHeartbeatBlueprint

--- tests/cato_server/test_test_heartbeat_blueprint.py
+++ tests/cato_server/test_test_heartbeat_blueprint.py
@@ -24,3 +24,10 @@ def test_should_update_new_test_heartbeat(client, test_result):
     assert (
         datetime.datetime.now() - datetime.datetime.fromisoformat(json["last_beat"])
     ).seconds < 1
+
+
+def test_not_existing_test_result_id_should_400(client):
+    rv = client.post("/api/v1/test_heartbeats/42")
+
+    assert rv.status_code == 400
+    assert rv.get_json() == {"test_result_id": "No test result found with id 42"}
