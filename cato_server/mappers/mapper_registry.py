eeef5575610047ec4c5c09e32a61ff8ffe3f0887
Jan Honsbrok <jan.honsbrok@gmail.com>
2020-12-29 15:15:29 +0100

    add mapper registry

---
+++ cato_server/mappers/mapper_registry.py
@@ -0,0 +1,16 @@
+from typing import Type, TypeVar, Optional
+
+from cato_server.mappers.abstract_class_mapper import AbstractClassMapper
+
+T = TypeVar("T")
+
+
+class MapperRegistry:
+    def __init__(self):
+        self._mappers = {}
+
+    def register_mapper(self, cls: Type[T], mapper: AbstractClassMapper[T]):
+        self._mappers[cls] = mapper
+
+    def class_mapper_for_cls(self, cls: Type[T]) -> Optional[AbstractClassMapper[T]]:
+        return self._mappers.get(cls)
---
+++ tests/cato_server/mappers/test_mapper_registry.py
@@ -0,0 +1,19 @@
+from cato_server.domain.project import Project
+from cato_server.mappers.mapper_registry import MapperRegistry
+from cato_server.mappers.project_class_mapper import ProjectClassMapper
+
+
+def test_register_and_get():
+    mapper_registry = MapperRegistry()
+    mapper = ProjectClassMapper()
+
+    mapper_registry.register_mapper(Project, mapper)
+    returned_mapper = mapper_registry.class_mapper_for_cls(Project)
+
+    assert returned_mapper is mapper
+
+
+def test_no_mapper_exists():
+    mapper_registry = MapperRegistry()
+
+    assert not mapper_registry.class_mapper_for_cls(Project)
