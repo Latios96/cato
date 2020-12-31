39b1244b1bbcfacc18d7e90eb5f63ad28191a751
Jan Honsbrok <jan.honsbrok@gmail.com>
2020-12-31 14:45:07 +0100

    fix

--- tests/cato_server/storage/test_db_migrations.py
+++ tests/cato_server/storage/test_db_migrations.py
@@ -8,22 +8,6 @@ from cato_server.configuration.storage_configuration import StorageConfiguration
 from cato_server.storage.sqlalchemy.migrations.db_migrator import DbMigrator
 
 
-def test_migrations(test_resource_provider):
-    config_path = test_resource_provider.resource_by_name(
-        "alembic-config-for-tests.ini"
-    )
-    alembic_cfg = Config(config_path)
-    alembic_cfg.set_section_option(
-        "alembic",
-        "script_location",
-        os.path.join(
-            os.path.dirname(os.path.dirname(os.path.dirname(config_path))), "alembic"
-        ),
-    )
-
-    command.upgrade(alembic_cfg, "head")
-
-
 def test_db_migrator():
     db_migrator = DbMigrator(
         StorageConfiguration(file_storage_url="", database_url="sqlite:///:memory:")
