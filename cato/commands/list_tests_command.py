4fa16b31331894987133b6491e7a268a7116ab6f
Jan Honsbrok <jan.honsbrok@gmail.com>
2021-01-06 23:37:51 +0100

    add list tests command

---
+++ cato/commands/list_tests_command.py
@@ -0,0 +1,26 @@
+import logging
+
+from cato.commands.base_command import BaseCliCommand
+from cato.config.config_file_parser import JsonConfigParser
+from cato.domain.test_suite import count_tests, count_suites, iterate_suites_and_tests
+
+
+class ListTestsCommand(BaseCliCommand):
+    def __init__(
+        self,
+        json_config_parser: JsonConfigParser,
+        logger: logging.Logger,
+    ):
+        self._json_config_parser = json_config_parser
+        self._logger = logger
+
+    def list_tests(self, path: str):
+        config = self._json_config_parser.parse(path)
+
+        self._logger.info(
+            f"Found {count_tests(config.test_suites)} tests in {count_suites(config.test_suites)} suites:"
+        )
+        self._logger.info("")
+
+        for suite, test in iterate_suites_and_tests(config.test_suites):
+            self._logger.info(f"{suite.name}/{test.name}")
---
+++ tests/cato/commands/test_list_tests_command.py
@@ -0,0 +1,49 @@
+import copy
+import logging
+from unittest.mock import call
+
+from cato.commands.list_tests_command import ListTestsCommand
+from cato.config.config_file_parser import JsonConfigParser
+from cato.domain.config import Config
+from cato.domain.test import Test
+from cato.domain.test_suite import TestSuite
+from tests.utils import mock_safe
+
+TEST = Test(
+    name="My_first_test",
+    command="mayabatch -s {config_file_folder}/{test_name.json} -o {image_output}/{test_name.png}",
+    variables={"frame": "7"},
+)
+TEST_SUITE = TestSuite(
+    name="My_first_test_Suite",
+    tests=[TEST],
+    variables={"my_var": "from_suite"},
+)
+CONFIG = Config(
+    project_name="EXAMPLE_PROJECT",
+    path="test",
+    test_suites=[TEST_SUITE],
+    output_folder="output",
+    variables={"my_var": "from_config"},
+)
+
+
+def test_list_tests():
+    config = copy.deepcopy(CONFIG)
+    mock_json_config_parser = mock_safe(JsonConfigParser)
+    mock_logger = mock_safe(logging.Logger)
+    list_tests_command = ListTestsCommand(
+        mock_json_config_parser,
+        mock_logger,
+    )
+    mock_json_config_parser.parse.return_value = config
+
+    list_tests_command.list_tests("my_path")
+
+    mock_logger.info.assert_has_calls(
+        [
+            call("Found 1 tests in 1 suites:"),
+            call(""),
+            call("My_first_test_Suite/My_first_test"),
+        ]
+    )
