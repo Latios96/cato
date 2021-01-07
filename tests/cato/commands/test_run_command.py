8ef6fb1c8c63e7fb28886b0aed03d9ede279febe
Jan <jan.honsbrok@gmail.com>
2021-01-07 14:34:21 +0100

    refactor test setup

--- tests/cato/commands/test_run_command.py
+++ tests/cato/commands/test_run_command.py
@@ -40,117 +40,74 @@ CONFIG = Config(
 )
 
 
-def test_should_run_all_suites_and_tests():
-    config = copy.deepcopy(CONFIG)
-    mock_json_config_parser = mock_safe(JsonConfigParser)
-    mock_test_suite_runner = mock_safe(TestSuiteRunner)
-    mock_timing_report_generator = mock_safe(TimingReportGenerator)
-    mock_end_message_generator = mock_safe(EndMessageGenerator)
-    mock_logger = mock_safe(logging.Logger)
-    run_command = RunCommand(
-        mock_json_config_parser,
-        mock_test_suite_runner,
-        mock_timing_report_generator,
-        mock_end_message_generator,
-        mock_logger,
-    )
-    mock_json_config_parser.parse.return_value = config
-    started_at = datetime.datetime.now()
-    finished_at = datetime.datetime.now()
-    result = [
-        TestSuiteExecutionResult(
-            test_suite=TEST_SUITE,
-            result=TestStatus.SUCCESS,
-            test_results=[
-                TestExecutionResult(
-                    test=TEST,
-                    status=TestStatus.SUCCESS,
-                    output=["this", "is", "my", "output"],
-                    seconds=4,
-                    message="",
-                    image_output=None,
-                    reference_image=None,
-                    started_at=started_at,
-                    finished_at=finished_at,
-                )
-            ],
+class TestRunCommand:
+    def setup_method(self, method):
+        self.config = copy.deepcopy(CONFIG)
+        self.mock_json_config_parser = mock_safe(JsonConfigParser)
+        self.mock_test_suite_runner = mock_safe(TestSuiteRunner)
+        self.mock_timing_report_generator = mock_safe(TimingReportGenerator)
+        self.mock_end_message_generator = mock_safe(EndMessageGenerator)
+        self.mock_logger = mock_safe(logging.Logger)
+        self.run_command = RunCommand(
+            self.mock_json_config_parser,
+            self.mock_test_suite_runner,
+            self.mock_timing_report_generator,
+            self.mock_end_message_generator,
+            self.mock_logger,
         )
-    ]
-    mock_test_suite_runner.run_test_suites.return_value = result
-    mock_timing_report_generator.generate.return_value = "Timing report"
-    mock_end_message_generator.generate_end_message.return_value = "End message"
+        self.mock_json_config_parser.parse.return_value = self.config
 
-    run_command.run("my_path", None, None)
+    def test_should_run_all_suites_and_tests(self):
+        started_at = datetime.datetime.now()
+        finished_at = datetime.datetime.now()
+        result = [
+            TestSuiteExecutionResult(
+                test_suite=TEST_SUITE,
+                result=TestStatus.SUCCESS,
+                test_results=[
+                    TestExecutionResult(
+                        test=TEST,
+                        status=TestStatus.SUCCESS,
+                        output=["this", "is", "my", "output"],
+                        seconds=4,
+                        message="",
+                        image_output=None,
+                        reference_image=None,
+                        started_at=started_at,
+                        finished_at=finished_at,
+                    )
+                ],
+            )
+        ]
+        self.mock_test_suite_runner.run_test_suites.return_value = result
+        self.mock_timing_report_generator.generate.return_value = "Timing report"
+        self.mock_end_message_generator.generate_end_message.return_value = (
+            "End message"
+        )
 
-    mock_test_suite_runner.run_test_suites.assert_called_with(config)
-    mock_timing_report_generator.generate.assert_called_with(result)
-    mock_end_message_generator.generate_end_message.assert_called_with(result)
-    mock_logger.info.assert_has_calls(
-        [call(""), call("Timing report"), call(""), call("End message")]
-    )
+        self.run_command.run("my_path", None, None)
 
+        self.mock_test_suite_runner.run_test_suites.assert_called_with(self.config)
+        self.mock_timing_report_generator.generate.assert_called_with(result)
+        self.mock_end_message_generator.generate_end_message.assert_called_with(result)
+        self.mock_logger.info.assert_has_calls(
+            [call(""), call("Timing report"), call(""), call("End message")]
+        )
 
-def test_should_filter_by_suite_name():
-    config = copy.deepcopy(CONFIG)
-    mock_json_config_parser = mock_safe(JsonConfigParser)
-    mock_test_suite_runner = mock_safe(TestSuiteRunner)
-    mock_timing_report_generator = mock_safe(TimingReportGenerator)
-    mock_end_message_generator = mock_safe(EndMessageGenerator)
-    mock_logger = mock_safe(logging.Logger)
-    run_command = RunCommand(
-        mock_json_config_parser,
-        mock_test_suite_runner,
-        mock_timing_report_generator,
-        mock_end_message_generator,
-        mock_logger,
-    )
-    mock_json_config_parser.parse.return_value = config
+    def test_should_filter_by_suite_name(self):
+        self.run_command.run("my_path", "not existing name", None)
 
-    run_command.run("my_path", "not existing name", None)
+        self.mock_test_suite_runner.run_test_suites.assert_called_with(self.config)
 
-    mock_test_suite_runner.run_test_suites.assert_called_with(config)
+        assert self.config.test_suites == []
 
-    assert config.test_suites == []
+    def test_should_filter_by_test_identifier(self):
+        self.run_command.run("my_path", None, "not_existing_suite/test")
 
+        self.mock_test_suite_runner.run_test_suites.assert_called_with(self.config)
 
-def test_should_filter_by_test_identifier():
-    config = copy.deepcopy(CONFIG)
-    mock_json_config_parser = mock_safe(JsonConfigParser)
-    mock_test_suite_runner = mock_safe(TestSuiteRunner)
-    mock_timing_report_generator = mock_safe(TimingReportGenerator)
-    mock_end_message_generator = mock_safe(EndMessageGenerator)
-    mock_logger = mock_safe(logging.Logger)
-    run_command = RunCommand(
-        mock_json_config_parser,
-        mock_test_suite_runner,
-        mock_timing_report_generator,
-        mock_end_message_generator,
-        mock_logger,
-    )
-    mock_json_config_parser.parse.return_value = config
+        assert self.config.test_suites == []
 
-    run_command.run("my_path", None, "not_existing_suite/test")
-
-    mock_test_suite_runner.run_test_suites.assert_called_with(config)
-
-    assert config.test_suites == []
-
-
-def test_should_filter_by_invalid_test_identifier_str():
-    config = copy.deepcopy(CONFIG)
-    mock_json_config_parser = mock_safe(JsonConfigParser)
-    mock_test_suite_runner = mock_safe(TestSuiteRunner)
-    mock_timing_report_generator = mock_safe(TimingReportGenerator)
-    mock_end_message_generator = (mock_safe(EndMessageGenerator),)
-    mock_logger = mock_safe(logging.Logger)
-    run_command = RunCommand(
-        mock_json_config_parser,
-        mock_test_suite_runner,
-        mock_timing_report_generator,
-        mock_end_message_generator,
-        mock_logger,
-    )
-    mock_json_config_parser.parse.return_value = config
-
-    with pytest.raises(ValueError):
-        run_command.run("my_path", None, "not_existing_suite")
+    def test_should_filter_by_invalid_test_identifier_str(self):
+        with pytest.raises(ValueError):
+            self.run_command.run("my_path", None, "not_existing_suite")
