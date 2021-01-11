ba2938fde2d19a4e050cb0467916f3bbb795b0ef
Jan <jan.honsbrok@gmail.com>
2021-01-11 21:16:44 +0100

    fix saving

--- cato/runners/test_suite_runner.py
+++ cato/runners/test_suite_runner.py
@@ -1,8 +1,11 @@
-from typing import List
+from typing import List, Callable
 
 from cato.domain.config import Config
 from cato.domain.test_status import TestStatus
 from cato.domain.test_suite_execution_result import TestSuiteExecutionResult
+from cato.file_system_abstractions.last_run_information_repository import (
+    LastRunInformationRepository,
+)
 from cato.reporter.reporter import Reporter
 from cato.reporter.test_execution_reporter import TestExecutionReporter
 from cato.runners.test_runner import TestRunner
@@ -16,10 +19,16 @@ class TestSuiteRunner:
         test_runner: TestRunner,
         reporter: Reporter,
         test_execution_reporter: TestExecutionReporter,
+        last_run_information_repository_factory: Callable[
+            [str], LastRunInformationRepository
+        ],
     ):
         self._test_runner = test_runner
         self._reporter = reporter
         self._test_execution_reporter = test_execution_reporter
+        self._last_run_information_repository_factory = (
+            last_run_information_repository_factory
+        )
 
     def run_test_suites(self, config: Config) -> List[TestSuiteExecutionResult]:
 
@@ -48,5 +57,9 @@ class TestSuiteRunner:
                 self._test_execution_reporter.report_test_result(suite, result)
 
             results.append(TestSuiteExecutionResult(suite, suite_status, test_results))
+
+        self._test_execution_reporter.report_test_execution_end(
+            self._last_run_information_repository_factory(config.output_folder)
+        )
 
         return results
--- tests/cato/test_test_suite_runner.py
+++ tests/cato/test_test_suite_runner.py
@@ -8,6 +8,10 @@ from cato.domain.test_execution_result import TestExecutionResult
 from cato.domain.test_status import TestStatus
 from cato.domain.test_suite import TestSuite
 from cato.domain.test_suite_execution_result import TestSuiteExecutionResult
+from cato.file_system_abstractions.last_run_information_repository import (
+    LastRunInformationRepository,
+    LastRunInformation,
+)
 from cato.reporter.reporter import Reporter
 from cato.reporter.test_execution_reporter import TestExecutionReporter
 from cato.runners.test_runner import TestRunner
@@ -21,8 +25,12 @@ def test_run_empty_suites_should_fail():
     mock_reporter = mock_safe(Reporter)
     mock_test_runner = mock_safe(TestRunner)
     mock_execution_reporter = mock_safe(TestExecutionReporter)
+    mock_last_run_information_repository = mock_safe(LastRunInformationRepository)
     test_suite_runner = TestSuiteRunner(
-        mock_test_runner, mock_reporter, mock_execution_reporter
+        mock_test_runner,
+        mock_reporter,
+        mock_execution_reporter,
+        lambda x: mock_last_run_information_repository,
     )
 
     with pytest.raises(ValueError):
@@ -40,8 +48,12 @@ def test_run_suite_should_report_start_and_delegate_to_test_runner():
     mock_reporter = mock_safe(Reporter)
     mock_test_runner = mock_safe(TestRunner)
     mock_execution_reporter = mock_safe(TestExecutionReporter)
+    mock_last_run_information_repository = mock_safe(LastRunInformationRepository)
     test_suite_runner = TestSuiteRunner(
-        mock_test_runner, mock_reporter, mock_execution_reporter
+        mock_test_runner,
+        mock_reporter,
+        mock_execution_reporter,
+        lambda x: mock_last_run_information_repository,
     )
     test = Test(name="my_first_test", command="dummy_command", variables={})
     test_suite = TestSuite(name="example", tests=[test])
@@ -56,14 +68,21 @@ def test_run_suite_should_report_start_and_delegate_to_test_runner():
 
     mock_reporter.report_start_test_suite.assert_called_once()
     mock_test_runner.run_test.assert_called_with(config, test_suite, test)
+    mock_execution_reporter.report_test_execution_end.assert_called_with(
+        mock_last_run_information_repository
+    )
 
 
 def test_run_suite_should_return_correctly_collected_results():
     mock_reporter = mock_safe(Reporter)
     mock_test_runner = mock_safe(TestRunner)
     mock_execution_reporter = mock_safe(TestExecutionReporter)
+    mock_last_run_information_repository = mock_safe(LastRunInformationRepository)
     test_suite_runner = TestSuiteRunner(
-        mock_test_runner, mock_reporter, mock_execution_reporter
+        mock_test_runner,
+        mock_reporter,
+        mock_execution_reporter,
+        lambda x: mock_last_run_information_repository,
     )
     test = Test(name="my_first_test", command="dummy_command", variables={})
     test_suite = TestSuite(name="example", tests=[test])
@@ -91,3 +110,6 @@ def test_run_suite_should_return_correctly_collected_results():
     assert result == [
         TestSuiteExecutionResult(test_suite, TestStatus.SUCCESS, [execution_result])
     ]
+    mock_execution_reporter.report_test_execution_end.assert_called_with(
+        mock_last_run_information_repository
+    )
