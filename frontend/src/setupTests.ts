a28bbef69361dd2ecef189bf5b10e6165e17864c
Jan <jan.honsbrok@gmail.com>
2021-01-16 23:53:32 +0100

    typescript test setup

--- frontend/src/setupTests.ts
+++ frontend/src/setupTests.ts
@@ -2,4 +2,11 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import "@testing-library/jest-dom";
+import "@testing-library/jest-dom/extend-expect";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+import Enzyme from "enzyme";
+import { createSerializer } from "enzyme-to-json";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+expect.addSnapshotSerializer(createSerializer({ mode: "deep" }));
