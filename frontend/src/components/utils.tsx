df0382ebf224a49d6332caae4f319ce95f71a287
Jan <jan.honsbrok@gmail.com>
2021-01-17 00:00:44 +0100

    callback arg is checked and can not be null

--- frontend/src/components/utils.tsx
+++ frontend/src/components/utils.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 export function renderIf<T>(
-  value: T,
+  value: T | null | undefined,
   renderCallback: (value: T) => JSX.Element
 ): JSX.Element {
-  if (value) {
+  if (value !== undefined && value !== null) {
     return renderCallback(value);
   }
   return <React.Fragment />;
