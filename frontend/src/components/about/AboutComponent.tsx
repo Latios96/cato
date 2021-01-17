a617b3c79f5595c1f2e5afdab2dc8db0e882023f
Jan <jan.honsbrok@gmail.com>
2021-01-17 17:44:15 +0100

    Bump version: 0.29.0 → 0.30.0

--- frontend/src/components/about/AboutComponent.tsx
+++ frontend/src/components/about/AboutComponent.tsx
@@ -3,7 +3,7 @@ import { useQuery } from "react-query";
 import styles from "./AboutComponent.module.css";
 import { Spinner } from "react-bootstrap";
 
-const FRONTEND_VERSION = "0.29.0";
+const FRONTEND_VERSION = "0.30.0";
 
 interface AboutInformation {
   version: string;
