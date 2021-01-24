16315da1104f4b81005c7fc40659a0f8fd0caf71
Jan <jan.honsbrok@gmail.com>
2021-01-24 17:16:05 +0100

    remove unused conftest.py

--- tests/integrationtests/cato/conftest.py
+++
@@ -1,8 +0,0 @@
-import os
-
-import pytest
-
-from cato.config.config_file_writer import ConfigFileWriter
-from cato.domain.config import Config
-from cato.domain.test import Test
-from cato.domain.test_suite import TestSuite
