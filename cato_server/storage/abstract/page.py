80e00043cc5c602faf29461f4c9aeaad51c05585
Jan <jan.honsbrok@gmail.com>
2021-02-03 19:04:33 +0100

    add page methods and tests

--- cato_server/storage/abstract/page.py
+++ cato_server/storage/abstract/page.py
@@ -1,3 +1,4 @@
+import math
 from dataclasses import dataclass
 from typing import TypeVar, Generic, List
 
@@ -11,12 +12,30 @@ class PageRequest:
 
     @staticmethod
     def first(page_size: int):
-        return PageRequest(0, page_size)
+        return PageRequest(1, page_size)
+
+    @property
+    def offset(self):
+        return self.page_size * (self.page_number - 1)
 
 
 @dataclass
 class Page(Generic[T]):
     page_number: int
+    page_size: int
     total_pages: int
-    element_count: int
-    items: List[T]
+    entities: List[T]
+
+    @staticmethod
+    def from_page_request(
+        page_request: PageRequest, total_entity_count: int, entities: List[T]
+    ):
+        total_pages = math.ceil(total_entity_count / page_request.page_size)
+        if total_pages == 0:
+            total_pages = 1
+        return Page(
+            page_number=page_request.page_number,
+            page_size=page_request.page_size,
+            total_pages=total_pages,
+            entities=entities,
+        )
--- tests/unittests/cato_server/storage/test_page.py
+++ tests/unittests/cato_server/storage/test_page.py
@@ -1,8 +1,41 @@
-from cato_server.storage.abstract.page import PageRequest
+import pytest
+
+from cato_server.storage.abstract.page import PageRequest, Page
 
 
 class TestPageRequest:
     def test_create_first_page(self):
         page_request = PageRequest.first(10)
 
-        assert PageRequest(0, 10) == page_request
+        assert PageRequest(1, 10) == page_request
+
+    def test_offset_first_page(self):
+        page_request = PageRequest.first(10)
+
+        assert page_request.offset == 0
+
+    def test_offset_second_page(self):
+        page_request = PageRequest(2, 10)
+
+        assert page_request.offset == 10
+
+
+class TestPage:
+    def test_from_page_request(self):
+        page_request = PageRequest.first(10)
+        entities = [1, 2, 3]
+
+        page = Page.from_page_request(page_request, 10, entities)
+
+        assert page == Page(1, 10, 1, entities)
+
+    @pytest.mark.parametrize(
+        "total_entity_count,total_pages",
+        [(0, 1), (1, 1), (9, 1), (10, 1), (11, 2), (50, 5), (100, 10)],
+    )
+    def test_correct_total_page_count(self, total_entity_count, total_pages):
+        page_request = PageRequest.first(10)
+        entities = [1, 2, 3]
+
+        page = Page.from_page_request(page_request, total_entity_count, entities)
+        assert page == Page(1, 10, total_pages, entities)
