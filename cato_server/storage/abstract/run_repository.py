3816dd5bd8a57577be132b573d4b457aeab60439
Jan <jan.honsbrok@gmail.com>
2021-02-03 19:55:01 +0100

    remove unnecessary methods

--- cato_server/storage/abstract/run_repository.py
+++ cato_server/storage/abstract/run_repository.py
@@ -1,15 +1,9 @@
-from typing import Optional, Iterable
+from typing import Iterable
 
 from cato_server.domain.run import Run
 from cato_server.storage.abstract.abstract_repository import AbstractRepository
 
 
 class RunRepository(AbstractRepository[Run, int]):
-    def save(self, run: Run) -> Run:
-        raise NotImplementedError()
-
-    def find_by_id(self, id: int) -> Optional[Run]:
-        raise NotImplementedError()
-
     def find_by_project_id(self, id: int) -> Iterable[Run]:
         raise NotImplementedError()
