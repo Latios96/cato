6c403a581f841ad32012aab188b8a7c2b6c2663a
Jan <jan.honsbrok@gmail.com>
2021-02-03 19:55:24 +0100

    add abstract method to repo

--- cato_server/storage/abstract/run_repository.py
+++ cato_server/storage/abstract/run_repository.py
@@ -2,8 +2,14 @@ from typing import Iterable
 
 from cato_server.domain.run import Run
 from cato_server.storage.abstract.abstract_repository import AbstractRepository
+from cato_server.storage.abstract.page import PageRequest, Page
 
 
 class RunRepository(AbstractRepository[Run, int]):
     def find_by_project_id(self, id: int) -> Iterable[Run]:
+        raise NotImplementedError()
+
+    def find_by_project_id_with_paging(
+        self, id: int, page_request: PageRequest
+    ) -> Page[Run]:
         raise NotImplementedError()
