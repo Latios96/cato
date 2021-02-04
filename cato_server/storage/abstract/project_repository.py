92a63f0b15391cb456a2703f49023537f221678f
Jan <jan.honsbrok@gmail.com>
2021-02-04 09:38:09 +0100

    remove unused method

--- cato_server/storage/abstract/project_repository.py
+++ cato_server/storage/abstract/project_repository.py
@@ -5,8 +5,5 @@ from cato_server.storage.abstract.abstract_repository import AbstractRepository
 
 
 class ProjectRepository(AbstractRepository[Project, int]):
-    def save(self, project: Project) -> Project:
-        raise NotImplementedError()
-
     def find_by_name(self, name: str) -> Optional[Project]:
         raise NotImplementedError()
