2a6e138c3bcb37ba0b3045ce2e8f56e0a3c24730
Jan <jan.honsbrok@gmail.com>
2021-02-09 21:46:37 +0100

    fix linter

--- frontend/src/components/Pagination/usePagination.ts
+++ frontend/src/components/Pagination/usePagination.ts
@@ -1,5 +1,4 @@
-import { useCallback, useState } from "react";
-import { Page } from "./Page";
+import { useState } from "react";
 import { PageRequest } from "./PageRequest";
 interface ControllablePage {
   page_number: number;
