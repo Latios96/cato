29aeb2c628671dfbd2c10836387fc2b622f50614
Jan <jan.honsbrok@gmail.com>
2021-02-21 18:03:23 +0100

    add method to interface

--- cato_server/storage/abstract/suite_result_repository.py
+++ cato_server/storage/abstract/suite_result_repository.py
@@ -2,10 +2,16 @@ from typing import Optional, Iterable
 
 from cato_server.storage.abstract.abstract_repository import AbstractRepository
 from cato_server.domain.suite_result import SuiteResult
+from cato_server.storage.abstract.page import PageRequest, Page
 
 
 class SuiteResultRepository(AbstractRepository):
     def find_by_id(self, id: int) -> Optional[SuiteResult]:
+        raise NotImplementedError()
+
+    def find_by_run_id_with_paging(
+        self, run_id: int, page_request: PageRequest
+    ) -> Page[SuiteResult]:
         raise NotImplementedError()
 
     def find_by_run_id_and_name(self, run_id: int, name: str) -> Optional[SuiteResult]:
