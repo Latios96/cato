6abe94f41243d3439b35021c30de5c82cdf34b18
Jan Honsbrok <jan.honsbrok@gmail.com>
2021-02-24 20:06:11 +0100

    better test for migrator

--- tests/unittests/cato_server/storage/test_db_migrations.py
+++ tests/unittests/cato_server/storage/test_db_migrations.py
@@ -1,15 +1,12 @@
-import os
 import subprocess
 
 import pytest
-from alembic import command
-from alembic.config import Config
 
 from cato_server.configuration.storage_configuration import StorageConfiguration
 from cato_server.storage.sqlalchemy.migrations.db_migrator import DbMigrator
 
 
-def test_db_migrator():
+def test_db_migrator_sqlite():
     db_migrator = DbMigrator(
         StorageConfiguration(file_storage_url="", database_url="sqlite:///:memory:")
     )
@@ -29,25 +26,9 @@ requires_postgres = pytest.mark.skipif(
 
 
 @requires_postgres
-def test_example_postgres(postgresql, test_resource_provider):
+def test_db_migrator_postgresql(postgresql):
     connection = f"postgresql+psycopg2://{postgresql.info.user}:@{postgresql.info.host}:{postgresql.info.port}/{postgresql.info.dbname}"
-
-    config_path = test_resource_provider.resource_by_name(
-        "alembic-config-for-tests.ini"
+    db_migrator = DbMigrator(
+        StorageConfiguration(file_storage_url="", database_url=connection)
     )
-    alembic_cfg = Config(config_path)
-    alembic_cfg.set_section_option("alembic", "sqlalchemy.url", connection)
-    alembic_cfg.set_section_option(
-        "alembic",
-        "script_location",
-        os.path.join(
-            os.path.dirname(os.path.dirname(os.path.dirname(config_path))),
-            "cato_server",
-            "storage",
-            "sqlalchemy",
-            "migrations",
-            "alembic",
-        ),
-    )
-
-    command.upgrade(alembic_cfg, "head")
+    db_migrator.migrate()
