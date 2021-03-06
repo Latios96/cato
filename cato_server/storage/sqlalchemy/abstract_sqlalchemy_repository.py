28d6a363a2d9b0f4a0697b8768da0b372093cebd
Jan <jan.honsbrok@gmail.com>
2021-03-06 10:22:38 +0100

    use different collation

--- cato_server/storage/sqlalchemy/abstract_sqlalchemy_repository.py
+++ cato_server/storage/sqlalchemy/abstract_sqlalchemy_repository.py
@@ -124,4 +124,4 @@ class AbstractSqlAlchemyRepository(Generic[T, E, K]):
         if "sqlite" in query.session.bind.driver:
             return query.order_by(asc(collate(attr, "NOCASE")))
         else:
-            return query.order_by(asc(collate(attr, "de_DE")))
+            return query.order_by(asc(collate(attr, "C")))
