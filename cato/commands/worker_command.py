47ab26c34d4efb0aa4700918ee9c067cf12b87a0
Jan <jan.honsbrok@gmail.com>
2021-03-09 19:23:21 +0100

    implement worker command

---
+++ cato/commands/worker_command.py
@@ -0,0 +1,87 @@
+import logging
+import os
+
+from cato.commands.base_command import BaseCliCommand
+from cato.config.config_encoder import ConfigEncoder
+from cato.domain.config import Config
+from cato.domain.test import Test
+from cato.domain.test_execution_result import TestExecutionResult
+from cato.domain.test_status import TestStatus
+from cato.domain.test_suite import filter_by_test_identifier, TestSuite
+from cato.reporter.reporter import Reporter
+from cato.reporter.test_execution_reporter import TestExecutionReporter
+from cato.runners.test_runner import TestRunner
+from cato_server.domain.test_identifier import TestIdentifier
+
+
+class WorkerCommand(BaseCliCommand):
+    def __init__(
+        self,
+        config_encoder: ConfigEncoder,
+        test_execution_reporter: TestExecutionReporter,
+        test_runner: TestRunner,
+        reporter: Reporter,
+        logger: logging.Logger,
+    ):
+        self._config_encoder = config_encoder
+        self._test_execution_reporter = test_execution_reporter
+        self._test_runner = test_runner
+        self._reporter = reporter
+        self._logger = logger
+
+    def execute(
+        self,
+        encoded_config: str,
+        test_identifier_str: str,
+        run_id: int,
+        config_file_path: str,
+    ):
+        if os.path.isdir(config_file_path):
+            config_file_path = os.path.join("config.ini")
+        config = self._config_encoder.decode(encoded_config.encode(), config_file_path)
+
+        config.test_suites = filter_by_test_identifier(
+            config.test_suites, TestIdentifier.from_string(test_identifier_str)
+        )
+
+        if len(config.test_suites) == 0 or len(config.test_suites[0].tests) == 0:
+            raise ValueError(
+                f"Filter {test_identifier_str} did match not match a test!"
+            )
+
+        suite = config.test_suites[0]
+        test = config.test_suites[0].tests[0]
+
+        self._test_execution_reporter.use_run_id(run_id)
+
+        self._execute_test(config, suite, test)
+
+    def _execute_test(self, config: Config, suite: TestSuite, test: Test):
+        test_identifier = TestIdentifier(suite.name, test.name)
+
+        self._test_execution_reporter.report_test_execution_start(suite, test)
+
+        result = self._test_runner.run_test(config, suite, test)
+
+        if result.status == TestStatus.SUCCESS:
+            self._reporter.report_test_success(result)
+            self._success_message(test_identifier, result)
+        else:
+            self._reporter.report_test_failure(result)
+            self._failure_message(test_identifier, result)
+
+        self._test_execution_reporter.report_test_result(suite, result)
+
+    def _success_message(
+        self, test_identifier: TestIdentifier, result: TestExecutionResult
+    ):
+        self._logger.info("")
+        self._logger.info(f"Test {test_identifier} passed in f{result.seconds}")
+
+    def _failure_message(
+        self, test_identifier: TestIdentifier, result: TestExecutionResult
+    ):
+        self._logger.info("")
+        self._logger.info(
+            f"Test {test_identifier} failed in f{result.seconds}: f{result.message}"
+        )
---
+++ tests/unittests/cato/commands/test_worker_command.py
@@ -0,0 +1,105 @@
+import datetime
+import logging
+
+import pytest
+
+from cato.commands.worker_command import WorkerCommand
+from cato.config.config_encoder import ConfigEncoder
+from cato.config.config_file_parser import JsonConfigParser
+from cato.config.config_file_writer import ConfigFileWriter
+from cato.domain.test_execution_result import TestExecutionResult
+from cato.domain.test_status import TestStatus
+from cato.reporter.reporter import Reporter
+from cato.reporter.test_execution_reporter import TestExecutionReporter
+from cato.runners.test_runner import TestRunner
+from tests.utils import mock_safe
+
+
+class TestWorkerCommand:
+    def setup_method(self, method):
+        self.config_encoder = ConfigEncoder(ConfigFileWriter(), JsonConfigParser())
+        self.mock_test_execution_reporter = mock_safe(TestExecutionReporter)
+        self.mock_test_runner = mock_safe(TestRunner)
+        self.mock_reporter = mock_safe(Reporter)
+        self.mock_logger = mock_safe(logging.Logger)
+        self.worker_command = WorkerCommand(
+            self.config_encoder,
+            self.mock_test_execution_reporter,
+            self.mock_test_runner,
+            self.mock_reporter,
+            self.mock_logger,
+        )
+
+    def test_not_matching_identifier_for_suite_should_raise(self, config_fixture):
+        config_str = self.config_encoder.encode(config_fixture.CONFIG)
+
+        with pytest.raises(ValueError):
+            self.worker_command.execute(
+                config_str.decode(), "test/My_first_test", 5, "some/path"
+            )
+
+    def test_not_matching_identifier_for_test_should_raise(self, config_fixture):
+        config_str = self.config_encoder.encode(config_fixture.CONFIG)
+
+        with pytest.raises(ValueError):
+            self.worker_command.execute(
+                config_str.decode(), "My_first_test_Suite/test", 5, "some/path"
+            )
+
+    def test_test_run_success(self, config_fixture):
+        config_str = self.config_encoder.encode(config_fixture.CONFIG)
+        execution_result = TestExecutionResult(
+            config_fixture.TEST,
+            TestStatus.SUCCESS,
+            [],
+            1,
+            "this is a message",
+            None,
+            None,
+            datetime.datetime.now(),
+            datetime.datetime.now(),
+        )
+        self.mock_test_runner.run_test.return_value = execution_result
+
+        self.worker_command.execute(
+            config_str.decode(), "My_first_test_Suite/My_first_test", 5, "some/path"
+        )
+
+        self.mock_test_execution_reporter.use_run_id.assert_called_with(5)
+        self.mock_test_execution_reporter.report_test_execution_start.assert_called_with(
+            config_fixture.TEST_SUITE, config_fixture.TEST
+        )
+        self.mock_test_runner.run_test.assert_called_once()
+        self.mock_reporter.report_test_success.assert_called_once()
+        self.mock_logger.info.assert_called_with(
+            f"Test My_first_test_Suite/My_first_test passed in f{execution_result.seconds}"
+        )
+
+    def test_test_run_failure(self, config_fixture):
+        config_str = self.config_encoder.encode(config_fixture.CONFIG)
+        execution_result = TestExecutionResult(
+            config_fixture.TEST,
+            TestStatus.FAILED,
+            [],
+            1,
+            "this is a message",
+            None,
+            None,
+            datetime.datetime.now(),
+            datetime.datetime.now(),
+        )
+        self.mock_test_runner.run_test.return_value = execution_result
+
+        self.worker_command.execute(
+            config_str.decode(), "My_first_test_Suite/My_first_test", 5, "some/path"
+        )
+
+        self.mock_test_execution_reporter.use_run_id.assert_called_with(5)
+        self.mock_test_execution_reporter.report_test_execution_start.assert_called_with(
+            config_fixture.TEST_SUITE, config_fixture.TEST
+        )
+        self.mock_test_runner.run_test.assert_called_once()
+        self.mock_reporter.report_test_failure.assert_called_once()
+        self.mock_logger.info.assert_called_with(
+            f"Test My_first_test_Suite/My_first_test failed in f{execution_result.seconds}: f{execution_result.message}"
+        )
