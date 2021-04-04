1f5c1c652769052afa2d61088745a2bb5672e120
Jan <jan.honsbrok@gmail.com>
2021-04-04 18:04:07 +0200

    remove snapshot tests

--- frontend/src/components/utils.tsx
+++ frontend/src/components/utils.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 export function renderIf<T>(
   value: T | null | undefined,
   renderCallback: (value: T) => JSX.Element
@@ -7,4 +7,38 @@ export function renderIf<T>(
     return renderCallback(value);
   }
   return <React.Fragment />;
+}
+
+export interface FetchResult<T> {
+  data?: T;
+  error?: string;
+  isLoading: boolean;
+}
+
+export function useFetch<T>(url: string): FetchResult<T> {
+  const [fetchResult, setFetchResult] = useState({
+    isLoading: true,
+    data: undefined,
+    error: undefined,
+  });
+
+  fetch(url)
+    .then((res) => res.json())
+    .then(
+      (result) => {
+        setFetchResult({
+          isLoading: false,
+          data: result,
+          error: undefined,
+        });
+      },
+      (error) =>
+        setFetchResult({
+          isLoading: false,
+          data: undefined,
+          error: error,
+        })
+    );
+
+  return fetchResult;
 }
--- frontend/src/setupTests.ts
+++ frontend/src/setupTests.ts
@@ -3,10 +3,3 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom/extend-expect";
-import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
-import Enzyme from "enzyme";
-import { createSerializer } from "enzyme-to-json";
-
-Enzyme.configure({ adapter: new Adapter() });
-
-expect.addSnapshotSerializer(createSerializer({ mode: "deep" }));
