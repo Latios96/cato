a7619a4ddf2f92ec8e72dd0b5f393d6df74e258f
Jan <jan.honsbrok@gmail.com>
2021-04-07 20:31:12 +0200

    use correct executable

--- tests/integrationtests/cato/conftest.py
+++ tests/integrationtests/cato/conftest.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import sys
 
 import pytest
 
@@ -24,7 +25,19 @@ def cato_config(tmp_path, test_resource_provider, scenario_context):
     )
 
     scenario_context["config_folder"] = config_folder
-    scenario_context["config_path"] = os.path.join(config_folder, "cato.json")
+    config_path = os.path.join(config_folder, "cato.json")
+    scenario_context["config_path"] = config_path
+
+    with open(config_path) as f:
+        content = f.read()
+
+    executable = sys.executable
+    executable = executable.replace("\\", "/")
+
+    content = content.replace('"python_exe": "python"', f'"python_exe": "{executable}"')
+
+    with open(config_path, "wb") as f:
+        f.write(content.encode())
 
 
 @pytest.fixture
