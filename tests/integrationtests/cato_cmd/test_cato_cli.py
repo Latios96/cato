6809f0f8f4347c29b679485809c4846a1f63760e
Jan <jan.honsbrok@gmail.com>
2021-04-15 22:22:07 +0200

    remove duplicated tests

--- tests/integrationtests/cato_cmd/test_cato_cli.py
+++ tests/integrationtests/cato_cmd/test_cato_cli.py
@@ -20,58 +20,6 @@ from cato_server.mappers.object_mapper import ObjectMapper
 from tests.integrationtests.utils import change_cwd, snapshot_output, run_command
 
 
-def test_list_tests_command_from_path(snapshot, config_file_fixture):
-    snapshot_output(
-        snapshot,
-        [sys.executable, "-m", "cato", "list-tests", "--path", config_file_fixture],
-        workdir=os.path.dirname(os.path.dirname(config_file_fixture)),
-    )
-
-
-def test_list_tests_command_from_cwd(snapshot, config_file_fixture):
-    snapshot_output(
-        snapshot,
-        [sys.executable, "-m", "cato", "list-tests"],
-        workdir=os.path.dirname(config_file_fixture),
-    )
-
-
-def test_config_file_template(snapshot, tmp_path):
-    snapshot_output(
-        snapshot,
-        [sys.executable, "-m", "cato", "config-template", "."],
-        workdir=str(tmp_path),
-        trimmers={
-            "Wrote config file to .*cato\.json": "Wrote config file to SOME_RANDOM_DIR/cato.json"
-        },
-    )
-
-
-def test_update_missing_reference_images_should_have_no_effect(
-    snapshot, test_resource_provider
-):
-    snapshot_output(
-        snapshot,
-        [sys.executable, "-m", "cato", "update-missing-reference-images"],
-        workdir=test_resource_provider.resource_by_name("cato_test_config"),
-    )
-
-
-def test_update_reference_should_have_no_effect(snapshot, test_resource_provider):
-    snapshot_output(
-        snapshot,
-        [
-            sys.executable,
-            "-m",
-            "cato",
-            "update-reference",
-            "--test-identifier",
-            "My_first_test_Suite/My_first_test",
-        ],
-        workdir=test_resource_provider.resource_by_name("cato_test_config"),
-    )
-
-
 @pytest.fixture
 def run_config(tmp_path, test_resource_provider):
     test1 = Test(
