e37cd2a6a4af925f01f4e029db7fcb324ca76c46
Jan Honsbrok <jan.honsbrok@gmail.com>
2021-04-24 13:20:56 +0200

    emitting events when starting test

--- cato_server/usecases/start_test.py
+++ cato_server/usecases/start_test.py
@@ -1,9 +1,15 @@
 import datetime
 import logging
 
+from cato_server.configuration.optional_component import OptionalComponent
+from cato_server.domain.event import Event
 from cato_server.domain.execution_status import ExecutionStatus
 from cato_server.domain.test_result import TestResult
+from cato_server.mappers.object_mapper import ObjectMapper
+from cato_server.queues.abstract_message_queue import AbstractMessageQueue
 from cato_server.storage.abstract.test_result_repository import TestResultRepository
+from cato_api_models.catoapimodels import TestResultStartedDto
+
 
 logger = logging.getLogger(__name__)
 
@@ -12,8 +18,12 @@ class StartTest:
     def __init__(
         self,
         test_result_repository: TestResultRepository,
+        message_queue: OptionalComponent[AbstractMessageQueue],
+        object_mapper: ObjectMapper,
     ):
         self._test_result_repository = test_result_repository
+        self._message_queue = message_queue
+        self._object_mapper = object_mapper
 
     def start_test(self, test_result_id: int):
         logger.info("Starting test test with id %s", test_result_id)
@@ -32,9 +42,23 @@ class StartTest:
             self._reset_possible_data_from_previous_run(test_result)
 
         test_result = self._test_result_repository.save(test_result)
-        logger.info("Starting test with id %s", test_result_id)
+        logger.info("Started test %s", test_result)
+
+        if self._message_queue.is_available():
+            dto = TestResultStartedDto(test_result.id)
+            event = Event("TEST_RESULT_STARTED", dto)
+            logger.info("Sending event %s", event)
+            self._message_queue.component.send_event(
+                "test_result_events",
+                str(test_result.suite_result_id),
+                event,
+                self._object_mapper,
+            )
 
     def _reset_possible_data_from_previous_run(self, test_result: TestResult):
+        logger.info(
+            "Reseting data from previos run for test with id %s", test_result.id
+        )
         test_result.status = None
         test_result.seconds = None
         test_result.message = None
--- tests/unittests/cato_server/usecases/test_start_test_usecase.py
+++ tests/unittests/cato_server/usecases/test_start_test_usecase.py
@@ -2,16 +2,30 @@ import datetime
 
 import pytest
 
+from cato_server.configuration.optional_component import OptionalComponent
+from cato_server.domain.event import Event
 from cato_server.domain.execution_status import ExecutionStatus
+from cato_server.mappers.mapper_registry_factory import MapperRegistryFactory
+from cato_server.mappers.object_mapper import ObjectMapper
+from cato_server.queues.abstract_message_queue import AbstractMessageQueue
 from cato_server.storage.abstract.test_result_repository import TestResultRepository
 from cato_server.usecases.start_test import StartTest
 from tests.utils import mock_safe
+from cato_api_models.catoapimodels import TestResultStartedDto
 
 
 class TestStartTest:
     def setup_method(self, method):
         self.test_result_repository = mock_safe(TestResultRepository)
-        self.start_test_usecase = StartTest(self.test_result_repository)
+        self.message_queue = OptionalComponent(mock_safe(AbstractMessageQueue))
+        self.object_mapper = ObjectMapper(
+            MapperRegistryFactory().create_mapper_registry()
+        )
+        self.start_test_usecase = StartTest(
+            self.test_result_repository,
+            self.message_queue,
+            self.object_mapper,
+        )
 
     def test_starting_a_not_existing_test_should_fail(self):
         self.test_result_repository.find_by_id.return_value = None
@@ -28,12 +42,21 @@ class TestStartTest:
         test_result.started_at = None
         test_result.execution_status = ExecutionStatus.NOT_STARTED
         self.test_result_repository.find_by_id.return_value = test_result
+        self.test_result_repository.save.return_value = test_result
+        event_dto = TestResultStartedDto(test_result.id)
+        event = Event("TEST_RESULT_STARTED", event_dto)
 
         self.start_test_usecase.start_test(1)
 
         self.test_result_repository.save.assert_called_with(test_result)
         assert test_result.execution_status == ExecutionStatus.RUNNING
         assert test_result.started_at is not None
+        self.message_queue.component.send_event.assert_called_with(
+            "test_result_events",
+            str(test_result.suite_result_id),
+            event,
+            self.object_mapper,
+        )
 
     def test_starting_an_existing_which_was_started_at_least_once_should_reset_data(
         self, test_result_factory
@@ -46,6 +69,7 @@ class TestStartTest:
         self.start_test_usecase.start_test(1)
 
         self.test_result_repository.save.assert_called_with(test_result)
+        self.message_queue.component.send_event.assert_called_once()
         assert test_result.execution_status == ExecutionStatus.RUNNING
         assert test_result.started_at is not None
         assert test_result.status == None
