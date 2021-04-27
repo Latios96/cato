7dfe459ef4e04e79bc90bab5c7f7793bf3f36cb5
Jan <jan.honsbrok@gmail.com>
2021-04-27 21:21:32 +0200

    use logger

--- cato_server/api/framework/flask_impl/flask_api_provider.py
+++ cato_server/api/framework/flask_impl/flask_api_provider.py
@@ -5,6 +5,10 @@ from cato_server.api.framework.abstract.api_provider import AbstractApiProvider
 from cato_server.api.framework.abstract.api_resource import AbstractBaseResource
 from cato_server.api.framework.flask_impl.flask_test_client import FlaskTestClient
 
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 class FlaskApiProvider(AbstractApiProvider):
     def __init__(self):
@@ -16,7 +20,7 @@ class FlaskApiProvider(AbstractApiProvider):
 
     def run(self):
         Twisted(self._app)
-        print(f"Up and running on http://127.0.0.1:{5000}")
+        logger.info(f"Up and running on http://127.0.0.1:{5000}")
         self._app.run("127.0.0.1", 5000)
 
     def test_client(self):
