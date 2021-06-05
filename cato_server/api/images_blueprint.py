213c7a664b097014e4695a63a49e9e2b6a3f1ffc
Jan Honsbrok <jan.honsbrok@gmail.com>
2021-06-05 20:03:17 +0200

    convert images blueprint

--- cato_server/api/images_blueprint.py
+++ cato_server/api/images_blueprint.py
@@ -1,10 +1,12 @@
 import logging
 import os
+import shutil
 import tempfile
 
-from flask import jsonify, request, send_file, abort
+from fastapi import APIRouter, UploadFile
+from fastapi.params import File
+from starlette.responses import JSONResponse, FileResponse, Response
 
-from cato_server.api.base_blueprint import BaseBlueprint
 from cato_server.images.store_image import StoreImage
 from cato_server.mappers.object_mapper import ObjectMapper
 from cato_server.storage.abstract.abstract_file_storage import AbstractFileStorage
@@ -13,53 +15,57 @@ from cato_server.storage.abstract.image_repository import ImageRepository
 logger = logging.getLogger(__name__)
 
 
-class ImagesBlueprint(BaseBlueprint):
+class ImagesBlueprint(APIRouter):
     def __init__(
         self,
         file_storage: AbstractFileStorage,
         image_repository: ImageRepository,
         object_mapper: ObjectMapper,
     ):
-        super(ImagesBlueprint, self).__init__("images", __name__)
+        super(ImagesBlueprint, self).__init__()
         self._file_storage = file_storage
         self._image_repository = image_repository
         self._object_mapper = object_mapper
 
-        self.route("images", methods=["POST"])(self.upload_file)
-        self.route("images/original_file/<int:file_id>", methods=["GET"])(
-            self.get_original_image_file
-        )
-        self.route("images/<int:image_id>", methods=["GET"])(self.get_image)
+        self.post("/images")(self.upload_file)
+        self.get("/images/original_file/{file_id}")(self.get_original_image_file)
+        self.get("/images/{image_id}")(self.get_image)
 
-    def upload_file(self):
-        uploaded_file = request.files["file"]
+    def upload_file(self, file: UploadFile = File(...)):
+        uploaded_file = file
         if not uploaded_file.filename:
-            return jsonify({"file": "Filename can not be empty!"}), 400
+            return JSONResponse(
+                content={"file": "Filename can not be empty!"}, status_code=400
+            )
 
         store_image = StoreImage(self._file_storage, self._image_repository)
 
         try:
             with tempfile.TemporaryDirectory() as tmpdirname:
                 tmp_path = os.path.join(tmpdirname, uploaded_file.filename)
-                uploaded_file.save(tmp_path)
+                with open(tmp_path, "wb") as tmp:
+                    shutil.copyfileobj(uploaded_file.file, tmp)
                 image = store_image.store_image(tmp_path)
 
                 logger.info("Deleting tmpdir %s", tmpdirname)
         except Exception as e:
             logger.error(e, exc_info=True)
-            return jsonify({"message": "Error when saving image"}), 400
-        return self.json_response(self._object_mapper.to_json(image)), 201
+            return JSONResponse(
+                content={"message": "Error when saving image"}, status_code=400
+            )
+
+        return JSONResponse(content=self._object_mapper.to_dict(image), status_code=201)
 
     def get_original_image_file(self, file_id: int):
         image = self._image_repository.find_by_id(file_id)
         file = self._file_storage.find_by_id(image.original_file_id)
         file_path = self._file_storage.get_path(file)
         if file and os.path.exists(file_path):
-            return send_file(file_path, attachment_filename=file.name)
-        return jsonify({"file_id": "No file found!"}), 404
+            return FileResponse(path=file_path, filename=file.name)
+        return JSONResponse(content={"file_id": "No file found!"}, status_code=201)
 
     def get_image(self, image_id):
         image = self._image_repository.find_by_id(image_id)
         if not image:
-            abort(404)
-        return self.json_response(self._object_mapper.to_json(image)), 200
+            return Response(status_code=404)
+        return JSONResponse(content=self._object_mapper.to_dict(image), status_code=200)
--- tests/unittests/cato_server/api/test_images_blueprint.py
+++ tests/unittests/cato_server/api/test_images_blueprint.py
@@ -8,11 +8,11 @@ API_V_IMAGES = "/api/v1/images"
 
 def test_upload_image(client, test_resource_provider):
     test_image = test_resource_provider.resource_by_name("test_image_white.jpg")
-    data = {"file": (open(test_image, "rb"), "test_image_white.jpg")}
-    response = client.post(API_V_IMAGES, data=data)
+    data = {"file": ("test_image_white.jpg", open(test_image, "rb"))}
+    response = client.post(API_V_IMAGES, files=data)
 
     assert response.status_code == 201
-    assert response.get_json() == {
+    assert response.json() == {
         "channels": [{"file_id": 2, "id": 1, "image_id": 1, "name": "rgb"}],
         "id": 1,
         "name": "test_image_white.jpg",
@@ -24,11 +24,11 @@ def test_upload_image(client, test_resource_provider):
 
 def test_upload_image_no_filename(client, test_resource_provider):
     test_image = test_resource_provider.resource_by_name("test_image_white.jpg")
-    data = {"file": (open(test_image, "rb"), "")}
-    response = client.post(API_V_IMAGES, data=data)
+    data = {"file": ("", open(test_image, "rb"))}
+    response = client.post(API_V_IMAGES, files=data)
 
     assert response.status_code == 400
-    assert response.get_json() == {"file": "Filename can not be empty!"}
+    assert response.json() == {"file": "Filename can not be empty!"}
 
 
 def test_get_image_not_found(client):
@@ -39,14 +39,14 @@ def test_get_image_not_found(client):
 
 def test_get_image_found_image(client, test_resource_provider):
     test_image = test_resource_provider.resource_by_name("test_image_white.jpg")
-    data = {"file": (open(test_image, "rb"), "test_image_white.jpg")}
-    response = client.post(API_V_IMAGES, data=data)
-    image_id = response.get_json()["id"]
+    data = {"file": ("test_image_white.jpg", open(test_image, "rb"))}
+    response = client.post(API_V_IMAGES, files=data)
+    image_id = response.json()["id"]
 
     response = client.get(f"/api/v1/images/{image_id}")
 
     assert response.status_code == 200
-    assert response.get_json() == {
+    assert response.json() == {
         "channels": [{"file_id": 2, "id": 1, "image_id": 1, "name": "rgb"}],
         "id": 1,
         "name": "test_image_white.jpg",
@@ -58,8 +58,8 @@ def test_get_image_found_image(client, test_resource_provider):
 
 def test_upload_unsupported_file(client, test_resource_provider):
     test_file = test_resource_provider.resource_by_name("unsupported-file.txt")
-    data = {"file": (open(test_file, "rb"), "test_file.txt")}
+    data = {"file": ("test_file.txt", open(test_file, "rb"))}
 
-    response = client.post(API_V_IMAGES, data=data)
+    response = client.post(API_V_IMAGES, files=data)
 
     assert response.status_code == 400
