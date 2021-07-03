83a33b43897287ecf0739d82ae8c108a87e89141
Jan Honsbrok <jan.honsbrok@gmail.com>
2021-07-03 14:08:44 +0200

    implement new image comparator

---
+++ cato_server/images/image_comparator.py
@@ -0,0 +1,58 @@
+from dataclasses import dataclass
+
+import cv2
+from skimage import metrics
+
+from cato.domain.test_status import TestStatus
+from cato_server.domain.comparison_settings import ComparisonSettings
+from cato_server.domain.comparison_result import ComparisonResult
+
+
+@dataclass
+class Resolution:
+    width: int
+    height: int
+
+    def __str__(self):
+        return f"{self.width}x{self.height}px"
+
+
+class ImageComparator:
+    def compare(
+        self, reference: str, output: str, comparison_settings: ComparisonSettings
+    ) -> ComparisonResult:
+        output_image = cv2.imread(output)
+        reference_image = cv2.imread(reference)
+
+        output_image_resolution = Resolution(
+            output_image.shape[0], output_image.shape[1]
+        )
+        reference_image_resolution = Resolution(
+            reference_image.shape[0], reference_image.shape[1]
+        )
+
+        images_have_same_resolution = (
+            output_image_resolution == reference_image_resolution
+        )
+        if not images_have_same_resolution:
+            return ComparisonResult(
+                status=TestStatus.FAILED,
+                message=f"Images have different resolutions! Reference image is {reference_image_resolution}, output image is {output_image_resolution}",
+                diff_image=None,
+            )
+
+        (score, diff) = metrics.structural_similarity(
+            reference_image, output_image, full=True, multichannel=True
+        )
+        diff = (diff * 255).astype("uint8")
+
+        if score < comparison_settings.threshold:
+            return ComparisonResult(
+                status=TestStatus.FAILED,
+                message=f"Images are not equal! {comparison_settings.method} score was {score:.3f}, max threshold is {comparison_settings.threshold:.3f}",
+                diff_image=None,
+            )
+
+        return ComparisonResult(
+            status=TestStatus.SUCCESS, message=None, diff_image=None
+        )
---
+++ tests/unittests/cato_server/images/test_image_comparator.py
@@ -0,0 +1,92 @@
+from cato.domain.test_status import TestStatus
+from cato_server.domain.comparison_method import ComparisonMethod
+from cato_server.domain.comparison_result import ComparisonResult
+from cato_server.domain.comparison_settings import ComparisonSettings
+from cato_server.images.image_comparator import ImageComparator
+
+
+def test_compare_image_should_fail_different_resolution(test_resource_provider):
+    output_image = test_resource_provider.resource_by_name("100x100_reference.png")
+    reference_image = test_resource_provider.resource_by_name("200x100_reference.png")
+    image_comparator = ImageComparator()
+
+    comparison_result = image_comparator.compare(
+        reference_image,
+        output_image,
+        ComparisonSettings(threshold=1, method=ComparisonMethod.SSIM),
+    )
+
+    assert comparison_result == ComparisonResult(
+        status=TestStatus.FAILED,
+        message="Images have different resolutions! Reference image is 100x200px, output image is 100x100px",
+        diff_image=None,
+    )
+
+
+def test_compare_image_should_fail_one_pixel_different(test_resource_provider):
+    output_image = test_resource_provider.resource_by_name("single_pixel_output.png")
+    reference_image = test_resource_provider.resource_by_name(
+        "single_pixel_reference.png"
+    )
+    image_comparator = ImageComparator()
+
+    comparison_result = image_comparator.compare(
+        reference_image,
+        output_image,
+        ComparisonSettings(threshold=1, method=ComparisonMethod.SSIM),
+    )
+
+    assert comparison_result == ComparisonResult(
+        status=TestStatus.FAILED,
+        message="Images are not equal! ComparisonMethod.SSIM score was 0.994, max threshold is 1.000",
+        diff_image=None,
+    )
+
+
+def test_compare_image_should_fail_waith_and_without_watermark(test_resource_provider):
+    output_image = test_resource_provider.resource_by_name("with_watermark.png")
+    reference_image = test_resource_provider.resource_by_name("without_watermark.png")
+    image_comparator = ImageComparator()
+
+    comparison_result = image_comparator.compare(
+        reference_image,
+        output_image,
+        ComparisonSettings(threshold=1, method=ComparisonMethod.SSIM),
+    )
+
+    assert comparison_result == ComparisonResult(
+        status=TestStatus.FAILED,
+        message="Images are not equal! ComparisonMethod.SSIM score was 0.885, max threshold is 1.000",
+        diff_image=None,
+    )
+
+
+def test_compare_image_should_succeed_same_image(test_resource_provider):
+    image = test_resource_provider.resource_by_name("100x100_reference.png")
+    image_comparator = ImageComparator()
+
+    comparison_result = image_comparator.compare(
+        image, image, ComparisonSettings(threshold=1, method=ComparisonMethod.SSIM)
+    )
+
+    assert comparison_result == ComparisonResult(
+        status=TestStatus.SUCCESS, message=None, diff_image=None
+    )
+
+
+def test_compare_image_should_succeed_threshold_not_exceeded(test_resource_provider):
+    output_image = test_resource_provider.resource_by_name("with_watermark.png")
+    reference_image = test_resource_provider.resource_by_name("without_watermark.png")
+    image_comparator = ImageComparator()
+
+    comparison_result = image_comparator.compare(
+        reference_image,
+        output_image,
+        ComparisonSettings(threshold=0.8, method=ComparisonMethod.SSIM),
+    )
+
+    assert comparison_result == ComparisonResult(
+        status=TestStatus.SUCCESS,
+        message=None,
+        diff_image=None,
+    )
