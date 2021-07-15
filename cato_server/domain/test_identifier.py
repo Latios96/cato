2f41aa60616d9c6dd03b04fd2aa7f847da08e216
Jan Honsbrok <jan.honsbrok@gmail.com>
2021-07-15 09:47:58 +0200

    add more validation for test identifier

--- cato_server/domain/test_identifier.py
+++ cato_server/domain/test_identifier.py
@@ -1,17 +1,32 @@
-from dataclasses import dataclass
+import attr
 
 
-@dataclass
+@attr.s
 class TestIdentifier:
-    suite_name: str
-    test_name: str
+    suite_name: str = attr.ib()
+    test_name: str = attr.ib()
 
     @staticmethod
     def from_string(string: str):
-        suite_name, test_name = string.split("/")
+        splitted_string = string.split("/")
+        if len(splitted_string) != 2:
+            raise ValueError(f"Invalid test identifier: {string}")
+        suite_name, test_name = splitted_string
         return TestIdentifier(suite_name, test_name)
 
     def __str__(self):
         return f"{self.suite_name}/{self.test_name}"
 
     __test__ = False
+
+    @suite_name.validator
+    def _check_suite_name(self, attribute, value):
+        self._validate_test_identifier_part("suite name", value)
+
+    @test_name.validator
+    def _check_test_name(self, attribute, value):
+        self._validate_test_identifier_part("suite name", value)
+
+    def _validate_test_identifier_part(self, part, value):
+        if "/" in value:
+            raise ValueError(r"Test Identifier {part} can not contain '/'")
--- tests/unittests/cato/test_test_identifier.py
+++ tests/unittests/cato/test_test_identifier.py
@@ -1,5 +1,32 @@
+import pytest
+
 from cato_server.domain.test_identifier import TestIdentifier
 
 
 def test_from_string():
     assert TestIdentifier.from_string("suite/test") == TestIdentifier("suite", "test")
+
+
+def test_from_string_invalid():
+    with pytest.raises(ValueError):
+        assert TestIdentifier.from_string("suite/test/invalid")
+
+
+def test_construct_valid():
+    test_identifier = TestIdentifier("suite", "test")
+
+    assert test_identifier.suite_name == "suite"
+    assert test_identifier.test_name == "test"
+
+
+@pytest.mark.parametrize(
+    "suite_name,test_name",
+    [
+        ("test/invalid", "valid"),
+        ("valid", "test/invalid"),
+        ("test/invalid", "invalid/test"),
+    ],
+)
+def test_construct_invalid(suite_name, test_name):
+    with pytest.raises(ValueError):
+        assert TestIdentifier(suite_name, test_name)
