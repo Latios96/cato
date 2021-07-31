22964178ff8ac12b1aab53ad9a0d7c21253cfa29
Jan Honsbrok <jan.honsbrok@gmail.com>
2021-07-31 11:51:10 +0200

    add support for map from for generic class mapper

--- cato_server/mappers/generic_class_mapper.py
+++ cato_server/mappers/generic_class_mapper.py
@@ -1,29 +1,18 @@
 import copy
-import datetime
-from dataclasses import _is_dataclass_instance, fields
-from enum import Enum
-from typing import Type, Dict, TypeVar, Mapping, Collection
+from dataclasses import _is_dataclass_instance, fields, is_dataclass
+from typing import Type, Dict, TypeVar, Collection
 
-import attr
-from dataclasses_json.core import (
-    _decode_dataclass,
-    _encode_overrides,
-    _user_overrides_or_exts,
-)
-from dataclasses_json.utils import _handle_undefined_parameters_safe
-
-from cato_server.mappers.abstract_class_mapper import AbstractClassMapper
 from cato_server.mappers.mapper_registry import MapperRegistry
 
 T = TypeVar("T")
 
 
-class GenericClassMapper(AbstractClassMapper[T]):
+class GenericClassMapper:
     def __init__(self, mapper_registry: MapperRegistry):
         self._mapper_registry = mapper_registry
 
-    def map_from_dict(self, json_data: Dict) -> T:
-        pass
+    def map_from_dict(self, json_data: Dict, cls: Type[T]) -> T:
+        return self._from_dict(json_data, cls)
 
     def map_to_dict(self, obj: T) -> Dict:
         return self._to_dict(obj)
@@ -48,3 +37,32 @@ class GenericClassMapper(AbstractClassMapper[T]):
             return [self._to_dict(o) for o in obj]
         else:
             return copy.deepcopy(obj)
+
+    def _from_dict(self, json_data, cls):
+        type_hint_args = getattr(cls, "__args__", ())
+        is_optional = None.__class__ in type_hint_args
+        if is_optional:
+            if json_data is None:
+                return None
+            cls = type_hint_args[0]
+        else:
+            if json_data is None:
+                raise KeyError(f"Class {cls} is not optional, but not provided")
+        if self._mapper_registry.class_mapper_for_cls(cls):
+            mapper = self._mapper_registry.class_mapper_for_cls(cls)
+            return mapper.map_from_dict(json_data)
+        elif self._mapper_registry.value_mapper_for_cls(cls):
+            mapper = self._mapper_registry.value_mapper_for_cls(cls)
+            return mapper.map_from(json_data)
+        elif is_dataclass(cls):
+            args_dict = {}
+            for field in fields(cls):
+                args_dict[field.name] = self._from_dict(
+                    json_data.get(field.name), field.type
+                )
+            return cls(**args_dict)
+        elif isinstance(json_data, list):
+            collection_type = cls.__args__[0]
+            return [self._from_dict(o, collection_type) for o in json_data]
+        else:
+            return copy.deepcopy(json_data)
--- tests/unittests/cato_server/mappers/test_generic_mapper.py
+++ tests/unittests/cato_server/mappers/test_generic_mapper.py
@@ -1,6 +1,8 @@
 import datetime
 from dataclasses import dataclass
-from typing import List, Dict
+from typing import List, Dict, Optional
+
+import pytest
 
 from cato_server.mappers.abstract_class_mapper import AbstractClassMapper, T
 from cato_server.mappers.generic_class_mapper import GenericClassMapper
@@ -8,12 +10,60 @@ from cato_server.mappers.internal.datetime_value_mapper import DateTimeValueMapp
 from cato_server.mappers.mapper_registry import MapperRegistry
 
 
+@dataclass
+class SimpleDataClass:
+    my_int: int
+    my_string: str
+
+
+@dataclass
+class SimpleDataClassWithStringList:
+    my_int: int
+    my_string_list: List[str]
+
+
+@dataclass
+class ListedClass:
+    my_int: int
+    my_string: str
+
+
+@dataclass
+class SimpleDataClassWithListedClassList:
+    my_int: int
+    my_list: List[ListedClass]
+
+
+@dataclass
+class DataClassWithDatetime:
+    my_int: int
+    my_datetime: datetime.datetime
+
+
+@dataclass
+class NestedClass:
+    my_int: int
+    my_string: str
+
+
+@dataclass
+class DataClassWithNestedClass:
+    my_nested_class: NestedClass
+
+
+@dataclass
+class DataClassWithNestedOptionalClass:
+    my_nested_class: Optional[NestedClass]
+
+
+@dataclass
+class DataClassWithOptionalNestedClass:
+    my_string: str
+    my_nested_class: Optional[NestedClass]
+
+
 class TestMapToDict:
     def test_map_simple_dataclass(self):
-        @dataclass
-        class SimpleDataClass:
-            my_int: int
-            my_string: str
 
         result = GenericClassMapper(MapperRegistry()).map_to_dict(
             SimpleDataClass(my_int=1, my_string="my_string")
@@ -22,30 +72,19 @@ class TestMapToDict:
         assert result == {"my_int": 1, "my_string": "my_string"}
 
     def test_map_dataclass_with_simple_list(self):
-        @dataclass
-        class SimpleDataClass:
-            my_int: int
-            my_string_list: List[str]
 
         result = GenericClassMapper(MapperRegistry()).map_to_dict(
-            SimpleDataClass(my_int=1, my_string_list=["my", "string", "list"])
+            SimpleDataClassWithStringList(
+                my_int=1, my_string_list=["my", "string", "list"]
+            )
         )
 
         assert result == {"my_int": 1, "my_string_list": ["my", "string", "list"]}
 
     def test_map_dataclass_with_list_of_dataclasses(self):
-        @dataclass
-        class ListedClass:
-            my_int: int
-            my_string: str
-
-        @dataclass
-        class SimpleDataClass:
-            my_int: int
-            my_list: List[ListedClass]
 
         result = GenericClassMapper(MapperRegistry()).map_to_dict(
-            SimpleDataClass(
+            SimpleDataClassWithListedClassList(
                 my_int=1,
                 my_list=[
                     ListedClass(my_int=1, my_string="my_string1"),
@@ -65,10 +104,6 @@ class TestMapToDict:
         }
 
     def test_map_dataclass_with_custom_value_mapper_for_datetime(self):
-        @dataclass
-        class DataClassWithDatetime:
-            my_int: int
-            my_datetime: datetime.datetime
 
         registry = MapperRegistry()
         registry.register_value_mapper(datetime.datetime, DateTimeValueMapper())
@@ -83,15 +118,6 @@ class TestMapToDict:
         assert result == {"my_datetime": "2021-07-31T09:31:00", "my_int": 1}
 
     def test_map_dataclass_with_custom_class_mapper_for_field(self):
-        @dataclass
-        class NestedClass:
-            my_int: int
-            my_string: str
-
-        @dataclass
-        class DataClassWithNestedClass:
-            my_nested_class: NestedClass
-
         class CustomMapper(AbstractClassMapper[NestedClass]):
             def map_to_dict(self, test_result: NestedClass) -> Dict:
                 return {"my_int": 42, "my_string": test_result.my_string}
@@ -109,34 +135,271 @@ class TestMapToDict:
         assert result == {"my_nested_class": {"my_int": 42, "my_string": "my_test"}}
 
     def test_map_dataclass_with_custom_mapper_for_dataclass(self):
+        class CustomMapper(AbstractClassMapper[SimpleDataClass]):
+            def map_to_dict(self, test_result: SimpleDataClass) -> Dict:
+                return {"my_int": 42, "my_string": test_result.my_string}
+
+        registry = MapperRegistry()
+        registry.register_class_mapper(SimpleDataClass, CustomMapper())
+        mapper = GenericClassMapper(registry)
+
+        result = mapper.map_to_dict(SimpleDataClass(my_int=1, my_string="my_test"))
+
+        assert result == {"my_int": 42, "my_string": "my_test"}
+
+
+class TestMapFromDict:
+    def test_simple_class(self):
+
+        result = GenericClassMapper(MapperRegistry()).map_from_dict(
+            {"my_int": 1, "my_string": "my_string"}, SimpleDataClass
+        )
+
+        assert result == SimpleDataClass(my_int=1, my_string="my_string")
+
+    def test_simple_class_with_simple_list(self):
+
+        result = GenericClassMapper(MapperRegistry()).map_from_dict(
+            {"my_int": 1, "my_string_list": ["my", "test", "list"]},
+            SimpleDataClassWithStringList,
+        )
+
+        assert result == SimpleDataClassWithStringList(
+            my_int=1, my_string_list=["my", "test", "list"]
+        )
+
+    def test_simple_class_with_dataclass_list(self):
+
+        result = GenericClassMapper(MapperRegistry()).map_from_dict(
+            {
+                "my_int": 1,
+                "my_list": [
+                    {"my_int": 1, "my_string": "my1"},
+                    {"my_int": 2, "my_string": "my2"},
+                    {"my_int": 3, "my_string": "my3"},
+                ],
+            },
+            SimpleDataClassWithListedClassList,
+        )
+
+        assert result == SimpleDataClassWithListedClassList(
+            my_int=1,
+            my_list=[
+                ListedClass(my_int=1, my_string="my1"),
+                ListedClass(my_int=2, my_string="my2"),
+                ListedClass(my_int=3, my_string="my3"),
+            ],
+        )
+
+    def test_simple_dataclass_with_nested_dataclass(self):
+
+        result = GenericClassMapper(MapperRegistry()).map_from_dict(
+            {
+                "my_nested_class": {"my_int": 1, "my_string": "test"},
+            },
+            DataClassWithNestedClass,
+        )
+        assert result == DataClassWithNestedClass(
+            my_nested_class=NestedClass(my_int=1, my_string="test")
+        )
+
+    def test_map_dataclass_with_custom_class_mapper_for_field(self):
+        class CustomMapper(AbstractClassMapper[NestedClass]):
+            def map_from_dict(self, json_data: Dict) -> T:
+                return NestedClass(my_int=42, my_string=json_data["my_string"])
+
+        registry = MapperRegistry()
+        registry.register_class_mapper(NestedClass, CustomMapper())
+        mapper = GenericClassMapper(registry)
+
+        result = mapper.map_from_dict(
+            {"my_nested_class": {"my_int": 1, "my_string": "my_test"}},
+            DataClassWithNestedClass,
+        )
+
+        assert result == DataClassWithNestedClass(
+            my_nested_class=NestedClass(my_int=42, my_string="my_test")
+        )
+
+    def test_map_dataclass_with_custom_mapper(self):
+        class CustomMapper(AbstractClassMapper[SimpleDataClass]):
+            def map_from_dict(self, json_data: Dict) -> T:
+                return SimpleDataClass(my_int=42, my_string=json_data["my_string"])
+
+        registry = MapperRegistry()
+        registry.register_class_mapper(SimpleDataClass, CustomMapper())
+        mapper = GenericClassMapper(registry)
+
+        result = mapper.map_from_dict(
+            {"my_int": 1, "my_string": "my_test"}, SimpleDataClass
+        )
+
+        assert result == SimpleDataClass(my_int=42, my_string="my_test")
+
+    def test_map_dataclass_with_custom_mapper_for_value(self):
+
+        registry = MapperRegistry()
+        registry.register_value_mapper(datetime.datetime, DateTimeValueMapper())
+        mapper = GenericClassMapper(registry)
+
+        result = mapper.map_from_dict(
+            {"my_datetime": "2021-07-31T09:31:00", "my_int": 1}, DataClassWithDatetime
+        )
+
+        assert result == DataClassWithDatetime(
+            my_int=1, my_datetime=datetime.datetime(2021, 7, 31, 9, 31)
+        )
+
+    @pytest.mark.parametrize("json_value,parsed_value", [(None, None), (0, 0), (1, 1)])
+    def test_map_simple_class_with_optional_int_field(self, json_value, parsed_value):
+        @dataclass
+        class SimpleDataClassWithOptionalField:
+            my_optional: Optional[int]
+
+        result = GenericClassMapper(MapperRegistry()).map_from_dict(
+            {"my_optional": json_value}, SimpleDataClassWithOptionalField
+        )
+
+        assert result == SimpleDataClassWithOptionalField(parsed_value)
+
+    @pytest.mark.parametrize(
+        "json_value,parsed_value", [(None, None), ("", ""), ("1", "1")]
+    )
+    def test_map_simple_class_with_optional_str_field(self, json_value, parsed_value):
+        @dataclass
+        class SimpleDataClassWithOptionalField:
+            my_optional: Optional[str]
+
+        result = GenericClassMapper(MapperRegistry()).map_from_dict(
+            {"my_optional": json_value}, SimpleDataClassWithOptionalField
+        )
+
+        assert result == SimpleDataClassWithOptionalField(parsed_value)
+
+    def test_map_dataclass_nested_optional_class(self):
+
+        result = GenericClassMapper(MapperRegistry()).map_from_dict(
+            {},
+            DataClassWithNestedOptionalClass,
+        )
+        assert result == DataClassWithNestedOptionalClass(my_nested_class=None)
+
+    def test_map_dataclass_nested_optional_class_and_other_value(self):
+
+        result = GenericClassMapper(MapperRegistry()).map_from_dict(
+            {"my_string": "test"},
+            DataClassWithOptionalNestedClass,
+        )
+        assert result == DataClassWithOptionalNestedClass(
+            my_nested_class=None, my_string="test"
+        )
+
+    def test_map_dataclass_nested_optional_class_with_custom_mapper(self):
         @dataclass
         class ClassWithCustomMapper:
             my_int: int
-            my_string: str
+            my_nested_class: Optional[NestedClass]
 
         class CustomMapper(AbstractClassMapper[ClassWithCustomMapper]):
-            def map_to_dict(self, test_result: ClassWithCustomMapper) -> Dict:
-                return {"my_int": 42, "my_string": test_result.my_string}
+            def map_from_dict(self, json_data: Dict) -> T:
+                return ClassWithCustomMapper(my_int=42, my_nested_class=None)
 
         registry = MapperRegistry()
         registry.register_class_mapper(ClassWithCustomMapper, CustomMapper())
         mapper = GenericClassMapper(registry)
 
-        result = mapper.map_to_dict(
-            ClassWithCustomMapper(my_int=1, my_string="my_test")
+        result = mapper.map_from_dict(
+            {
+                "my_int": 1,
+            },
+            ClassWithCustomMapper,
         )
 
-        assert result == {"my_int": 42, "my_string": "my_test"}
+        assert result == ClassWithCustomMapper(my_int=42, my_nested_class=None)
 
+    def test_map_dataclass_with_custom_class_mapper_for_optional_field(self):
+        @dataclass
+        class DataClassWithDatetime:
+            my_int: int
+            my_datetime: Optional[datetime.datetime]
 
-# dataclass
-# dataclass with simple list
-# dataclass with list of dataclasses
-# dataclass with nested dataclass
-# dataclass with nested dataclass and custom mapper for nested
-# class with custom mapper
-# dataclass with optional simple field
-# dataclass with optional nested class
-# dataclass with optional nested class with custom mapper
-# dataclass with nested class with nested optional field
-# dataclass with list of optional values
+        registry = MapperRegistry()
+        registry.register_value_mapper(datetime.datetime, DateTimeValueMapper())
+        mapper = GenericClassMapper(registry)
+
+        result = mapper.map_from_dict({"my_int": 1}, DataClassWithDatetime)
+
+        assert result == DataClassWithDatetime(my_int=1, my_datetime=None)
+
+    def test_map_dataclass_nested_class_with_optional_field(self):
+        @dataclass
+        class NestedClass:
+            my_int: int
+            my_string: Optional[str]
+
+        @dataclass
+        class ClassWithCustomMapper:
+            my_int: int
+            my_nested_class: NestedClass
+
+        mapper = GenericClassMapper(MapperRegistry())
+
+        result = mapper.map_from_dict(
+            {"my_int": 1, "my_nested_class": {"my_int": 2}}, ClassWithCustomMapper
+        )
+
+        assert result == ClassWithCustomMapper(
+            my_int=1, my_nested_class=NestedClass(my_int=2, my_string=None)
+        )
+
+    def test_map_with_list_of_optional_values(self):
+        @dataclass
+        class ClassWithListOfOptionals:
+            my_list: List[Optional[int]]
+
+        mapper = GenericClassMapper(MapperRegistry())
+
+        result = mapper.map_from_dict(
+            {"my_list": [1, 0, 2, 3, None]}, ClassWithListOfOptionals
+        )
+
+        assert result == ClassWithListOfOptionals(my_list=[1, 0, 2, 3, None])
+
+    def test_map_with_list_of_optional_nested_class(self):
+        @dataclass
+        class ClassWithListOfOptionals:
+            my_list: List[Optional[NestedClass]]
+
+        mapper = GenericClassMapper(MapperRegistry())
+
+        result = mapper.map_from_dict(
+            {
+                "my_list": [
+                    {"my_int": 1, "my_string": "2"},
+                    None,
+                    {"my_int": 2, "my_string": "3"},
+                ]
+            },
+            ClassWithListOfOptionals,
+        )
+
+        assert result == ClassWithListOfOptionals(
+            my_list=[
+                NestedClass(my_int=1, my_string="2"),
+                None,
+                NestedClass(my_int=2, my_string="3"),
+            ]
+        )
+
+    def test_map_non_optional_from_none(self):
+
+        with pytest.raises(KeyError):
+            GenericClassMapper(MapperRegistry()).map_from_dict(None, SimpleDataClass)
+
+    def test_simple_dataclass_with_nested_dataclass_not_present(self):
+
+        with pytest.raises(KeyError):
+            GenericClassMapper(MapperRegistry()).map_from_dict(
+                {},
+                DataClassWithNestedClass,
+            )
