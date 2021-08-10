ebed873dfc6edb3110e7bd38a955e0d2f59afa8d
Jan Honsbrok <jan.honsbrok@gmail.com>
2021-08-10 16:43:14 +0200

    add "back to project" link

--- frontend/src/pages/RunPages/internal/SideBar.tsx
+++ frontend/src/pages/RunPages/internal/SideBar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./SideBar.module.scss";
 import { Link } from "react-router-dom";
 import { CurrentPage } from "./CurrentPage";
-
+import { ChevronLeft } from "react-bootstrap-icons";
 interface Props {
   currentPage: CurrentPage;
   projectId: number;
@@ -15,8 +15,14 @@ function SideBar(props: Props) {
       className={
         "d-flex flex-column flex-shrink-0 p-3 text-white " + styles.sideMenu
       }
+      id={"sidebar"}
     >
       <ul className={`nav nav-pills flex-column mb-aut o`}>
+        <li>
+          <Link to={`/projects/${props.projectId}`}>
+            <ChevronLeft style={{ marginTop: "-3px" }} /> Back to Project
+          </Link>
+        </li>
         <li
           className={
             props.currentPage === CurrentPage.OVERVIEW ? styles.active : ""
