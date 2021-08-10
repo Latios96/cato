f69634b49b1b76b1aefd2b74fafb3dfc7427ed05
Jan Honsbrok <jan.honsbrok@gmail.com>
2021-08-10 16:11:06 +0200

    push page to url

--- frontend/src/pages/ProjectPage/internal/RunList.tsx
+++ frontend/src/pages/ProjectPage/internal/RunList.tsx
@@ -6,12 +6,23 @@ import {
 import { RunDto } from "../../../catoapimodels";
 import RunListImplementation from "./RunListImplementation";
 import { useReFetch } from "../../../hooks/useReFetch";
+import { useHistory } from "react-router-dom";
+import {
+  fromQueryString,
+  toQueryString,
+} from "../../../components/Pagination/pageQueryStringUtils";
 interface Props {
   projectId: number;
 }
 
 function RunList(props: Props) {
-  const [currentPage, setCurrentPage] = useState(requestFirstPageOfSize(25));
+  const history = useHistory();
+  const page = fromQueryString(
+    history.location.search.substring(1),
+    requestFirstPageOfSize(25)
+  );
+
+  const [currentPage, setCurrentPage] = useState(page);
   const { loading, error, data } = useReFetch<Page<RunDto>>(
     `/api/v1/runs/project/${props.projectId}?page_number=${currentPage.page_number}&page_size=${currentPage.page_size}`,
     5000,
@@ -26,6 +37,7 @@ function RunList(props: Props) {
       error={error}
       pageChangedCallback={(page) => {
         setCurrentPage(page);
+        history.push({ search: "?" + toQueryString(page) });
       }}
     />
   );
