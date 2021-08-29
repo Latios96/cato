5315a0054a8cec2acaa08a019d492596d4073aa0
Jan Honsbrok <jan.honsbrok@gmail.com>
2021-08-29 12:53:23 +0200

    remove todos

--- cato_server/api/filter_option_utils.py
+++ cato_server/api/filter_option_utils.py
@@ -9,7 +9,5 @@ from cato_server.storage.abstract.test_result_filter_options import (
 def result_filter_options_from_request(
     request_args: ImmutableMultiDict,
 ) -> TestResultFilterOptions:
-    status_filter_string = request_args.get(
-        "statusFilter", StatusFilter.NONE
-    )  # todo instead of None make this optional
+    status_filter_string = request_args.get("statusFilter", StatusFilter.NONE)
     return TestResultFilterOptions(status=StatusFilter(status_filter_string))
--- cato_server/storage/abstract/test_result_filter_options.py
+++ cato_server/storage/abstract/test_result_filter_options.py
@@ -3,7 +3,7 @@ from enum import Enum
 
 
 class StatusFilter(Enum):
-    NONE = "NONE"  # todo remove None
+    NONE = "NONE"
     NOT_STARTED = "NOT_STARTED"
     RUNNING = "RUNNING"
     FAILED = "FAILED"
@@ -12,7 +12,4 @@ class StatusFilter(Enum):
 
 @dataclass
 class TestResultFilterOptions:
-    status: StatusFilter  # todo instead of None make this optional
-
-
-# todo implement utils on the typescript site for this
+    status: StatusFilter
