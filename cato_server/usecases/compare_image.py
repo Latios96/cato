24c783830d6426f1071599807e4119eac4f8e440
Jan Honsbrok <jan.honsbrok@gmail.com>
2021-09-20 21:36:41 +0200

    compare_image: compare images from db

--- cato_server/usecases/compare_image.py
+++ cato_server/usecases/compare_image.py
@@ -11,6 +11,7 @@ from cato_common.domain.compare_image_result import CompareImageResult
 from cato_common.domain.image import Image
 from cato_server.images.advanced_image_comparator import AdvancedImageComparator
 from cato_server.images.store_image import StoreImage
+from cato_server.storage.abstract.abstract_file_storage import AbstractFileStorage
 
 logger = logging.getLogger(__name__)
 
@@ -20,9 +21,11 @@ class CompareImage:
         self,
         store_image: StoreImage,
         advanced_image_comparator: AdvancedImageComparator,
+        file_storage: AbstractFileStorage,
     ):
         self._store_image = store_image
         self._advanced_image_comparator = advanced_image_comparator
+        self._file_storage = file_storage
 
     def compare_image(
         self,
@@ -57,15 +60,70 @@ class CompareImage:
             error=result.error,
         )
 
+    def compare_image_from_db(
+        self,
+        output_image: Image,
+        reference_image: Image,
+        comparison_settings: ComparisonSettings,
+    ) -> CompareImageResult:
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            original_output_image_file = self._file_storage.find_by_id(
+                output_image.original_file_id
+            )
+            if not original_output_image_file:
+                raise ValueError(
+                    f"Can not compare images, found no file for original image file id {output_image.original_file_id}"
+                )
+            output_image_path = self._store_image_on_disk(
+                tmpdirname,
+                self._file_storage.get_read_stream(original_output_image_file),
+                output_image.name,
+            )
+
+            reference_image_image_file = self._file_storage.find_by_id(
+                reference_image.original_file_id
+            )
+            if not reference_image_image_file:
+                raise ValueError(
+                    f"Can not compare images, found no file for original image file id {reference_image.original_file_id}"
+                )
+
+            reference_image_path = self._store_image_on_disk(
+                tmpdirname,
+                self._file_storage.get_read_stream(reference_image_image_file),
+                reference_image.name,
+            )
+
+            result = self._advanced_image_comparator.compare(
+                reference_image_path, output_image_path, comparison_settings, tmpdirname
+            )
+
+            stored_diff_image = self._store_image.store_image(result.diff_image)
+
+            logger.debug("Deleting tmpdir %s", tmpdirname)
+
+        return CompareImageResult(
+            status=result.status,
+            message=result.message,
+            reference_image_id=reference_image.id,
+            output_image_id=output_image.id,
+            diff_image_id=stored_diff_image.id,
+            error=result.error,
+        )
+
     def _store_image_in_db(
         self, tmpdirname: str, image_file: IO, image_name: str
     ) -> Tuple[Image, str]:
+        tmp_path = self._store_image_on_disk(tmpdirname, image_file, image_name)
+        image = self._store_image.store_image(tmp_path)
+
+        return image, tmp_path
+
+    def _store_image_on_disk(self, tmpdirname, image_file, image_name):
         tmp_path = Path(tmpdirname) / str(uuid.uuid4()) / image_name
         if not os.path.exists(tmp_path.parent):
             os.makedirs(tmp_path.parent)
         with open(tmp_path, "wb") as tmp:
             shutil.copyfileobj(image_file, tmp)
             logger.debug("Wrote image to temporary file")
-        image = self._store_image.store_image(str(tmp_path))
-
-        return image, str(tmp_path)
+        return str(tmp_path)
--- tests/unittests/cato_server/usecases/test_compare_image.py
+++ tests/unittests/cato_server/usecases/test_compare_image.py
@@ -1,12 +1,16 @@
 from io import BytesIO
 
+import pytest
+
 from cato.domain.test_status import TestStatus
 from cato.domain.comparison_method import ComparisonMethod
 from cato.domain.comparison_result import ComparisonResult
 from cato.domain.comparison_settings import ComparisonSettings
+from cato_common.domain.file import File
 from cato_common.domain.image import Image
 from cato_server.images.advanced_image_comparator import AdvancedImageComparator
 from cato_server.images.store_image import StoreImage
+from cato_server.storage.abstract.abstract_file_storage import AbstractFileStorage
 from cato_server.usecases.compare_image import CompareImage
 from cato_common.domain.compare_image_result import CompareImageResult
 from tests.utils import mock_safe
@@ -29,6 +33,7 @@ class TestCompareImage:
         self.mock_store_image = mock_safe(StoreImage)
         self.mock_store_image.store_image.side_effect = self._mocked_store_image
         self.mock_image_comparator = mock_safe(AdvancedImageComparator)
+        self.mock_file_storage = mock_safe(AbstractFileStorage)
         self.comparison_result = ComparisonResult(
             status=TestStatus.SUCCESS, message="", diff_image="some-path", error=1
         )
@@ -36,8 +41,14 @@ class TestCompareImage:
             method=ComparisonMethod.SSIM, threshold=1
         )
         self.mock_image_comparator.compare.return_value = self.comparison_result
+        self.mock_file_storage.find_by_id.side_effect = lambda x: File(
+            id=x, name=f"{x}.png", hash="1", value_counter=0
+        )
+        self.mock_file_storage.get_read_stream.side_effect = lambda x: BytesIO(
+            b"output image" if x == 1 else b"reference image"
+        )
         self.compare_image = CompareImage(
-            self.mock_store_image, self.mock_image_comparator
+            self.mock_store_image, self.mock_image_comparator, self.mock_file_storage
         )
 
     def test_compare_images_successfully(self):
@@ -58,6 +69,55 @@ class TestCompareImage:
             error=1,
         )
         assert self.mock_store_image.store_image.call_count == 3
+        assert self.mock_image_comparator.compare.call_args[0][0].endswith(
+            "reference.png"
+        )
+        assert self.mock_image_comparator.compare.call_args[0][1].endswith("output.png")
+        assert (
+            self.mock_image_comparator.compare.call_args[0][0]
+            != self.mock_image_comparator.compare.call_args[0][1]
+        )
+        assert (
+            self.mock_image_comparator.compare.call_args[0][2]
+            == self.comparison_settings
+        )
+
+    def test_compare_images_from_db_successfully(self):
+        self.mock_file_storage.find_by_id.side_effect = lambda x: File(
+            id=x, name=f"{x}.png", hash="1", value_counter=0
+        )
+        self.mock_file_storage.get_read_stream.side_effect = lambda x: BytesIO(
+            b"output image"
+        )
+        result = self.compare_image.compare_image_from_db(
+            Image(
+                id=1,
+                name="output.png",
+                original_file_id=10,
+                channels=[],
+                width=1920,
+                height=1080,
+            ),
+            Image(
+                id=2,
+                name="reference.png",
+                original_file_id=20,
+                channels=[],
+                width=1920,
+                height=1080,
+            ),
+            self.comparison_settings,
+        )
+
+        assert result == CompareImageResult(
+            status=self.comparison_result.status,
+            message=self.comparison_result.message,
+            reference_image_id=2,
+            output_image_id=1,
+            diff_image_id=1,
+            error=1,
+        )
+        assert self.mock_store_image.store_image.call_count == 1
         assert self.mock_image_comparator.compare.call_args[0][0].endswith(
             "reference.png"
         )
@@ -105,6 +165,109 @@ class TestCompareImage:
             self.mock_image_comparator.compare.call_args[0][2]
             == self.comparison_settings
         )
+
+    def test_compare_images_from_db_failure(self):
+        self.mock_image_comparator.compare.return_value = ComparisonResult(
+            status=TestStatus.FAILED, message="Failed", diff_image="some-path", error=1
+        )
+
+        result = self.compare_image.compare_image_from_db(
+            Image(
+                id=1,
+                name="output.png",
+                original_file_id=10,
+                channels=[],
+                width=1920,
+                height=1080,
+            ),
+            Image(
+                id=2,
+                name="reference.png",
+                original_file_id=20,
+                channels=[],
+                width=1920,
+                height=1080,
+            ),
+            self.comparison_settings,
+        )
+
+        assert result == CompareImageResult(
+            status=TestStatus.FAILED,
+            message="Failed",
+            reference_image_id=2,
+            output_image_id=1,
+            diff_image_id=1,
+            error=1,
+        )
+        assert self.mock_store_image.store_image.call_count == 1
+        assert self.mock_image_comparator.compare.call_args[0][0].endswith(
+            "reference.png"
+        )
+        assert self.mock_image_comparator.compare.call_args[0][1].endswith("output.png")
+        assert (
+            self.mock_image_comparator.compare.call_args[0][0]
+            != self.mock_image_comparator.compare.call_args[0][1]
+        )
+        assert (
+            self.mock_image_comparator.compare.call_args[0][2]
+            == self.comparison_settings
+        )
+
+    def test_compare_images_from_db_no_output_image(self):
+        self.mock_file_storage.find_by_id.side_effect = (
+            lambda x: File(id=x, name=f"{x}.png", hash="1", value_counter=0)
+            if x == 10
+            else None
+        )
+
+        with pytest.raises(ValueError):
+            self.compare_image.compare_image_from_db(
+                Image(
+                    id=1,
+                    name="output.png",
+                    original_file_id=10,
+                    channels=[],
+                    width=1920,
+                    height=1080,
+                ),
+                Image(
+                    id=2,
+                    name="reference.png",
+                    original_file_id=20,
+                    channels=[],
+                    width=1920,
+                    height=1080,
+                ),
+                self.comparison_settings,
+            )
+
+    def test_compare_images_from_db_no_reference_image(self):
+        self.mock_file_storage.find_by_id.side_effect = (
+            lambda x: File(id=x, name=f"{x}.png", hash="1", value_counter=0)
+            if x == 20
+            else None
+        )
+
+        with pytest.raises(ValueError):
+            self.compare_image.compare_image_from_db(
+                Image(
+                    id=1,
+                    name="output.png",
+                    original_file_id=10,
+                    channels=[],
+                    width=1920,
+                    height=1080,
+                ),
+                Image(
+                    id=2,
+                    name="reference.png",
+                    original_file_id=20,
+                    channels=[],
+                    width=1920,
+                    height=1080,
+                ),
+                self.comparison_settings,
+            )
 
     def test_compare_images_with_same_name(self):
         result = self.compare_image.compare_image(
