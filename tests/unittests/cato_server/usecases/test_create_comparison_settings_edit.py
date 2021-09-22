070d5eb1d4d662fbbd7e84e4da3a55232fe7868b
Jan Honsbrok <jan.honsbrok@gmail.com>
2021-09-22 20:49:02 +0200

    clean up test code

--- tests/unittests/cato_server/usecases/test_create_comparison_settings_edit.py
+++ tests/unittests/cato_server/usecases/test_create_comparison_settings_edit.py
@@ -1,5 +1,4 @@
 import datetime
-from unittest import mock
 
 import pytest
 
@@ -26,7 +25,7 @@ def test_create_edit_with_success(test_result_factory):
     mock_test_edit_repository = mock_safe(TestEditRepository)
     mock_test_edit_repository.save.side_effect = lambda x: x
     mock_test_result_repository = mock_safe(TestResultRepository)
-    mock_test_result_repository.find_by_id.return_value = test_result_factory(
+    test_result = test_result_factory(
         id=5,
         comparison_settings=ComparisonSettings(
             method=ComparisonMethod.SSIM, threshold=0.5
@@ -36,6 +35,7 @@ def test_create_edit_with_success(test_result_factory):
         image_output=1,
         error_value=1,
     )
+    mock_test_result_repository.find_by_id.return_value = test_result
     mock_compare_image = mock_safe(CompareImage)
     mock_compare_image.compare_image_from_db.return_value = CompareImageResult(
         status=TestStatus.SUCCESS,
@@ -85,6 +85,10 @@ def test_create_edit_with_success(test_result_factory):
     mock_compare_image.compare_image_from_db.assert_called_with(
         _create_image(1), _create_image(2), edit_comparison_settings
     )
+    mock_test_result_repository.save.assert_called_with(test_result)
+    assert test_result.diff_image == 13
+    assert test_result.status == TestStatus.SUCCESS
+    assert test_result.message == "still success"
 
 
 def _create_image(x):
@@ -118,9 +122,9 @@ def test_no_test_with_id_found():
 
 
 def test_no_reference_image(test_result_factory):
-    test_edit_repository = mock_safe(TestEditRepository)
-    test_result_repository = mock_safe(TestResultRepository)
-    test_result_repository.find_by_id.return_value = test_result_factory(
+    mock_test_edit_repository = mock_safe(TestEditRepository)
+    mock_test_result_repository = mock_safe(TestResultRepository)
+    mock_test_result_repository.find_by_id.return_value = test_result_factory(
         reference_image=1,
         image_output=2,
         comparison_settings=ComparisonSettings(
@@ -131,8 +135,8 @@ def test_no_reference_image(test_result_factory):
     mock_image_repository = mock_safe(ImageRepository)
     mock_image_repository.find_by_id.side_effect = lambda x: "image" if x == 1 else None
     create_comparison_settings_edit = CreateComparisonSettingsEdit(
-        test_edit_repository,
-        test_result_repository,
+        mock_test_edit_repository,
+        mock_test_result_repository,
         mock_compare_image,
         mock_image_repository,
     )
@@ -141,12 +145,13 @@ def test_no_reference_image(test_result_factory):
         create_comparison_settings_edit.create_edit(
             1, ComparisonSettings(method=ComparisonMethod.SSIM, threshold=1)
         )
+    mock_test_result_repository.save.assert_not_called()
 
 
 def test_no_output_image(test_result_factory):
-    test_edit_repository = mock_safe(TestEditRepository)
-    test_result_repository = mock_safe(TestResultRepository)
-    test_result_repository.find_by_id.return_value = test_result_factory(
+    mock_test_edit_repository = mock_safe(TestEditRepository)
+    mock_test_result_repository = mock_safe(TestResultRepository)
+    mock_test_result_repository.find_by_id.return_value = test_result_factory(
         reference_image=1,
         image_output=2,
         comparison_settings=ComparisonSettings(
@@ -157,8 +162,8 @@ def test_no_output_image(test_result_factory):
     mock_image_repository = mock_safe(ImageRepository)
     mock_image_repository.find_by_id.side_effect = lambda x: "image" if x == 2 else None
     create_comparison_settings_edit = CreateComparisonSettingsEdit(
-        test_edit_repository,
-        test_result_repository,
+        mock_test_edit_repository,
+        mock_test_result_repository,
         mock_compare_image,
         mock_image_repository,
     )
@@ -167,19 +172,21 @@ def test_no_output_image(test_result_factory):
         create_comparison_settings_edit.create_edit(
             1, ComparisonSettings(method=ComparisonMethod.SSIM, threshold=1)
         )
+    mock_test_result_repository.save.assert_not_called()
+    mock_test_edit_repository.save.assert_not_called()
 
 
 def test_test_with_no_comparison_settings(test_result_factory):
-    test_edit_repository = mock_safe(TestEditRepository)
-    test_result_repository = mock_safe(TestResultRepository)
-    test_result_repository.find_by_id.return_value = test_result_factory(
+    mock_test_edit_repository = mock_safe(TestEditRepository)
+    mock_test_result_repository = mock_safe(TestResultRepository)
+    mock_test_result_repository.find_by_id.return_value = test_result_factory(
         id=5,
     )
     mock_compare_image = mock_safe(CompareImage)
     mock_image_repository = mock_safe(ImageRepository)
     create_comparison_settings_edit = CreateComparisonSettingsEdit(
-        test_edit_repository,
-        test_result_repository,
+        mock_test_edit_repository,
+        mock_test_result_repository,
         mock_compare_image,
         mock_image_repository,
     )
@@ -188,3 +195,5 @@ def test_test_with_no_comparison_settings(test_result_factory):
         create_comparison_settings_edit.create_edit(
             1, ComparisonSettings(method=ComparisonMethod.SSIM, threshold=1)
         )
+    mock_test_result_repository.save.assert_not_called()
+    mock_test_edit_repository.save.assert_not_called()
