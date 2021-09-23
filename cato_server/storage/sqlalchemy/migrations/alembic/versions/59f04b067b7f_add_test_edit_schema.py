da766d58f36ad577a3a3f3e973bd7c2b89d7adcf
Jan Honsbrok <jan.honsbrok@gmail.com>
2021-09-23 20:18:18 +0200

    fix migration

--- cato_server/storage/sqlalchemy/migrations/alembic/versions/59f04b067b7f_add_test_edit_schema.py
+++ cato_server/storage/sqlalchemy/migrations/alembic/versions/59f04b067b7f_add_test_edit_schema.py
@@ -33,6 +33,24 @@ def upgrade():
         sa.Column("new_comparison_method", sa.String, nullable=False),
         sa.Column("old_threshold", sa.FLOAT, nullable=False),
         sa.Column("new_threshold", sa.FLOAT, nullable=False),
+        sa.Column("old_status", sa.String, nullable=True),
+        sa.Column("new_status", sa.String, nullable=True),
+        sa.Column("old_message", sa.String, nullable=True),
+        sa.Column("new_message", sa.String, nullable=True),
+        sa.Column(
+            "old_diff_image_id",
+            sa.Integer,
+            sa.ForeignKey("image_entity.id"),
+            nullable=True,
+        ),
+        sa.Column(
+            "new_diff_image_id",
+            sa.Integer,
+            sa.ForeignKey("image_entity.id"),
+            nullable=True,
+        ),
+        sa.Column("old_error_value", sa.Float, nullable=True),
+        sa.Column("new_error_value", sa.Float, nullable=True),
     )
     op.create_table(
         "reference_image_edit_entity",
