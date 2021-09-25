c5d4e1eb66d27d33c1c2a17cce312c13a36ac54c
Jan Honsbrok <jan.honsbrok@gmail.com>
2021-09-25 13:24:07 +0200

    add create reference image edit to api

--- cato_server/api/test_edit_blueprint.py
+++ cato_server/api/test_edit_blueprint.py
@@ -8,12 +8,14 @@ from cato.domain.comparison_settings import ComparisonSettings
 from cato_common.mappers.object_mapper import ObjectMapper
 from cato_server.api.validators.create_test_edits_validators import (
     CreateComparisonSettingsEditValidator,
+    CreateReferenceImageEditValidator,
 )
 from cato_server.storage.abstract.test_edit_repository import TestEditRepository
 from cato_server.storage.abstract.test_result_repository import TestResultRepository
 from cato_server.usecases.create_comparison_settings_edit import (
     CreateComparisonSettingsEdit,
 )
+from cato_server.usecases.create_reference_image_edit import CreateReferenceImageEdit
 
 
 class TestEditBlueprint(APIRouter):
@@ -23,21 +25,27 @@ class TestEditBlueprint(APIRouter):
         test_result_repository: TestResultRepository,
         object_mapper: ObjectMapper,
         create_comparison_settings_edit: CreateComparisonSettingsEdit,
+        create_reference_image_edit: CreateReferenceImageEdit,
     ):
         super(TestEditBlueprint, self).__init__()
         self._test_edit_repository = test_edit_repository
         self._object_mapper = object_mapper
         self._test_result_repository = test_result_repository
         self._create_comparison_settings_edit = create_comparison_settings_edit
+        self._create_reference_image_edit = create_reference_image_edit
 
         self.get("/test_edits/{test_result_id}")(self.test_edits_by_test_result_id)
         self.get("/test_edits/can-edit/{test_result_id}/comparison_settings")(
-            self.can_edit_test_result
+            self.can_edit_comparison_settings
+        )
+        self.get("/test_edits/can-edit/{test_result_id}/reference_image")(
+            self.can_edit_reference_image
         )
         self.get("/test_edits/runs/{run_id}")(self.test_edits_by_run_id)
         self.post("/test_edits/comparison_settings")(
             self.create_comparison_settings_edit
         )
+        self.post("/test_edits/reference_image")(self.create_reference_image_edit)
 
     def test_edits_by_test_result_id(
         self, test_result_id: int, request: Request
@@ -46,11 +54,21 @@ class TestEditBlueprint(APIRouter):
 
         return JSONResponse(content=self._object_mapper.many_to_dict(edits))
 
-    def can_edit_test_result(
+    def can_edit_comparison_settings(
         self,
         test_result_id: int,
     ) -> Response:
         can_edit, message = self._create_comparison_settings_edit.can_create_edit(
+            test_result_id
+        )
+
+        return JSONResponse(content={"can_edit": can_edit, "message": message})
+
+    def can_edit_reference_image(
+        self,
+        test_result_id: int,
+    ) -> Response:
+        can_edit, message = self._create_reference_image_edit.can_create_edit(
             test_result_id
         )
 
@@ -77,6 +95,22 @@ class TestEditBlueprint(APIRouter):
         edit = self._create_comparison_settings_edit.create_edit(
             test_result_id, comparison_settings
         )
+
+        return JSONResponse(
+            content=self._object_mapper.to_dict(edit), status_code=CREATED
+        )
+
+    async def create_reference_image_edit(self, request: Request) -> Response:
+        request_json = await request.json()
+        errors = CreateReferenceImageEditValidator(
+            self._test_result_repository
+        ).validate(request_json)
+        if errors:
+            return JSONResponse(content=errors, status_code=BAD_REQUEST)
+
+        test_result_id = request_json["test_result_id"]
+
+        edit = self._create_reference_image_edit.create_edit(test_result_id)
 
         return JSONResponse(
             content=self._object_mapper.to_dict(edit), status_code=CREATED
--- tests/unittests/cato_server/api/test_test_edit_blueprint.py
+++ tests/unittests/cato_server/api/test_test_edit_blueprint.py
@@ -2,9 +2,6 @@ import datetime
 
 from cato.domain.comparison_method import ComparisonMethod
 from cato.domain.comparison_settings import ComparisonSettings
-from cato_server.storage.sqlalchemy.sqlalchemy_test_result_repository import (
-    SqlAlchemyTestResultRepository,
-)
 
 
 def test_get_test_edits_by_test_result_id(client, test_edit, test_result):
@@ -196,3 +193,102 @@ def test_can_create_comparison_settings_edit_should_return_false(
         "can_edit": False,
         "message": "Can't edit a test result which has no comparison settings!",
     }
+
+
+def test_can_create_reference_image_edit_should_return_true(
+    client, sessionmaker_fixture, suite_result, saving_test_result_factory, stored_image
+):
+    test_result = saving_test_result_factory(
+        suite_result_id=suite_result.id,
+        reference_image=stored_image.id,
+        image_output=stored_image.id,
+        comparison_settings=ComparisonSettings(
+            method=ComparisonMethod.SSIM, threshold=0.5
+        ),
+    )
+    url = f"/api/v1/test_edits/can-edit/{test_result.id}/reference_image"
+
+    rv = client.get(url)
+
+    assert rv.status_code == 200
+    assert rv.json() == {"can_edit": True, "message": None}
+
+
+def test_can_create_reference_image_edit_should_return_false(
+    client, sessionmaker_fixture, suite_result, saving_test_result_factory
+):
+    test_result = saving_test_result_factory(
+        suite_result_id=suite_result.id, image_output=None
+    )
+    url = f"/api/v1/test_edits/can-edit/{test_result.id}/reference_image"
+
+    rv = client.get(url)
+
+    assert rv.status_code == 200
+    assert rv.json() == {
+        "can_edit": False,
+        "message": "Can't edit a test result which has no image_output!",
+    }
+
+
+def test_create_reference_image_edit_success(
+    client,
+    suite_result,
+    saving_test_result_factory,
+    stored_image_factory,
+):
+    test_result = saving_test_result_factory(
+        comparison_settings=ComparisonSettings(
+            method=ComparisonMethod.SSIM, threshold=1
+        ),
+        suite_result_id=suite_result.id,
+        image_output=stored_image_factory().id,
+        reference_image=stored_image_factory().id,
+    )
+    url = f"/api/v1/test_edits/reference_image"
+
+    rv = client.post(
+        url,
+        json={"test_result_id": test_result.id},
+    )
+
+    assert rv.status_code == 201
+    json = rv.json()
+    now = datetime.datetime.now()
+    json["created_at"] = now
+    assert json == {
+        "created_at": now,
+        "edit_type": "REFERENCE_IMAGE",
+        "id": 1,
+        "new_value": {
+            "diff_image_id": 3,
+            "error_value": 1.0,
+            "message": None,
+            "reference_image_id": 1,
+            "status": "SUCCESS",
+        },
+        "old_value": {
+            "diff_image_id": None,
+            "error_value": None,
+            "message": "success",
+            "reference_image_id": 2,
+            "status": "SUCCESS",
+        },
+        "test_id": 1,
+    }
+
+
+def test_create_reference_image_edit_failure(
+    client,
+):
+    url = f"/api/v1/test_edits/reference_image"
+
+    rv = client.post(
+        url,
+        json={
+            "test_result_id": 1,
+        },
+    )
+
+    assert rv.status_code == 400
+    assert rv.json() == {"id": ["No TestResult with id 1 exists!"]}
