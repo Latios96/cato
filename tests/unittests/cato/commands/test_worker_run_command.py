7f3af1a430fd7f83260e005f37e7b1679488ac86
Jan Honsbrok <jan.honsbrok@gmail.com>
2021-11-06 15:41:28 +0100

    refactor: remove setup_method

--- tests/unittests/cato/commands/test_worker_run_command.py
+++ tests/unittests/cato/commands/test_worker_run_command.py
@@ -26,43 +26,51 @@ def submission_info_fixture(config_fixture):
     )
 
 
-class TestWorkerRunCommand:
-    def setup_method(self, method):
-        self.mock_test_execution_reporter = mock_safe(TestExecutionReporter)
-        self.mock_test_runner = mock_safe(TestRunner)
-        self.mock_reporter = mock_safe(Reporter)
-        self.mock_logger = mock_safe(logging.Logger)
-        self.cato_api_client = mock_safe(CatoApiClient)
-        self.worker_command = WorkerRunCommand(
-            self.mock_test_execution_reporter,
-            self.mock_test_runner,
-            self.mock_reporter,
-            self.mock_logger,
-            self.cato_api_client,
-        )
+@pytest.fixture
+def text_context():
+    class TestContext:
+        def __init__(self):
+            self.mock_test_execution_reporter = mock_safe(TestExecutionReporter)
+            self.mock_test_runner = mock_safe(TestRunner)
+            self.mock_reporter = mock_safe(Reporter)
+            self.mock_logger = mock_safe(logging.Logger)
+            self.cato_api_client = mock_safe(CatoApiClient)
+            self.worker_command = WorkerRunCommand(
+                self.mock_test_execution_reporter,
+                self.mock_test_runner,
+                self.mock_reporter,
+                self.mock_logger,
+                self.cato_api_client,
+            )
 
+    return TestContext()
+
+
+class TestWorkerRunCommand:
     def test_not_matching_identifier_for_suite_should_raise(
-        self, submission_info_fixture
+        self, submission_info_fixture, text_context
     ):
-        self.cato_api_client.get_submission_info_by_id.return_value = (
+        text_context.cato_api_client.get_submission_info_by_id.return_value = (
             submission_info_fixture
         )
 
         with pytest.raises(ValueError):
-            self.worker_command.execute(1, "test/My_first_test")
+            text_context.worker_command.execute(1, "test/My_first_test")
 
     def test_not_matching_identifier_for_test_should_raise(
-        self, submission_info_fixture
+        self, submission_info_fixture, text_context
     ):
-        self.cato_api_client.get_submission_info_by_id.return_value = (
+        text_context.cato_api_client.get_submission_info_by_id.return_value = (
             submission_info_fixture
         )
 
         with pytest.raises(ValueError):
-            self.worker_command.execute(1, "My_first_test_Suite/test")
+            text_context.worker_command.execute(1, "My_first_test_Suite/test")
 
-    def test_test_run_success(self, config_fixture, submission_info_fixture):
-        self.cato_api_client.get_submission_info_by_id.return_value = (
+    def test_test_run_success(
+        self, config_fixture, submission_info_fixture, text_context
+    ):
+        text_context.cato_api_client.get_submission_info_by_id.return_value = (
             submission_info_fixture
         )
         execution_result = TestExecutionResult(
@@ -78,20 +86,24 @@ class TestWorkerRunCommand:
             datetime.datetime.now(),
             1,
         )
-        self.mock_test_runner.run_test.return_value = execution_result
+        text_context.mock_test_runner.run_test.return_value = execution_result
 
-        self.worker_command.execute(1, "My_first_test_Suite/My_first_test")
+        text_context.worker_command.execute(1, "My_first_test_Suite/My_first_test")
 
-        self.mock_test_execution_reporter.use_run_id.assert_called_with(5)
-        self.mock_test_execution_reporter.report_test_execution_start.assert_called_with(
+        text_context.mock_test_execution_reporter.use_run_id.assert_called_with(5)
+        text_context.mock_test_execution_reporter.report_test_execution_start.assert_called_with(
             config_fixture.TEST_SUITE, config_fixture.TEST
         )
-        self.mock_reporter.set_verbose_mode.assert_called_with(VerboseMode.VERY_VERBOSE)
-        self.mock_test_runner.run_test.assert_called_once()
-        self.mock_reporter.report_test_success.assert_called_once()
+        text_context.mock_reporter.set_verbose_mode.assert_called_with(
+            VerboseMode.VERY_VERBOSE
+        )
+        text_context.mock_test_runner.run_test.assert_called_once()
+        text_context.mock_reporter.report_test_success.assert_called_once()
 
-    def test_test_run_failure(self, config_fixture, submission_info_fixture):
-        self.cato_api_client.get_submission_info_by_id.return_value = (
+    def test_test_run_failure(
+        self, config_fixture, submission_info_fixture, text_context
+    ):
+        text_context.cato_api_client.get_submission_info_by_id.return_value = (
             submission_info_fixture
         )
         execution_result = TestExecutionResult(
@@ -107,13 +119,13 @@ class TestWorkerRunCommand:
             datetime.datetime.now(),
             1,
         )
-        self.mock_test_runner.run_test.return_value = execution_result
+        text_context.mock_test_runner.run_test.return_value = execution_result
 
-        self.worker_command.execute(1, "My_first_test_Suite/My_first_test")
+        text_context.worker_command.execute(1, "My_first_test_Suite/My_first_test")
 
-        self.mock_test_execution_reporter.use_run_id.assert_called_with(5)
-        self.mock_test_execution_reporter.report_test_execution_start.assert_called_with(
+        text_context.mock_test_execution_reporter.use_run_id.assert_called_with(5)
+        text_context.mock_test_execution_reporter.report_test_execution_start.assert_called_with(
             config_fixture.TEST_SUITE, config_fixture.TEST
         )
-        self.mock_test_runner.run_test.assert_called_once()
-        self.mock_reporter.report_test_failure.assert_called_once()
+        text_context.mock_test_runner.run_test.assert_called_once()
+        text_context.mock_reporter.report_test_failure.assert_called_once()
