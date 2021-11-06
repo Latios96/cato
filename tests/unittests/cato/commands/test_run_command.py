38d7bf6b2407b727cc8aff7826a1aded70318c25
Jan Honsbrok <jan.honsbrok@gmail.com>
2021-11-06 15:52:22 +0100

    refactor: remove setup_method

--- tests/unittests/cato/commands/test_run_command.py
+++ tests/unittests/cato/commands/test_run_command.py
@@ -50,33 +50,39 @@ CONFIG = RunConfig(
 )
 
 
-class TestRunCommand:
-    def setup_method(self, method):
-        self.config = copy.deepcopy(CONFIG)
-        self.mock_json_config_parser = mock_safe(JsonConfigParser)
-        self.mock_test_suite_runner = mock_safe(TestSuiteRunner)
-        self.mock_timing_report_generator = mock_safe(TimingReportGenerator)
-        self.mock_end_message_generator = mock_safe(EndMessageGenerator)
-        self.mock_logger = mock_safe(logging.Logger)
-        self.mock_reporter = mock_safe(Reporter)
-        self.mock_last_run_information_repository = mock_safe(
-            LastRunInformationRepository
-        )
-        self.mock_cato_api_client = mock_safe(CatoApiClient)
-        self.run_command = RunCommand(
-            self.mock_json_config_parser,
-            self.mock_test_suite_runner,
-            self.mock_timing_report_generator,
-            self.mock_end_message_generator,
-            self.mock_logger,
-            self.mock_reporter,
-            lambda x: self.mock_last_run_information_repository,
-            self.mock_cato_api_client,
-        )
-        self.mock_json_config_parser.parse.return_value = self.config
-        self.run_command._read_config = lambda x: self.config
+@pytest.fixture
+def test_context():
+    class TestContext:
+        def __init__(self):
+            self.config = copy.deepcopy(CONFIG)
+            self.mock_json_config_parser = mock_safe(JsonConfigParser)
+            self.mock_test_suite_runner = mock_safe(TestSuiteRunner)
+            self.mock_timing_report_generator = mock_safe(TimingReportGenerator)
+            self.mock_end_message_generator = mock_safe(EndMessageGenerator)
+            self.mock_logger = mock_safe(logging.Logger)
+            self.mock_reporter = mock_safe(Reporter)
+            self.mock_last_run_information_repository = mock_safe(
+                LastRunInformationRepository
+            )
+            self.mock_cato_api_client = mock_safe(CatoApiClient)
+            self.run_command = RunCommand(
+                self.mock_json_config_parser,
+                self.mock_test_suite_runner,
+                self.mock_timing_report_generator,
+                self.mock_end_message_generator,
+                self.mock_logger,
+                self.mock_reporter,
+                lambda x: self.mock_last_run_information_repository,
+                self.mock_cato_api_client,
+            )
+            self.mock_json_config_parser.parse.return_value = self.config
+            self.run_command._read_config = lambda x: self.config
 
-    def test_should_run_all_suites_and_tests(self):
+    return TestContext()
+
+
+class TestRunCommand:
+    def test_should_run_all_suites_and_tests(self, test_context):
         started_at = datetime.datetime.now()
         finished_at = datetime.datetime.now()
         result = [
@@ -100,71 +106,91 @@ class TestRunCommand:
                 ],
             )
         ]
-        self.mock_test_suite_runner.run_test_suites.return_value = result
-        self.mock_timing_report_generator.generate.return_value = "Timing report"
-        self.mock_end_message_generator.generate_end_message.return_value = (
+        test_context.mock_test_suite_runner.run_test_suites.return_value = result
+        test_context.mock_timing_report_generator.generate.return_value = (
+            "Timing report"
+        )
+        test_context.mock_end_message_generator.generate_end_message.return_value = (
             "End message"
         )
 
-        self.run_command.run("my_path", None, None, False, VerboseMode.DEFAULT)
+        test_context.run_command.run("my_path", None, None, False, VerboseMode.DEFAULT)
 
-        self.mock_test_suite_runner.run_test_suites.assert_called_with(self.config)
-        self.mock_timing_report_generator.generate.assert_called_with(result)
-        self.mock_end_message_generator.generate_end_message.assert_called_with(result)
-        self.mock_logger.info.assert_has_calls(
+        test_context.mock_test_suite_runner.run_test_suites.assert_called_with(
+            test_context.config
+        )
+        test_context.mock_timing_report_generator.generate.assert_called_with(result)
+        test_context.mock_end_message_generator.generate_end_message.assert_called_with(
+            result
+        )
+        test_context.mock_logger.info.assert_has_calls(
             [call(""), call("Timing report"), call(""), call("End message")]
         )
-        self.mock_reporter.set_verbose_mode.assert_called_with(VerboseMode.DEFAULT)
+        test_context.mock_reporter.set_verbose_mode.assert_called_with(
+            VerboseMode.DEFAULT
+        )
 
-    def test_should_filter_by_suite_name(self):
-        self.run_command.run(
+    def test_should_filter_by_suite_name(self, test_context):
+        test_context.run_command.run(
             "my_path", "not existing name", None, False, VerboseMode.DEFAULT
         )
 
-        self.mock_test_suite_runner.run_test_suites.assert_called_with(self.config)
+        test_context.mock_test_suite_runner.run_test_suites.assert_called_with(
+            test_context.config
+        )
 
-        assert self.config.suites == []
+        assert test_context.config.suites == []
 
-    def test_should_filter_by_test_identifier(self):
-        self.run_command.run(
+    def test_should_filter_by_test_identifier(self, test_context):
+        test_context.run_command.run(
             "my_path", None, "not_existing_suite/test", False, VerboseMode.DEFAULT
         )
 
-        self.mock_test_suite_runner.run_test_suites.assert_called_with(self.config)
+        test_context.mock_test_suite_runner.run_test_suites.assert_called_with(
+            test_context.config
+        )
 
-        assert self.config.suites == []
+        assert test_context.config.suites == []
 
-    def test_should_filter_by_invalid_test_identifier_str(self):
+    def test_should_filter_by_invalid_test_identifier_str(self, test_context):
         with pytest.raises(ValueError):
-            self.run_command.run(
+            test_context.run_command.run(
                 "my_path", None, "not_existing_suite", False, VerboseMode.DEFAULT
             )
 
-    def test_existing_last_run_information_should_filter_no_tests_executed(self):
-        self.mock_last_run_information_repository.read_last_run_information.return_value = LastRunInformation(
+    def test_existing_last_run_information_should_filter_no_tests_executed(
+        self, test_context
+    ):
+        test_context.mock_last_run_information_repository.read_last_run_information.return_value = LastRunInformation(
             last_run_id=2
         )
-        self.mock_cato_api_client.get_test_results_by_run_id_and_test_status.return_value = (
+        test_context.mock_cato_api_client.get_test_results_by_run_id_and_test_status.return_value = (
             []
         )
 
-        self.run_command.run("my_path", None, None, True, VerboseMode.DEFAULT)
+        test_context.run_command.run("my_path", None, None, True, VerboseMode.DEFAULT)
 
-        self.mock_test_suite_runner.run_test_suites.assert_called_with(self.config)
-        assert self.config.suites == []
+        test_context.mock_test_suite_runner.run_test_suites.assert_called_with(
+            test_context.config
+        )
+        assert test_context.config.suites == []
 
-    def test_existing_last_run_information_should_filter_one_test_executed(self):
-        self.mock_last_run_information_repository.read_last_run_information.return_value = LastRunInformation(
+    def test_existing_last_run_information_should_filter_one_test_executed(
+        self, test_context
+    ):
+        test_context.mock_last_run_information_repository.read_last_run_information.return_value = LastRunInformation(
             last_run_id=2
         )
-        self.mock_cato_api_client.get_test_results_by_run_id_and_test_status.return_value = [
+        test_context.mock_cato_api_client.get_test_results_by_run_id_and_test_status.return_value = [
             TestIdentifier.from_string("My_first_test_Suite/My_first_test")
         ]
 
-        self.run_command.run("my_path", None, None, True, VerboseMode.DEFAULT)
+        test_context.run_command.run("my_path", None, None, True, VerboseMode.DEFAULT)
 
-        self.mock_test_suite_runner.run_test_suites.assert_called_with(self.config)
-        assert self.config.suites == [
+        test_context.mock_test_suite_runner.run_test_suites.assert_called_with(
+            test_context.config
+        )
+        assert test_context.config.suites == [
             TestSuite(
                 name="My_first_test_Suite",
                 tests=[TEST],
@@ -172,15 +198,17 @@ class TestRunCommand:
             )
         ]
 
-    def test_existing_last_run_information_should_not_use(self):
-        self.mock_last_run_information_repository.read_last_run_information.return_value = LastRunInformation(
+    def test_existing_last_run_information_should_not_use(self, test_context):
+        test_context.mock_last_run_information_repository.read_last_run_information.return_value = LastRunInformation(
             last_run_id=2
         )
-        self.mock_cato_api_client.get_test_results_by_run_id_and_test_status.return_value = [
+        test_context.mock_cato_api_client.get_test_results_by_run_id_and_test_status.return_value = [
             TestIdentifier.from_string("My_first_test_Suite/My_first_test")
         ]
 
-        self.run_command.run("my_path", None, None, False, VerboseMode.DEFAULT)
+        test_context.run_command.run("my_path", None, None, False, VerboseMode.DEFAULT)
 
-        self.mock_test_suite_runner.run_test_suites.assert_called_with(self.config)
-        assert self.config.suites == CONFIG.suites
+        test_context.mock_test_suite_runner.run_test_suites.assert_called_with(
+            test_context.config
+        )
+        assert test_context.config.suites == CONFIG.suites
