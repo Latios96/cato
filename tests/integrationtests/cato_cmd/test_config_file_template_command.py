703ac33aa4f082da8757167d439037970a595155
Jan Honsbrok <jan.honsbrok@gmail.com>
2021-11-09 19:43:13 +0100

    migrate config file integration tests away from pytest-bdd

--- tests/integrationtests/cato_cmd/test_config_file_template_command.py
+++ tests/integrationtests/cato_cmd/test_config_file_template_command.py
@@ -1,67 +1,52 @@
 import json
 import os
 
-from pytest_bdd import scenario, given, when, then
+import pytest
 
 from cato.config.config_template_generator import TEMPLATE
 from tests.integrationtests.command_fixture import run_cato_command
 
 
-@scenario(
-    "test_config_file_template_command.feature",
-    "Create a new cato.json in an empty folder",
-)
-def test_in_empty_folder(scenario_context):
-    assert scenario_context["command_result"].output_contains_line(
-        "[INFO]  Wrote config file to {}".format(scenario_context["config_path"])
-    )
-
-
-@scenario(
-    "test_config_file_template_command.feature",
-    "Create a cato.json in a folder with an existing cato.json",
-)
-def test_in_non_emptyfolder(scenario_context):
-    assert scenario_context["command_result"].output_contains_line(
-        "[INFO]  Wrote config file to {}".format(scenario_context["config_path"])
-    )
-
-
-@given("An empty folder")
-def step_impl(scenario_context, tmp_path):
-    scenario_context["config_folder"] = str(tmp_path)
-    scenario_context["config_path"] = os.path.join(str(tmp_path), "cato.json")
-    assert os.listdir(str(tmp_path)) == []
-
-
-@then("a new cato.json should be created based on template")
-def step_impl(scenario_context):
-    assert os.listdir(scenario_context["config_folder"]) == ["cato.json"]
-
-
-@when("I run the config-template command with the folder")
-def step_impl(scenario_context):
-    command_result = run_cato_command(
-        ["config-template", scenario_context["config_folder"]]
-    )
-    assert command_result.exit_code == 0
-    scenario_context["command_result"] = command_result
-
-
-@given("a folder with non default cato.json")
-def step_impl(scenario_context, tmp_path):
+@pytest.fixture
+def empty_folder(tmp_path):
     config_folder = str(tmp_path)
-    scenario_context["config_folder"] = config_folder
-    scenario_context["config_path"] = os.path.join(str(tmp_path), "cato.json")
-    with open(scenario_context["config_path"], "w") as f:
+    config_path = os.path.join(str(tmp_path), "cato.json")
+    return config_folder, config_path
+
+
+def test_create_a_new_cato_json_in_an_empty_folder(empty_folder):
+    config_folder, config_path = empty_folder
+
+    run_config_template_command_in_folder(config_folder)
+
+    assert_config_folder_contains_cato_json(config_folder)
+
+
+def test_create_a_cato_json_in_a_non_empty_folder_should_override(empty_folder):
+    config_folder, config_path = empty_folder
+    write_non_default_cato_json(config_path)
+
+    run_config_template_command_in_folder(config_folder)
+
+    assert_config_folder_contains_template_cato_json(config_path)
+
+
+def write_non_default_cato_json(config_folder):
+    with open(config_folder, "w") as f:
         f.write("Hello world")
 
 
-@then("the cato.json should be overriden with template")
-def step_impl(scenario_context):
-    with open(scenario_context["config_path"]) as f:
-        conent = f.read()
-    with open(scenario_context["config_path"]) as f:
+def run_config_template_command_in_folder(config_folder):
+    command_result = run_cato_command(["config-template", config_folder])
+    assert command_result.exit_code == 0
+
+
+def assert_config_folder_contains_cato_json(config_folder):
+    assert os.listdir(config_folder) == ["cato.json"]
+
+
+def assert_config_folder_contains_template_cato_json(config_path):
+    with open(config_path) as f:
         content = json.load(f)
 
     assert content == TEMPLATE
