e33192cb7d5270a3c8d02d483363d7c3951f9a50
Jan Honsbrok <jan.honsbrok@gmail.com>
2021-11-10 19:39:42 +0100

    migrate list tests tests away from pytest-bdd

--- tests/integrationtests/cato_cmd/test_list_tests_command.py
+++ tests/integrationtests/cato_cmd/test_list_tests_command.py
@@ -1,49 +1,41 @@
 import os
 
-from pytest_bdd import scenario, given, when, then
-
 from tests.integrationtests.command_fixture import run_cato_command
 
 
-@scenario("test_list_tests_command.feature", "In Folder with cato.json")
-def test_in_folder():
-    pass
+def test_list_tests_described_in_a_config_file(test_resource_provider):
+    config_folder = test_resource_provider.resource_by_name("cato_test_config")
+    os.chdir(config_folder)
+    result = run_the_list_tests_command()
+    assert_result_contains_test_identifiers(result)
 
 
-@scenario("test_list_tests_command.feature", "Path to config")
-def test_path_to_config():
-    pass
+def test_list_tests_described_in_a_config_file_from_current_dir(test_resource_provider):
+    config_folder = test_resource_provider.resource_by_name("cato_test_config")
+    result = run_the_list_tests_command_with_path_to_config(config_folder)
+    assert_result_contains_test_identifiers(result)
 
 
-@given("I run the list-tests command")
-def step_impl(scenario_context):
+def run_the_list_tests_command():
     command_result = run_cato_command(["list-tests"])
     assert command_result.exit_code == 0
-    scenario_context["command_result"] = command_result
+    return command_result
 
 
-@then("I should see the test identifiers of the config on the terminal")
-def step_impl(scenario_context):
-    assert scenario_context["command_result"].output_contains_line(
+def assert_result_contains_test_identifiers(command_result):
+    assert command_result.output_contains_line(
         "[INFO]  My_first_test_Suite/My_first_test"
     )
 
 
-@given("A folder with a valid cato.json")
-def step_impl(scenario_context, test_resource_provider):
-    scenario_context["config_folder"] = test_resource_provider.resource_by_name(
-        "cato_test_config"
-    )
+def run_the_list_tests_command_with_path_to_config(config_folder):
+    config_path = os.path.join(config_folder, "cato.json")
+    command_result = run_cato_command(["list-tests", "--path", config_path])
+    assert command_result.exit_code == 0
+    return command_result
 
 
-@given("I changed to this folder")
-def step_impl(scenario_context, dir_changer):
-    os.chdir(scenario_context["config_folder"])
-
-
-@when("I run the list-tests command with the path to the config")
-def step_impl(scenario_context):
-    config_path = os.path.join(scenario_context["config_folder"], "cato.json")
-    scenario_context["command_result"] = run_cato_command(
-        ["list-tests", "--path", config_path]
+def assert_result_contains_test_identifiers(command_result):
+    assert command_result.output_contains_line(
+        "[INFO]  My_first_test_Suite/My_first_test"
     )
