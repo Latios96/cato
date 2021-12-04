614ea7471ed1b4fd392990de332380f5034159ba
Jan Honsbrok <jan.honsbrok@gmail.com>
2021-12-04 16:35:47 +0100

    fix test

--- tests/unittests/cato/utils/test_branch_detector.py
+++ tests/unittests/cato/utils/test_branch_detector.py
@@ -15,10 +15,12 @@ def test_should_return_none_if_not_in_a_git_repo(tmp_path):
 def test_should_branch_name(tmp_path):
     branch_detector = BranchDetector()
     with change_cwd(str(tmp_path)):
-        subprocess.check_output("git init")
+        subprocess.check_output(["git", "init"])
         (tmp_path / "test.txt").touch()
-        subprocess.check_output("git add test.txt")
-        subprocess.check_output('git commit -m"test"')
+        subprocess.check_output(["git", "add", "test.txt"])
+        subprocess.check_output(
+            ["git", "commit", "-m", "test", "--author=Bar <bar@foo.com>"]
+        )
 
     branch = branch_detector.detect_branch(str(tmp_path))
 
