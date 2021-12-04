359b80822ef9383b5f5e2d5467b261bc56b89c15
Jan Honsbrok <jan.honsbrok@gmail.com>
2021-12-04 20:19:51 +0100

    add ArrowButton

---
+++ frontend/src/components/Button/ArrowButton.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import {
+  ChevronDown,
+  ChevronLeft,
+  ChevronRight,
+  ChevronUp,
+} from "react-bootstrap-icons";
+
+import styles from "./ArrowButton.module.scss";
+
+type Direction = "up" | "down" | "left" | "right";
+
+interface Props {
+  text: string;
+  direction: Direction;
+  onClick?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+}
+
+function directionToIcon(direction: Direction) {
+  if (direction === "up") {
+    return <ChevronUp />;
+  } else if (direction === "down") {
+    return <ChevronDown />;
+  } else if (direction === "left") {
+    return <ChevronLeft />;
+  } else if (direction === "right") {
+    return <ChevronRight />;
+  }
+}
+
+function ArrowButton(props: Props) {
+  return (
+    <button className={styles.arrowButton} onClick={props.onClick}>
+      {props.text} {directionToIcon(props.direction)}
+    </button>
+  );
+}
+
+export default ArrowButton;
