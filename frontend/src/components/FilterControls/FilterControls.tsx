ea3b6efffafcd715694c06acbaf58bfd22cac4dc
Jan Honsbrok <jan.honsbrok@gmail.com>
2022-01-08 15:11:28 +0100

    fix: FilterControls did plop when getting enabled

--- frontend/src/components/FilterControls/FilterControls.tsx
+++ frontend/src/components/FilterControls/FilterControls.tsx
@@ -14,86 +14,90 @@ interface Props {
 function FilterControls(props: Props) {
   return (
     <div className={styles.filterControlContainer}>
-      <Form.Check
-        inline
-        id="All"
-        label="All"
-        name={"Filter"}
-        type={"radio"}
-        checked={props.currentFilterOptions.status === StatusFilter.NONE}
-        onClick={() =>
-          props.filterOptionsChanged(
-            props.currentFilterOptions
-              .withChangedStatusFilter(StatusFilter.NONE)
-              .withChangedFailureReason(undefined)
-          )
-        }
-        onChange={(e) => {}}
-      />
-      <Form.Check
-        inline
-        id="Failed"
-        label="Failed"
-        name={"Filter"}
-        type={"radio"}
-        checked={props.currentFilterOptions.status === StatusFilter.FAILED}
-        onClick={() =>
-          props.filterOptionsChanged(
-            props.currentFilterOptions.withChangedStatusFilter(
-              StatusFilter.FAILED
+      <div className={styles.checkboxContainer}>
+        <Form.Check
+          inline
+          id="All"
+          label="All"
+          name={"Filter"}
+          type={"radio"}
+          checked={props.currentFilterOptions.status === StatusFilter.NONE}
+          onClick={() =>
+            props.filterOptionsChanged(
+              props.currentFilterOptions
+                .withChangedStatusFilter(StatusFilter.NONE)
+                .withChangedFailureReason(undefined)
             )
-          )
-        }
-        onChange={(e) => {}}
-      />
-      <Form.Check
-        inline
-        id="Success"
-        label="Success"
-        name={"Filter"}
-        type={"radio"}
-        checked={props.currentFilterOptions.status === StatusFilter.SUCCESS}
-        onClick={() =>
-          props.filterOptionsChanged(
-            props.currentFilterOptions
-              .withChangedStatusFilter(StatusFilter.SUCCESS)
-              .withChangedFailureReason(undefined)
-          )
-        }
-        onChange={(e) => {}}
-      />
-      <Form.Check
-        inline
-        id="Running"
-        label="Running"
-        name={"Filter"}
-        type={"radio"}
-        checked={props.currentFilterOptions.status === StatusFilter.RUNNING}
-        onClick={() =>
-          props.filterOptionsChanged(
-            props.currentFilterOptions
-              .withChangedStatusFilter(StatusFilter.RUNNING)
-              .withChangedFailureReason(undefined)
-          )
-        }
-        onChange={(e) => {}}
-      />
-      <Form.Check
-        inline
-        id="Not Started"
-        label={"Not Started"}
-        name={"Filter"}
-        type={"radio"}
-        checked={props.currentFilterOptions.status === StatusFilter.NOT_STARTED}
-        onClick={() =>
-          props.filterOptionsChanged(
-            props.currentFilterOptions
-              .withChangedStatusFilter(StatusFilter.NOT_STARTED)
-              .withChangedFailureReason(undefined)
-          )
-        }
-        onChange={(e) => {}}
-      />
+          }
+          onChange={(e) => {}}
+        />
+        <Form.Check
+          inline
+          id="Failed"
+          label="Failed"
+          name={"Filter"}
+          type={"radio"}
+          checked={props.currentFilterOptions.status === StatusFilter.FAILED}
+          onClick={() =>
+            props.filterOptionsChanged(
+              props.currentFilterOptions.withChangedStatusFilter(
+                StatusFilter.FAILED
+              )
+            )
+          }
+          onChange={(e) => {}}
+        />
+        <Form.Check
+          inline
+          id="Success"
+          label="Success"
+          name={"Filter"}
+          type={"radio"}
+          checked={props.currentFilterOptions.status === StatusFilter.SUCCESS}
+          onClick={() =>
+            props.filterOptionsChanged(
+              props.currentFilterOptions
+                .withChangedStatusFilter(StatusFilter.SUCCESS)
+                .withChangedFailureReason(undefined)
+            )
+          }
+          onChange={(e) => {}}
+        />
+        <Form.Check
+          inline
+          id="Running"
+          label="Running"
+          name={"Filter"}
+          type={"radio"}
+          checked={props.currentFilterOptions.status === StatusFilter.RUNNING}
+          onClick={() =>
+            props.filterOptionsChanged(
+              props.currentFilterOptions
+                .withChangedStatusFilter(StatusFilter.RUNNING)
+                .withChangedFailureReason(undefined)
+            )
+          }
+          onChange={(e) => {}}
+        />
+        <Form.Check
+          inline
+          id="Not Started"
+          label={"Not Started"}
+          name={"Filter"}
+          type={"radio"}
+          checked={
+            props.currentFilterOptions.status === StatusFilter.NOT_STARTED
+          }
+          onClick={() =>
+            props.filterOptionsChanged(
+              props.currentFilterOptions
+                .withChangedStatusFilter(StatusFilter.NOT_STARTED)
+                .withChangedFailureReason(undefined)
+            )
+          }
+          onChange={(e) => {}}
+        />
+      </div>
       {!props.failureReasonIsNotFilterable &&
       props.currentFilterOptions.status === StatusFilter.FAILED ? (
         <div className={styles.failureReasonFilterControls}>
