21153596514c87db39cc54cdb2cd3eba80eba858
Jan Honsbrok <jan.honsbrok@gmail.com>
2022-01-08 14:02:44 +0100

    ProjectsView: propper error Handling

--- frontend/src/components/ProjectsView/ProjectsView.tsx
+++ frontend/src/components/ProjectsView/ProjectsView.tsx
@@ -6,61 +6,76 @@ import PlaceHolderText from "../PlaceholderText/PlaceHolderText";
 import _ from "lodash";
 import { Spinner } from "react-bootstrap";
 import { FetchResult, useFetch } from "../../hooks/useFetch";
+import {
+  DataLoadedState,
+  ErrorState,
+  LoadingState,
+  LoadingStateHandler,
+} from "../LoadingStateHandler/LoadingStateHandler";
+import ErrorMessageBox from "../ErrorMessageBox/ErrorMessageBox";
 
 interface Props {
   fetchResult: FetchResult<Project[]>;
 }
 
 export const ProjectsViewStateless = (props: Props) => {
-  const isLoading = props.fetchResult.isLoading;
-  const hasError = !!props.fetchResult.error;
   const emptyProjectsLists =
     props.fetchResult.data && props.fetchResult.data.length === 0;
-
-  if (isLoading) {
-    return (
-      <div className={styles.projectsView}>
-        <Spinner
-          animation="border"
-          role="LoadingIndicator"
-          className={styles.centered}
-        >
-          <span className="sr-only">Loading...</span>
-        </Spinner>
-      </div>
-    );
-  } else if (hasError) {
-    return (
-      <div className={styles.projectsView}>
-        <span className={styles.centered}>{props.fetchResult.error}</span>
-      </div>
-    );
-  } else if (emptyProjectsLists) {
-    return (
-      <div className={styles.projectsView}>
-        <PlaceHolderText
-          text={"No projects found"}
-          className={styles.centered}
-        />
-      </div>
-    );
-  } else if (props.fetchResult.data) {
-    return (
-      <div className={styles.projectsView}>
-        {" "}
-        {_.sortBy(props.fetchResult.data, [
-          (p: Project) => p.name.toLowerCase(),
-        ]).map((p: Project) => {
-          return (
-            <div key={p.id} className={styles.projectsViewProjectComponent}>
-              <LinkCard name={p.name} linkTo={`/projects/${p.id}`} />
+  return (
+    <LoadingStateHandler
+      isLoading={props.fetchResult.isLoading}
+      error={props.fetchResult.error}
+    >
+      <LoadingState>
+        <div className={styles.projectsView}>
+          <Spinner
+            animation="border"
+            role="LoadingIndicator"
+            className={styles.centered}
+          >
+            <span className="sr-only">Loading...</span>
+          </Spinner>
+        </div>
+      </LoadingState>
+      <ErrorState>
+        <div className={styles.projectsView}>
+          <span className={styles.centered}>
+            <ErrorMessageBox
+              heading={"An error encountered while loading all projects"}
+              message={props.fetchResult.error?.message}
+            />
+          </span>
+        </div>
+      </ErrorState>
+      <DataLoadedState>
+        {props.fetchResult.data !== undefined ? (
+          emptyProjectsLists ? (
+            <div className={styles.projectsView}>
+              <PlaceHolderText
+                text={"No projects found"}
+                className={styles.centered}
+              />
             </div>
-          );
-        })}
-      </div>
-    );
-  }
-  return <></>;
+          ) : (
+            <div className={styles.projectsView}>
+              {_.sortBy(props.fetchResult.data, [
+                (p: Project) => p.name.toLowerCase(),
+              ]).map((p: Project) => {
+                return (
+                  <div
+                    key={p.id}
+                    className={styles.projectsViewProjectComponent}
+                  >
+                    <LinkCard name={p.name} linkTo={`/projects/${p.id}`} />
+                  </div>
+                );
+              })}
+            </div>
+          )
+        ) : null}
+      </DataLoadedState>
+    </LoadingStateHandler>
+  );
 };
 
 export const ProjectsView = () => {
