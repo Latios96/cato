ac8988f46b6fbb0c4a5ff5ef252398d64050a070
Jan Honsbrok <jan.honsbrok@gmail.com>
2022-01-29 17:15:25 +0100

    outline session and session repository

--- cato_server/storage/abstract/session_repository.py
+++ cato_server/storage/abstract/session_repository.py
@@ -1,3 +1,5 @@
+import datetime
+
 from cato_server.domain.auth.session import Session
 from cato_server.domain.auth.session_token import SessionToken
 from cato_server.storage.abstract.abstract_repository import AbstractRepository
@@ -5,4 +7,7 @@ from cato_server.storage.abstract.abstract_repository import AbstractRepository
 
 class SessionRepository(AbstractRepository[Session, int]):
     def find_by_session_token(self, session_token: SessionToken):
+        pass
+
+    def find_by_expires_at_is_older_than(self, date: datetime.datetime):
         pass
