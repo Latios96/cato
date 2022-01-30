b9d27401575675ed3723381867f89cc939ed5537
Jan Honsbrok <jan.honsbrok@gmail.com>
2022-01-30 16:24:59 +0100

    refactor: rename variable to better match actual usage

--- cato_server/configuration/app_configuration_writer.py
+++ cato_server/configuration/app_configuration_writer.py
@@ -14,34 +14,34 @@ logger = logging.getLogger(__name__)
 
 class AppConfigurationWriter:
     def write_stream(self, config: AppConfiguration, stream: IO) -> None:
-        config_reader = configparser.ConfigParser()
-        config_reader.add_section("app")
-        config_reader.add_section("storage")
-        config_reader.add_section("scheduler")
-        config_reader.add_section("session")
-        config_reader.set("app", "port", str(config.port))
-        config_reader.set("app", "debug", str(config.debug))
-        config_reader.set(
+        config_writer = configparser.ConfigParser()
+        config_writer.add_section("app")
+        config_writer.add_section("storage")
+        config_writer.add_section("scheduler")
+        config_writer.add_section("session")
+        config_writer.set("app", "port", str(config.port))
+        config_writer.set("app", "debug", str(config.debug))
+        config_writer.set(
             "storage", "database_url", config.storage_configuration.database_url
         )
-        config_reader.set(
+        config_writer.set(
             "storage", "file_storage_url", config.storage_configuration.file_storage_url
         )
 
         scheduler_configuration = config.scheduler_configuration
-        config_reader.set("scheduler", "name", scheduler_configuration.name)
+        config_writer.set("scheduler", "name", scheduler_configuration.name)
         if scheduler_configuration.name == "Deadline":
-            config_reader.set(
+            config_writer.set(
                 "scheduler",
                 "deadline_url",
                 cast(DeadlineSchedulerConfiguration, scheduler_configuration).url,
             )
 
         if config.sentry_configuration.url:
-            config_reader.add_section("sentry")
-            config_reader.set("sentry", "url", config.sentry_configuration.url)
+            config_writer.add_section("sentry")
+            config_writer.set("sentry", "url", config.sentry_configuration.url)
 
-        config_reader.set(
+        config_writer.set(
             "session",
             "lifetime",
             humanfriendly.format_timespan(
@@ -49,7 +49,7 @@ class AppConfigurationWriter:
             ),
         )
 
-        config_reader.write(stream)
+        config_writer.write(stream)
 
     def write_file(self, config: AppConfiguration, path: str) -> None:
         with open(path, "w") as f:
