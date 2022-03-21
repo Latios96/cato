6b3b93d715d7ce8b3e296cd59a3df69579dddca0
Jan Honsbrok <jan.honsbrok@gmail.com>
2022-03-21 15:23:20 +0100

    feat: cleanup setupProxy and add logout route

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,7 +1,7 @@
-/*const createProxyMiddleware = require("http-proxy-middleware");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
-  ["/login", "/api/*"].forEach((route) => {
+  ["/login", "/logout", "/api/*"].forEach((route) => {
     app.use(
       route,
       createProxyMiddleware({
@@ -10,22 +10,4 @@ module.exports = function (app) {
       })
     );
   });
-};*/
-const { createProxyMiddleware } = require("http-proxy-middleware");
-
-module.exports = function (app) {
-  app.use(
-    "/api",
-    createProxyMiddleware({
-      target: "http://localhost:5000",
-      changeOrigin: true,
-    })
-  );
-  app.use(
-    "/login",
-    createProxyMiddleware({
-      target: "http://localhost:5000",
-      changeOrigin: true,
-    })
-  );
 };
