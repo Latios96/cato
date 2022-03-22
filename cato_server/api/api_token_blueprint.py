09558adef7428005b03c0287f2b034d65637164c
Jan Honsbrok <jan.honsbrok@gmail.com>
2022-03-22 14:23:47 +0100

    fix: move to correct url

--- cato_server/api/api_token_blueprint.py
+++ cato_server/api/api_token_blueprint.py
@@ -6,7 +6,7 @@ class ApiTokenBlueprint(APIRouter):
     def __init__(self):
         super(ApiTokenBlueprint, self).__init__()
 
-        self.get("/is_valid")(self.is_valid)
+        self.get("/api_tokens/is_valid")(self.is_valid)
 
     def is_valid(self):
         return Response(status_code=200)
--- tests/unittests/cato_server/api/middlewares/test_authentication_middleware.py
+++ tests/unittests/cato_server/api/middlewares/test_authentication_middleware.py
@@ -1,16 +1,18 @@
 def test_should_pass_without_header(client):
     # todo this will be changed once we enforce Authentication
-    response = client.get("/api/v1/is_valid")
+    response = client.get("/api/v1/api_tokens/is_valid")
     assert response.status_code == 200
 
 
 def test_should_fail_with_invalid_token(client):
-    response = client.get("/api/v1/is_valid", headers={"Authorization": "Bearer test"})
+    response = client.get(
+        "/api/v1/api_tokens/is_valid", headers={"Authorization": "Bearer test"}
+    )
     assert response.status_code == 401
 
 
 def test_should_pass_with_valid_header(client, api_token_str):
     s = str(api_token_str.to_bearer())
-    response = client.get("/api/v1/is_valid", headers={"Authorization": s})
+    response = client.get("/api/v1/api_tokens/is_valid", headers={"Authorization": s})
 
     assert response.status_code == 200
