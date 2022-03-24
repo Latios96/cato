6caa62aaec712ea4f7fc66c8fc5a1eb21d9e3f42
Jan Honsbrok <jan.honsbrok@gmail.com>
2022-03-24 15:37:46 +0100

    feat: add animation to login landing page

--- frontend/src/pages/LoginPage/LoginPage.tsx
+++ frontend/src/pages/LoginPage/LoginPage.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import styles from "./LoginPage.module.scss";
+// @ts-ignore
+import video from "./cato_login_keyvisual_animation.mp4";
 
 function LoginPage() {
   return (
     <div className={styles.loginBackground}>
+      <video className={styles.videoBackground} autoPlay muted loop>
+        <source src={video} type="video/mp4" />
+      </video>
       <div className={styles.loginContainer}>
         <span className={styles.logo}>cato</span>
         <a className="btn btn-primary" href={"/login"}>
