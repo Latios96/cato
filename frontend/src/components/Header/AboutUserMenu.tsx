7a47f84da98dc9d30cb38b3915f50f9b10e6bffe
Jan Honsbrok <jan.honsbrok@gmail.com>
2022-03-27 17:14:09 +0200

    refactor: extract menu logic from AboutUserMenu into a universal component

--- frontend/src/components/Header/AboutUserMenu.tsx
+++ frontend/src/components/Header/AboutUserMenu.tsx
@@ -1,52 +1,50 @@
-import { OverlayTrigger } from "react-bootstrap";
-import styles from "./AboutUserMenu.module.scss";
 import Avatar from "../Avatar/Avatar";
 import ArrowButton from "../Button/ArrowButton";
 import React from "react";
 import axios from "axios";
 import { useAuthenticatedUser } from "../../contexts/AuthenticatedUserContext/AuthenticatedUserContext";
+import PopoverMenu from "../PopoverMenu/PopoverMenu";
 
 export function AboutUserMenu() {
   const authenticatedUser = useAuthenticatedUser();
   return (
-    <OverlayTrigger
-      placement="bottom"
-      trigger={"click"}
-      rootClose={true}
-      overlay={
-        <div className={styles.inputContainer}>
-          <div className={styles.menuContent}>
-            <span className={"font-weight-bolder"}>
-              {authenticatedUser.fullname}
-            </span>
-            <span>@{authenticatedUser.username}</span>
-          </div>
-          <div
-            className={styles.menuContent + " " + styles.menuContentClickable}
-            onClick={() => {
-              axios
-                .post("/logout")
-                .then(() => {
-                  window.location.href = "/";
-                })
-                .catch(() => {});
-            }}
-          >
-            <div id={"btn-logout"} className={styles.menuElement}>
-              Logout
-            </div>
-          </div>
+    <PopoverMenu
+      id={"about-user-menu"}
+      menuItems={[
+        {
+          id: "username",
+          element: (
+            <>
+              <span className={"font-weight-bolder"}>
+                {authenticatedUser.fullname}
+              </span>
+              <span>@{authenticatedUser.username}</span>
+            </>
+          ),
+        },
+        {
+          id: "logout",
+          element: <>Logout</>,
+          onClick: () => {
+            axios
+              .post("/logout")
+              .then(() => {
+                window.location.href = "/";
+              })
+              .catch(() => {});
+          },
+        },
+      ]}
+      menuTriggerElement={
+        <div className={"d-flex"}>
+          <Avatar user={authenticatedUser} />
+          <ArrowButton
+            id={"btn-about-user-menu"}
+            direction={"down"}
+            color={"white"}
+          />
         </div>
       }
-    >
-      <div className={"d-flex"}>
-        <Avatar user={authenticatedUser} />
-        <ArrowButton
-          id={"btn-about-user-menu"}
-          direction={"down"}
-          color={"white"}
-        />
-      </div>
-    </OverlayTrigger>
+    />
   );
 }
---
+++ frontend/src/components/PopoverMenu/PopoverMenu.tsx
@@ -0,0 +1,46 @@
+import React, { ReactElement } from "react";
+import styles from "./PopoverMenu.module.scss";
+
+import { OverlayTrigger } from "react-bootstrap";
+import { joinClassnames } from "../../utils/classnameUtils";
+import { isNotLast } from "../../utils/arrayUtils";
+
+interface MenuItem {
+  id: string;
+  element: JSX.Element;
+  onClick?: () => void;
+}
+interface Props {
+  id: string;
+  menuItems: MenuItem[];
+  menuTriggerElement: ReactElement;
+}
+function PopoverMenu(props: Props) {
+  return (
+    <OverlayTrigger
+      placement="bottom"
+      trigger={"click"}
+      rootClose={true}
+      overlay={
+        <div className={styles.inputContainer}>
+          {props.menuItems.map((menuItem, i) => {
+            const classNames = joinClassnames([
+              styles.menuItem,
+              menuItem.onClick && styles.menuItemClickable,
+              isNotLast(i, props.menuItems) && styles.menuItemBorder,
+            ]);
+            return (
+              <div id={props.id + "-" + menuItem.id} className={classNames}>
+                {menuItem.element}
+              </div>
+            );
+          })}
+        </div>
+      }
+    >
+      {props.menuTriggerElement}
+    </OverlayTrigger>
+  );
+}
+
+export default PopoverMenu;
