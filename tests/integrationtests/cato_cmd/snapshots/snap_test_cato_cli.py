d75e0f721920a1eecb0543c537a1715ccf9bb6ea
Jan Honsbrok <jan.honsbrok@gmail.com>
2022-03-27 13:10:00 +0200

    chore: update snapshots

--- tests/integrationtests/cato_cmd/snapshots/snap_test_cato_cli.py
+++ tests/integrationtests/cato_cmd/snapshots/snap_test_cato_cli.py
@@ -7,25 +7,6 @@ from snapshottest import Snapshot
 
 snapshots = Snapshot()
 
-snapshots["test_config_file_template 1"] = [
-    """[INFO]  Wrote config file to SOME_RANDOM_DIR/cato.json
-"""
-]
-
-snapshots[
-    "test_list_tests_command_from_cwd 1"
-] = """[INFO]  Found 1 tests in 1 suites:
-[INFO]  
-[INFO]  My_first_test_Suite/My_first_test
-"""
-
-snapshots[
-    "test_list_tests_command_from_path 1"
-] = """[INFO]  Found 1 tests in 1 suites:
-[INFO]  
-[INFO]  My_first_test_Suite/My_first_test
-"""
-
 snapshots["test_submit_command 1"] = [
     """[INFO]  Reporting execution start to server..
 [INFO]  No project with name EXAMPLE_PROJECT exists, creating one..
@@ -37,10 +18,6 @@ snapshots["test_submit_command 1"] = [
 [INFO]  Submitted 1 suite with 1 test to scheduler.
 """
 ]
-
-snapshots["test_update_missing_reference_images_should_have_no_effect 1"] = ""
-
-snapshots["test_update_reference_should_have_no_effect 1"] = ""
 
 snapshots["test_worker_run_command 1"] = [
     """[INFO]  Reporting execution start to server..
