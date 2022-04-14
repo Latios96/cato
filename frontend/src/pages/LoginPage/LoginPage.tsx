fbb8519dfe40e2e3a3dd12bc5c825134fa1f7b64
Jan Honsbrok <jan.honsbrok@gmail.com>
2022-04-14 15:36:16 +0200

    fix: add correct page title

--- frontend/src/pages/LoginPage/LoginPage.tsx
+++ frontend/src/pages/LoginPage/LoginPage.tsx
@@ -2,24 +2,30 @@ import React from "react";
 import styles from "./LoginPage.module.scss";
 // @ts-ignore
 import video from "./cato_login_keyvisual_animation_1080p.mp4";
+import { Helmet } from "react-helmet";
 
 function LoginPage() {
   return (
-    <div className={styles.loginBackground}>
-      <video className={styles.videoBackground} autoPlay muted loop>
-        <source src={video} type="video/mp4" />
-      </video>
-      <div className={styles.loginContainer}>
-        <span className={styles.logo}>cato</span>
-        <a
-          id={"login"}
-          className="btn btn-primary"
-          href={`/login?from=${window.location.pathname}`}
-        >
-          Login
-        </a>
+    <>
+      <Helmet>
+        <title>Login</title>
+      </Helmet>
+      <div className={styles.loginBackground}>
+        <video className={styles.videoBackground} autoPlay muted loop>
+          <source src={video} type="video/mp4" />
+        </video>
+        <div className={styles.loginContainer}>
+          <span className={styles.logo}>cato</span>
+          <a
+            id={"login"}
+            className="btn btn-primary"
+            href={`/login?from=${window.location.pathname}`}
+          >
+            Login
+          </a>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
