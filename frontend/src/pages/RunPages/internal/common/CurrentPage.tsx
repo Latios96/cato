ccdc423d7789bc8cf55621833306597a7e1c7c7d
Jan Honsbrok <jan.honsbrok@gmail.com>
2022-04-14 15:41:48 +0200

    feat: add function to get a display string

--- frontend/src/pages/RunPages/internal/common/CurrentPage.tsx
+++ frontend/src/pages/RunPages/internal/common/CurrentPage.tsx
@@ -3,3 +3,14 @@ export enum CurrentPage {
   SUITES,
   TESTS,
 }
+
+export function toDisplayString(currentPage: CurrentPage): string {
+  switch (currentPage) {
+    case CurrentPage.OVERVIEW:
+      return "Overview";
+    case CurrentPage.SUITES:
+      return "Suites";
+    case CurrentPage.TESTS:
+      return "Tests";
+  }
+}
