7d626c476ee52e003742730ff98bde56182cea7f
Jan Honsbrok <jan.honsbrok@gmail.com>
2022-07-22 15:12:09 +0200

    feat: add TaskResultFactory

---
+++ cato_server/task_queue/task_result_factory.py
@@ -0,0 +1,70 @@
+import json
+from typing import TypeVar
+
+from celery.result import AsyncResult
+
+from cato_common.mappers.object_mapper import ObjectMapper
+from cato_server.configuration.app_configuration import AppConfiguration
+from cato_server.task_queue.task_result import TaskResult, TaskResultState
+
+T = TypeVar("T")
+
+CELERY_STATES_FOR_PENDING = frozenset({"PENDING", "RECEIVED", "STARTED", "RETRY"})
+CELERY_STATES_FOR_SUCCESS = frozenset({"SUCCESS"})
+CELERY_STATES_FOR_FAILURE = frozenset({"FAILURE", "REVOKED", "REJECTED", "IGNORED"})
+
+
+class TaskResultFactory:
+    def __init__(
+        self, app_configuration: AppConfiguration, object_mapper: ObjectMapper
+    ):
+        self._app_configuration = app_configuration
+        self._object_mapper = object_mapper
+
+    def from_async_result(self, async_result: AsyncResult):
+        if async_result.state in CELERY_STATES_FOR_PENDING:
+            return self.create_pending_result(async_result.task_id)
+        elif async_result.state in CELERY_STATES_FOR_SUCCESS:
+            return self.create_success_result(
+                async_result.task_id, json.loads(async_result.result)
+            )
+        elif async_result.state in CELERY_STATES_FOR_FAILURE:
+            return self.create_failed_result(
+                async_result.task_id,
+                self._error_message_from_traceback(async_result.traceback),
+            )
+
+    def create_pending_result(self, task_id: str) -> TaskResult:
+        return TaskResult(
+            task_id=task_id,
+            state=TaskResultState.PENDING,
+            url=self._create_url(task_id),
+            _result=None,
+            _error_message=None,
+        )
+
+    def create_success_result(self, task_id: str, result: T) -> TaskResult[T]:
+        return TaskResult(
+            task_id=task_id,
+            state=TaskResultState.SUCCESS,
+            url=self._create_url(task_id),
+            _result=result,
+            _error_message=None,
+        )
+
+    def create_failed_result(self, task_id: str, error_message: str) -> TaskResult:
+        return TaskResult(
+            task_id=task_id,
+            state=TaskResultState.FAILURE,
+            url=self._create_url(task_id),
+            _result=None,
+            _error_message=error_message,
+        )
+
+    def _create_url(self, task_id: str):
+        return self._app_configuration.public_url + f"/api/v1/task_results/{task_id}"
+
+    def _error_message_from_traceback(self, traceback):
+        if traceback:
+            return list(filter(lambda x: x, traceback.split("\n")))[-1]
+        return "<no message>"
---
+++ tests/unittests/cato_server/task_queue/test_task_result_factory.py
@@ -0,0 +1,137 @@
+# from_async_result
+# pending
+# succcess
+# failed
+# pending
+# succcess
+# failed
+import uuid
+
+import pytest
+from celery.result import AsyncResult
+
+from cato_server.configuration.app_configuration_defaults import (
+    AppConfigurationDefaults,
+)
+from cato_server.task_queue.task_result import TaskResult, TaskResultState
+from cato_server.task_queue.task_result_factory import TaskResultFactory
+from tests.utils import mock_safe
+
+TASK_ID = str(uuid.uuid4())
+TASK_URL = f"http://127.0.0.1/api/v1/task_results/{TASK_ID}"
+
+REAL_TRACEBACK = """Traceback (most recent call last):
+  File "C:\Python37\Lib\code.py", line 90, in runcode
+    exec(code, self.locals)
+  File "<input>", line 1, in <module>
+ValueError: Test"""
+
+
+@pytest.fixture
+def task_result_factory(object_mapper):
+    return TaskResultFactory(AppConfigurationDefaults().create(), object_mapper)
+
+
+def test_should_create_pending_task_result(task_result_factory):
+    pending_result = task_result_factory.create_pending_result(TASK_ID)
+
+    assert pending_result == TaskResult(
+        task_id=TASK_ID,
+        state=TaskResultState.PENDING,
+        url=TASK_URL,
+        _result=None,
+        _error_message=None,
+    )
+
+
+def test_should_create_successful_task_result(task_result_factory):
+    successful_result = task_result_factory.create_success_result(TASK_ID, 42)
+
+    assert successful_result == TaskResult(
+        task_id=TASK_ID,
+        state=TaskResultState.SUCCESS,
+        url=TASK_URL,
+        _result=42,
+        _error_message=None,
+    )
+
+
+def test_should_create_failed_task_result(task_result_factory):
+    failed_result = task_result_factory.create_failed_result(
+        TASK_ID, "this is my error message"
+    )
+
+    assert failed_result == TaskResult(
+        task_id=TASK_ID,
+        state=TaskResultState.FAILURE,
+        url=TASK_URL,
+        _result=None,
+        _error_message="this is my error message",
+    )
+
+
+class TestFromAsyncResult:
+    @pytest.mark.parametrize(
+        "celery_status", ["PENDING", "RECEIVED", "STARTED", "RETRY"]
+    )
+    def test_should_create_pending_task_result(
+        self, celery_status, task_result_factory
+    ):
+        async_result = mock_safe(AsyncResult)
+        async_result.task_id = TASK_ID
+        async_result.state = celery_status
+
+        pending_result = task_result_factory.from_async_result(async_result)
+
+        assert pending_result == TaskResult(
+            task_id=TASK_ID,
+            state=TaskResultState.PENDING,
+            url=TASK_URL,
+            _result=None,
+            _error_message=None,
+        )
+
+    def test_should_create_successful_task_result(self, task_result_factory):
+        async_result = mock_safe(AsyncResult)
+        async_result.task_id = TASK_ID
+        async_result.state = "SUCCESS"
+        async_result.result = '{"value": 42}'
+
+        successful_result = task_result_factory.from_async_result(async_result)
+
+        assert successful_result == TaskResult(
+            task_id=TASK_ID,
+            state=TaskResultState.SUCCESS,
+            url=TASK_URL,
+            _result={"value": 42},
+            _error_message=None,
+        )
+
+    @pytest.mark.parametrize(
+        "traceback, expected_message",
+        [
+            (REAL_TRACEBACK, "ValueError: Test"),
+            ("", "<no message>"),
+            ("ValueError", "ValueError"),
+        ],
+    )
+    @pytest.mark.parametrize(
+        "celery_status", ["FAILURE", "REVOKED", "REJECTED", "IGNORED"]
+    )
+    def test_should_create_failed_task_result(
+        self, celery_status, traceback, expected_message, task_result_factory
+    ):
+        async_result = mock_safe(AsyncResult)
+        async_result.task_id = TASK_ID
+        async_result.state = "FAILURE"
+        async_result.traceback = traceback
+
+        failed_result = task_result_factory.from_async_result(async_result)
+
+        assert failed_result == TaskResult(
+            task_id=TASK_ID,
+            state=TaskResultState.FAILURE,
+            url=TASK_URL,
+            _result=None,
+            _error_message=expected_message,
+        )
