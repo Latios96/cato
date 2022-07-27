20925204e3398ec018d4965288c6e5c41cf6adf2
Jan Honsbrok <jan.honsbrok@gmail.com>
2022-07-27 14:02:36 +0200

    feat: add TaskResultTemplate to poll for task results

--- cato_api_client/task_result_template.py
+++ cato_api_client/task_result_template.py
@@ -1,10 +1,19 @@
 import datetime
 import logging
-from typing import TypeVar
+from typing import TypeVar, Type
 
-from tenacity import stop_after_attempt, wait_fixed, before_log, after_log, retry
+from tenacity import (
+    stop_after_attempt,
+    wait_fixed,
+    before_log,
+    after_log,
+    retry,
+    retry_if_exception_type,
+    RetryError,
+)
 
 from cato_api_client.http_template import HttpTemplate
+from cato_common.mappers.object_mapper import ObjectMapper
 from cato_server.task_queue.task_result import (
     TaskResult,
     TaskResultState,
@@ -16,16 +25,19 @@ T = TypeVar("T")
 
 
 class TaskResultTemplate:
-    def __init__(self, http_template: HttpTemplate):
+    def __init__(self, http_template: HttpTemplate, object_mapper: ObjectMapper):
         self._http_template = http_template
+        self._object_mapper = object_mapper
 
     def wait_for_task_result_to_complete(
         self,
-        task_result: TaskResult[T],
+        task_result: TaskResult,
+        result_cls: Type[T],
         timeout: datetime.timedelta,
         poll_interval: datetime.timedelta,
-    ):
+    ) -> T:
         @retry(
+            retry=retry_if_exception_type(AssertionError),
             stop=stop_after_attempt(int(timeout.total_seconds())),
             wait=wait_fixed(poll_interval.total_seconds()),
             before=before_log(logger, logging.DEBUG),
@@ -33,9 +45,21 @@ class TaskResultTemplate:
         )
         def fetch_task_result():
             task_result_response = self._http_template.get_for_entity(
-                task_result.url, TaskResult
+                task_result.url, TaskResult[result_cls]
             )
-            assert task_result_response.get_entity().state != TaskResultState.PENDING
-            return task_result_response.get_entity()
+            if not task_result_response.status_code() == 200:
+                raise ValueError(
+                    f"Something went wrong when retrieving task result: {fetch_task_result}"
+                )
 
-        return fetch_task_result()
+            entity = task_result_response.get_entity()
+            assert entity.state != TaskResultState.PENDING
+
+            return self._object_mapper.from_dict(entity.result, result_cls)
+
+        try:
+            return fetch_task_result()
+        except RetryError:
+            raise RuntimeError(
+                f"Timeout while retrieving task result {fetch_task_result}"
+            )
---
+++ tests/unittests/cato_api_client/test_task_result_template.py
@@ -0,0 +1,176 @@
+import datetime
+from dataclasses import dataclass
+from typing import Dict, Type, TypeVar
+
+import pytest
+
+from cato_api_client.http_template import HttpTemplate, HttpTemplateResponse
+from cato_api_client.task_result_template import TaskResultTemplate
+from cato_server.task_queue.task_result import TaskResult, TaskResultState
+from tests.utils import mock_safe
+
+R = TypeVar("R")
+
+
+@pytest.fixture
+def test_context(object_mapper):
+    class TestContext:
+        def __init__(self):
+            self.mock_http_template = mock_safe(HttpTemplate)
+            self.task_result_template = TaskResultTemplate(
+                self.mock_http_template, object_mapper
+            )
+
+    return TestContext()
+
+
+@pytest.fixture()
+def mock_http_template_response_factory(object_mapper):
+    class Response:
+        def __init__(self, status_code, json_value):
+            self.status_code = status_code
+            self._json_value = json_value
+
+        def json(self):
+            return self._json_value
+
+    def mock_http_template_response(
+        status_code: int, json_value: Dict, response_cls: Type[R]
+    ):
+        return HttpTemplateResponse(
+            Response(status_code, json_value), response_cls, object_mapper
+        )
+
+    return mock_http_template_response
+
+
+@dataclass
+class MyResult:
+    value: int
+
+
+def test_should_get_task_result_successfully(
+    test_context, mock_http_template_response_factory
+):
+    task_result_to_wait_for = TaskResult(
+        task_id="id",
+        state=TaskResultState.PENDING,
+        url="/test",
+        result_=None,
+        error_message_=None,
+    )
+
+    response_1 = mock_http_template_response_factory(
+        200,
+        {
+            "taskId": "id",
+            "state": TaskResultState.PENDING,
+            "url": "/test",
+            "result_": None,
+            "errorMessage_": None,
+        },
+        TaskResult,
+    )
+    response_2 = mock_http_template_response_factory(
+        200,
+        {
+            "taskId": "id",
+            "state": TaskResultState.PENDING,
+            "url": "/test",
+            "result_": None,
+            "errorMessage_": None,
+        },
+        TaskResult,
+    )
+    response_3 = mock_http_template_response_factory(
+        200,
+        {
+            "taskId": "id",
+            "state": TaskResultState.SUCCESS,
+            "url": "/test",
+            "result_": {"value": 42},
+            "errorMessage_": None,
+        },
+        TaskResult,
+    )
+    test_context.mock_http_template.get_for_entity.side_effect = [
+        response_1,
+        response_2,
+        response_3,
+    ]
+
+    result = test_context.task_result_template.wait_for_task_result_to_complete(
+        task_result_to_wait_for,
+        MyResult,
+        timeout=datetime.timedelta(seconds=10),
+        poll_interval=datetime.timedelta(seconds=0.5),
+    )
+
+    assert result.value == 42
+
+
+def test_should_timeout(test_context, mock_http_template_response_factory):
+    task_result_to_wait_for = TaskResult(
+        task_id="id",
+        state=TaskResultState.PENDING,
+        url="/test",
+        result_=None,
+        error_message_=None,
+    )
+
+    response_1 = mock_http_template_response_factory(
+        200,
+        {
+            "taskId": "id",
+            "state": TaskResultState.PENDING,
+            "url": "/test",
+            "result_": None,
+            "errorMessage_": None,
+        },
+        TaskResult,
+    )
+    test_context.mock_http_template.get_for_entity.side_effect = [
+        response_1,
+    ]
+
+    with pytest.raises(RuntimeError):
+        test_context.task_result_template.wait_for_task_result_to_complete(
+            task_result_to_wait_for,
+            MyResult,
+            timeout=datetime.timedelta(seconds=0.1),
+            poll_interval=datetime.timedelta(seconds=0.5),
+        )
+
+
+def test_should_throw_when_requests_does_not_return_200(
+    test_context, mock_http_template_response_factory
+):
+    task_result_to_wait_for = TaskResult(
+        task_id="id",
+        state=TaskResultState.PENDING,
+        url="/test",
+        result_=None,
+        error_message_=None,
+    )
+
+    response_1 = mock_http_template_response_factory(
+        500,
+        {
+            "taskId": "id",
+            "state": TaskResultState.PENDING,
+            "url": "/test",
+            "result_": None,
+            "errorMessage_": None,
+        },
+        TaskResult,
+    )
+
+    test_context.mock_http_template.get_for_entity.side_effect = [response_1]
+
+    with pytest.raises(ValueError):
+        test_context.task_result_template.wait_for_task_result_to_complete(
+            task_result_to_wait_for,
+            MyResult,
+            timeout=datetime.timedelta(seconds=10),
+            poll_interval=datetime.timedelta(seconds=0.5),
+        )
