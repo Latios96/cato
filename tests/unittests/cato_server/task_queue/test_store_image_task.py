84bc86362896a6288eb8a13d8adf7509da3043e8
Jan Honsbrok <jan.honsbrok@gmail.com>
2022-08-09 15:31:34 +0200

    tests: improve test

--- tests/unittests/cato_server/task_queue/test_store_image_task.py
+++ tests/unittests/cato_server/task_queue/test_store_image_task.py
@@ -1,5 +1,6 @@
 import pytest
 
+from cato_common.dtos.store_image_result import StoreImageResult
 from cato_server.images.store_image import StoreImage
 from cato_server.storage.abstract.abstract_file_storage import AbstractFileStorage
 from cato_server.task_queue.store_image_task import StoreImageTask, StoreImageParams
@@ -21,14 +22,21 @@ def test_context(object_mapper):
     return TestContext()
 
 
-def test_should_call_store_image_successfully(test_context, stored_image_factory):
-    original_file = stored_image_factory()
+def test_should_call_store_image_successfully(
+    test_context, stored_file_factory, stored_image_factory
+):
+    original_file = stored_file_factory()
+    expected_stored_image = stored_image_factory()
     test_context.mock_file_storage.find_by_id.return_value = original_file
+    test_context.mock_store_image.store_image_from_file_entity.return_value = (
+        expected_stored_image
+    )
 
-    test_context.store_image_task._execute(
+    stored_image = test_context.store_image_task._execute(
         StoreImageParams(original_file_id=original_file.id)
     )
 
+    assert stored_image == StoreImageResult(image=expected_stored_image)
     test_context.mock_store_image.store_image_from_file_entity.assert_called_with(
         original_file
     )
