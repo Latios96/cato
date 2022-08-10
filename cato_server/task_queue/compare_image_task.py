afe25524dd4e8cec2a69ecc57d57288984aa0bd5
Jan Honsbrok <jan.honsbrok@gmail.com>
2022-08-10 15:35:39 +0200

    feat: add CompareImage task

---
+++ cato_server/task_queue/compare_image_task.py
@@ -0,0 +1,52 @@
+from dataclasses import dataclass
+
+from cato.domain.comparison_settings import ComparisonSettings
+from cato_common.domain.compare_image_result import CompareImageResult
+from cato_common.mappers.object_mapper import ObjectMapper
+from cato_server.images.store_image import StoreImage
+from cato_server.storage.abstract.abstract_file_storage import AbstractFileStorage
+from cato_server.task_queue.task import Task
+from cato_server.usecases.compare_image import CompareImage
+
+
+@dataclass
+class CompareImageParams:
+    output_image_id: int
+    reference_image_id: int
+    comparison_settings: ComparisonSettings
+
+
+class CompareImageTask(Task):
+    def __init__(
+        self,
+        object_mapper: ObjectMapper,
+        file_storage: AbstractFileStorage,
+        store_image: StoreImage,
+        compare_image: CompareImage,
+    ):
+        super(CompareImageTask, self).__init__(object_mapper, CompareImageParams)
+        self._object_mapper = object_mapper
+        self._compare_image = compare_image
+        self._file_storage = file_storage
+        self._store_image = store_image
+
+    def _execute(self, params: CompareImageParams) -> CompareImageResult:
+        output_image_file = self._file_storage.find_by_id(params.output_image_id)
+        if not output_image_file:
+            raise RuntimeError(
+                f"Did not expect to not find an Image with id {params.output_image_id} in the db.",
+            )
+        reference_image_file = self._file_storage.find_by_id(params.reference_image_id)
+        if not reference_image_file:
+            raise RuntimeError(
+                f"Did not expect to not find an Image with id {params.reference_image_id} in the db.",
+            )
+
+        output_image = self._store_image.store_image_from_file_entity(output_image_file)
+        reference_image = self._store_image.store_image_from_file_entity(
+            reference_image_file
+        )
+
+        return self._compare_image.compare_image_from_db(
+            output_image, reference_image, params.comparison_settings
+        )
---
+++ tests/unittests/cato_server/task_queue/test_compare_image_task.py
@@ -0,0 +1,99 @@
+import pytest
+
+from cato.domain.comparison_settings import ComparisonSettings
+from cato_common.domain.compare_image_result import CompareImageResult
+from cato_common.domain.result_status import ResultStatus
+from cato_server.images.store_image import StoreImage
+from cato_server.storage.abstract.abstract_file_storage import AbstractFileStorage
+from cato_server.task_queue.compare_image_task import (
+    CompareImageTask,
+    CompareImageParams,
+)
+from cato_server.usecases.compare_image import CompareImage
+from tests.utils import mock_safe
+
+
+@pytest.fixture
+def test_context(object_mapper):
+    class TestContext:
+        def __init__(self):
+            self.mock_compare_image = mock_safe(CompareImage)
+            self.mock_file_storage = mock_safe(AbstractFileStorage)
+            self.mock_store_image = mock_safe(StoreImage)
+            self.compare_image_result = CompareImageResult(
+                status=ResultStatus.SUCCESS,
+                message="",
+                reference_image_id=3,
+                output_image_id=1,
+                diff_image_id=2,
+                error=1,
+            )
+            self.mock_compare_image.compare_image_from_db.return_value = (
+                self.compare_image_result
+            )
+            self.compare_image_task = CompareImageTask(
+                object_mapper,
+                self.mock_file_storage,
+                self.mock_store_image,
+                self.mock_compare_image,
+            )
+
+    return TestContext()
+
+
+def test_compare_images_successfully(
+    test_context, stored_file_factory, stored_image_factory
+):
+    output_image_file = stored_file_factory(id=1)
+    reference_image_file = stored_file_factory(id=2)
+    test_context.mock_file_storage.find_by_id.side_effect = (
+        lambda x: output_image_file if x == 1 else reference_image_file
+    )
+    output_image = stored_image_factory(id=1)
+    reference_image = stored_image_factory(id=2)
+    test_context.mock_store_image.store_image_from_file_entity.side_effect = (
+        lambda x: output_image if x.id == output_image_file.id else reference_image
+    )
+    compare_image_settings = ComparisonSettings.default()
+    compare_image_params = CompareImageParams(
+        output_image_file.id, reference_image_file.id, compare_image_settings
+    )
+
+    compare_image_result = test_context.compare_image_task._execute(
+        compare_image_params
+    )
+
+    assert compare_image_result == test_context.compare_image_result
+    test_context.mock_compare_image.compare_image_from_db.assert_called_with(
+        output_image, reference_image, compare_image_settings
+    )
+    test_context.mock_file_storage.find_by_id.assert_any_call(1)
+    test_context.mock_file_storage.find_by_id.assert_any_call(2)
+
+
+def test_no_output_image_in_db_should_fail(test_context, stored_file_factory):
+    reference_image_file = stored_file_factory(id=2)
+    test_context.mock_file_storage.find_by_id.side_effect = (
+        lambda x: reference_image_file if x == 2 else None
+    )
+    compare_image_settings = ComparisonSettings.default()
+    compare_image_params = CompareImageParams(
+        1, reference_image_file.id, compare_image_settings
+    )
+
+    with pytest.raises(RuntimeError):
+        test_context.compare_image_task._execute(compare_image_params)
+
+
+def test_no_reference_image_in_db_should_fail(test_context, stored_file_factory):
+    output_image_file = stored_file_factory(id=1)
+    test_context.mock_file_storage.find_by_id.side_effect = (
+        lambda x: output_image_file if x == 1 else None
+    )
+    compare_image_settings = ComparisonSettings.default()
+    compare_image_params = CompareImageParams(
+        output_image_file.id, 2, compare_image_settings
+    )
+
+    with pytest.raises(RuntimeError):
+        test_context.compare_image_task._execute(compare_image_params)
