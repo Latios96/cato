0c4811c266bd1e78ea7c396955bd8a0455e7b6f3
Jan Honsbrok <jan.honsbrok@gmail.com>
2022-08-10 11:19:43 +0200

    feat: add a mock for celery

---
+++ tests/__fixtures__/celery_mock.py
@@ -0,0 +1,63 @@
+import datetime
+import traceback
+import uuid
+
+import mock.mock
+
+
+class MockAsyncResult:
+    def __init__(self, task_id, app, status, result, traceback_str):
+        self._task_id = task_id
+        self._app = app
+        self._status = status
+        self._result = result
+        self._traceback_str = traceback_str
+
+    @property
+    def task_id(self):
+        return self._task_id
+
+    @property
+    def result(self):
+        return self._result
+
+    @property
+    def state(self):
+        return self._status
+
+    @property
+    def traceback(self):
+        return self._traceback_str
+
+
+class MockCeleryApp:
+    def __init__(self):
+        self.results = {}
+        self.backend = mock.MagicMock()
+        self.backend.get_task_meta.side_effect = self.results.get
+        self.backend.meta_from_decoded.side_effect = lambda x: x
+
+    def task(self, *args, **opts):
+        callable = args[0]
+        # todo assert this is a callable
+        app = self
+
+        class WrapCls:
+            def delay(self, *args, **opts) -> MockAsyncResult:
+                task_id = str(uuid.uuid4())
+                try:
+                    result = callable(*args, **opts)
+                except Exception as e:
+                    return MockAsyncResult(
+                        task_id, app, "FAILURE", None, traceback.format_exc()
+                    )
+                app.results[task_id] = {
+                    "task_id": id,
+                    "status": "SUCCESS",
+                    "result": result,
+                    "traceback": None,
+                    "date_done": datetime.datetime.utcnow(),
+                }
+                return MockAsyncResult(task_id, app, "SUCCESS", result, None)
+
+        return WrapCls()
---
+++ tests/__fixtures__/test_celery_mock.py
@@ -0,0 +1,102 @@
+import json
+
+from celery.result import AsyncResult
+
+from cato_common.domain.tasks.task_result import TaskResult, TaskResultState
+from cato_server.task_queue.task_result_factory import TaskResultFactory
+from tests.__fixtures__.celery_mock import MockCeleryApp
+
+
+def test_submit_successful_task():
+    app = MockCeleryApp()
+
+    @app.task
+    def my_hello(name):
+        return f"Hello {name}"
+
+    async_result = my_hello.delay("Horst")
+
+    assert async_result.result == "Hello Horst"
+
+
+def test_submit_failing_task():
+    app = MockCeleryApp()
+
+    @app.task
+    def my_failure():
+        raise RuntimeError("Error")
+
+    async_result = my_failure.delay("Horst")
+
+    assert async_result.result is None
+    assert async_result.traceback is not None
+
+
+def test_successful_mocked_async_result_should_work_with_task_result_factory(
+    app_and_config_fixture, object_mapper
+):
+    app, config = app_and_config_fixture
+    task_result_factory = TaskResultFactory(config, object_mapper)
+    app = MockCeleryApp()
+
+    @app.task
+    def my_hello(name):
+        return json.dumps({"Hello": name})
+
+    async_result = my_hello.delay("Horst")
+    task_result = task_result_factory.from_async_result(async_result)
+
+    assert task_result == TaskResult(
+        task_id=async_result.task_id,
+        state=TaskResultState.SUCCESS,
+        url=f"http://127.0.0.1:{config.port}/api/v1/result/{async_result.task_id}",
+        result_={"Hello": "Horst"},
+        error_message_=None,
+    )
+
+
+def test_failed_mocked_async_result_should_work_with_task_result_factory(
+    app_and_config_fixture, object_mapper
+):
+    app, config = app_and_config_fixture
+    task_result_factory = TaskResultFactory(config, object_mapper)
+    app = MockCeleryApp()
+
+    @app.task
+    def my_failure():
+        raise RuntimeError("Error")
+
+    async_result = my_failure.delay()
+    task_result = task_result_factory.from_async_result(async_result)
+
+    assert task_result == TaskResult(
+        task_id=async_result.task_id,
+        state=TaskResultState.FAILURE,
+        url=f"http://127.0.0.1:{config.port}/api/v1/result/{async_result.task_id}",
+        result_=None,
+        error_message_="RuntimeError: Error",
+    )
+
+
+def test_successful_celeryasync_result_should_work_with_task_result_factory(
+    app_and_config_fixture, object_mapper
+):
+    app, config = app_and_config_fixture
+    task_result_factory = TaskResultFactory(config, object_mapper)
+    app = MockCeleryApp()
+
+    @app.task
+    def my_hello(name):
+        return json.dumps({"Hello": name})
+
+    mocked_async_result = my_hello.delay("Horst")
+    celery_async_result = AsyncResult(mocked_async_result.task_id, app=app)
+    task_result = task_result_factory.from_async_result(celery_async_result)
+
+    assert task_result == TaskResult(
+        task_id=mocked_async_result.task_id,
+        state=TaskResultState.SUCCESS,
+        url=f"http://127.0.0.1:{config.port}/api/v1/result/{mocked_async_result.task_id}",
+        result_={"Hello": "Horst"},
+        error_message_=None,
+    )
