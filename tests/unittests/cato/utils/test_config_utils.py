53f8c2d03a9029a65979be70334c693c68c5e31a
Jan Honsbrok <jan.honsbrok@gmail.com>
2022-09-11 15:44:28 +0200

    refactor: organize tests in class

--- tests/unittests/cato/utils/test_config_utils.py
+++ tests/unittests/cato/utils/test_config_utils.py
@@ -3,19 +3,18 @@ import os
 from cato.utils.config_utils import resolve_config_path
 
 
-def test_config_path_should_use_cwd():
-    path = resolve_config_path(None)
+class TestResolveConfigPath:
+    def test_config_path_should_use_cwd(self):
+        path = resolve_config_path(None)
 
-    assert path == os.path.join(os.getcwd(), "cato.json")
+        assert path == os.path.join(os.getcwd(), "cato.json")
 
+    def test_config_path_should_use_provided_file(self):
+        path = resolve_config_path(os.path.join("my_folder", "catoo.json"))
 
-def test_config_path_should_use_provided_file():
-    path = resolve_config_path(os.path.join("my_folder", "catoo.json"))
+        assert path == os.path.abspath(os.path.join("my_folder", "catoo.json"))
 
-    assert path == os.path.abspath(os.path.join("my_folder", "catoo.json"))
+    def test_config_path_should_use_provided_folder(self, tmp_path):
+        path = resolve_config_path(str(tmp_path))
 
-
-def test_config_path_should_use_provided_folder(tmp_path):
-    path = resolve_config_path(str(tmp_path))
-
-    assert path == os.path.abspath(os.path.join(str(tmp_path), "cato.json"))
+        assert path == os.path.abspath(os.path.join(str(tmp_path), "cato.json"))
