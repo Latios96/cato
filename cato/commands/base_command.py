34fbda5f058b8b2bdad06245a8c91bf8c6699638
Jan Honsbrok <jan.honsbrok@gmail.com>
2022-09-12 17:28:30 +0200

    feat: add cli_variables parameter to BaseCommand._read_config

--- cato/commands/base_command.py
+++ cato/commands/base_command.py
@@ -1,4 +1,5 @@
 import os
+from typing import Dict
 
 from cato.utils.config_utils import resolve_config_path
 from cato_common.config.config_file_parser import JsonConfigParser
@@ -12,10 +13,16 @@ class BaseCliCommand(object):
     def _config_path(self, path: str) -> str:
         return resolve_config_path(path)
 
-    def _read_config(self, config_path: str) -> RunConfig:
+    def _read_config(
+        self, config_path: str, cli_variables: Dict[str, str] = None
+    ) -> RunConfig:
         config_path = resolve_config_path(config_path)
         config = self._json_config_parser.parse(config_path)
         # todo add option to pass resources folder as argument
+
+        if cli_variables:
+            config.variables.update(cli_variables)
+
         return RunConfig(
             project_name=config.project_name,
             resource_path=os.path.dirname(config_path),
--- tests/unittests/cato/commands/test_base_command.py
+++ tests/unittests/cato/commands/test_base_command.py
@@ -7,17 +7,18 @@ from cato_common.domain.comparison_settings import ComparisonSettings
 from cato_common.domain.config import RunConfig
 from cato_common.domain.test import Test
 from cato_common.domain.test_suite import TestSuite
+from tests.utils import mock_safe
 
 
 def test_read_config_should_populate_run_config_correctly(test_resource_provider):
-    config_directoy = test_resource_provider.resource_by_name("cato_test_config")
+    config_directory = test_resource_provider.resource_by_name("cato_test_config")
     base_command = BaseCliCommand(JsonConfigParser())
 
-    run_config = base_command._read_config(config_directoy)
+    run_config = base_command._read_config(config_directory)
 
     assert run_config == RunConfig(
         project_name="Example",
-        resource_path=config_directoy,
+        resource_path=config_directory,
         suites=[
             TestSuite(
                 name="My_first_test_Suite",
@@ -36,4 +37,66 @@ def test_read_config_should_populate_run_config_correctly(test_resource_provider
         ],
         output_folder=os.getcwd(),
         variables={},
+    )
+
+
+def test_should_add_cli_vars_to_project(test_resource_provider, config_fixture):
+    config_directory = test_resource_provider.resource_by_name("cato_test_config")
+    cli_variables = {"my_custom_var": "my_custom_value"}
+    base_command = BaseCliCommand(JsonConfigParser())
+
+    run_config = base_command._read_config(
+        config_directory,
+        cli_variables=cli_variables,
+    )
+
+    assert run_config == RunConfig(
+        project_name="Example",
+        resource_path=config_directory,
+        suites=[
+            TestSuite(
+                name="My_first_test_Suite",
+                tests=[
+                    Test(
+                        name="My_first_test",
+                        command="python --version",
+                        variables={},
+                        comparison_settings=ComparisonSettings(
+                            method=ComparisonMethod.SSIM, threshold=0.8
+                        ),
+                    )
+                ],
+                variables={},
+            )
+        ],
+        output_folder=os.getcwd(),
+        variables={"my_custom_var": "my_custom_value"},
+    )
+
+
+def test_should_override_project_vars_with_cli_vars(
+    test_resource_provider, config_fixture
+):
+    config_directory = test_resource_provider.resource_by_name("cato_test_config")
+    mocked_parser = mock_safe(JsonConfigParser)
+    config = config_fixture.CONFIG
+    config.variables["my_custom_var"] = "value_before"
+    mocked_parser.parse.return_value = config
+    cli_variables = {"my_custom_var": "my_custom_value"}
+    base_command = BaseCliCommand(mocked_parser)
+
+    run_config = base_command._read_config(
+        config_directory,
+        cli_variables=cli_variables,
+    )
+
+    assert run_config == RunConfig(
+        project_name="EXAMPLE_PROJECT",
+        resource_path=config_directory,
+        suites=[config_fixture.TEST_SUITE],
+        output_folder=os.getcwd(),
+        variables={
+            "my_var": "from_config",
+            "my_custom_var": "my_custom_value",
+        },
     )
