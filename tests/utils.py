c55b85e374cef8cd794e5f4eb2f30a8707d3baed
Jan Honsbrok <jan.honsbrok@gmail.com>
2022-09-17 13:27:53 +0200

    feat: make or_default support callable providers

--- tests/utils.py
+++ tests/utils.py
@@ -8,9 +8,13 @@ def mock_safe(cls: Type[T]) -> T:
     return mock.MagicMock()
 
 
-def or_default(value, default_value):
+def or_default(value: T, default_value: Union[T, Callable[[], T]]):
     if value is "FORCE_NONE":
         return None
-    if value is None:
-        return default_value
-    return value
+
+    if value is not None:
+        return value
+
+    if callable(default_value):
+        return default_value()
+    return default_value
