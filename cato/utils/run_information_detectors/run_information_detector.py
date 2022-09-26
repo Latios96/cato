9cd24db464757cb465cf579c414f63ae61f57236
Jan Honsbrok <jan.honsbrok@gmail.com>
2022-09-26 17:51:57 +0200

    feat: detect Github Actions run information in client

--- cato/utils/run_information_detectors/run_information_detector.py
+++ cato/utils/run_information_detectors/run_information_detector.py
@@ -1,3 +1,6 @@
+from cato.utils.run_information_detectors.github_actions_run_information_detector import (
+    GithubActionsRunInformationDetector,
+)
 from cato.utils.run_information_detectors.local_run_information_detector import (
     LocalComputerRunInformationDetector,
 )
@@ -8,10 +11,16 @@ class RunInformationDetector:
     def __init__(
         self,
         local_computer_run_information_detector: LocalComputerRunInformationDetector,
+        github_actions_run_information_detector: GithubActionsRunInformationDetector,
     ):
         self._local_computer_run_information_detector = (
             local_computer_run_information_detector
         )
+        self._github_actions_run_information_detector = (
+            github_actions_run_information_detector
+        )
 
     def detect(self) -> BasicRunInformationForRunCreation:
+        if self._github_actions_run_information_detector.can_detect():
+            return self._github_actions_run_information_detector.detect()
         return self._local_computer_run_information_detector.detect()
--- tests/unittests/cato/utils/run_information_detectors/test_run_information_detector.py
+++ tests/unittests/cato/utils/run_information_detectors/test_run_information_detector.py
@@ -1,3 +1,6 @@
+from cato.utils.run_information_detectors.github_actions_run_information_detector import (
+    GithubActionsRunInformationDetector,
+)
 from cato.utils.run_information_detectors.run_information_detector import (
     RunInformationDetector,
 )
@@ -5,7 +8,10 @@ from cato.utils.run_information_detectors.local_run_information_detector import 
     LocalComputerRunInformationDetector,
 )
 from cato_common.domain.run_information import OS
-from cato_common.dtos.create_full_run_dto import BasicRunInformationForRunCreation
+from cato_common.dtos.create_full_run_dto import (
+    BasicRunInformationForRunCreation,
+    GithubActionsRunInformationForRunCreation,
+)
 from tests.utils import mock_safe
 
 
@@ -13,11 +19,16 @@ def test_should_detect_local_run_information():
     mock_local_computer_run_information_detector = mock_safe(
         LocalComputerRunInformationDetector
     )
+    mock_github_actions_run_information_detector = mock_safe(
+        GithubActionsRunInformationDetector
+    )
+    mock_github_actions_run_information_detector.can_detect.return_value = False
     mock_local_computer_run_information_detector.detect.return_value = (
         BasicRunInformationForRunCreation(os=OS.WINDOWS, computer_name="cray")
     )
     run_information_detector = RunInformationDetector(
-        mock_local_computer_run_information_detector
+        mock_local_computer_run_information_detector,
+        mock_github_actions_run_information_detector,
     )
 
     run_information = run_information_detector.detect()
@@ -25,3 +36,34 @@ def test_should_detect_local_run_information():
     assert run_information == BasicRunInformationForRunCreation(
         os=OS.WINDOWS, computer_name="cray"
     )
+
+
+def test_should_detect_github_actions_run_information():
+    mock_local_computer_run_information_detector = mock_safe(
+        LocalComputerRunInformationDetector
+    )
+    mock_github_actions_run_information_detector = mock_safe(
+        GithubActionsRunInformationDetector
+    )
+    mock_github_actions_run_information_detector.can_detect.return_value = True
+    run_information = GithubActionsRunInformationForRunCreation(
+        os=OS.WINDOWS,
+        computer_name="cray",
+        github_run_id=3052454707,
+        html_url="https://github.com/owner/repo-name/actions/runs/3052454707/jobs/4921861789",
+        job_name="build_ubuntu",
+        actor="owner",
+        attempt=1,
+        run_number=2,
+        github_url="https://github.com",
+        github_api_url="https://api.github.com",
+    )
+    mock_github_actions_run_information_detector.detect.return_value = run_information
+    run_information_detector = RunInformationDetector(
+        mock_local_computer_run_information_detector,
+        mock_github_actions_run_information_detector,
+    )
+
+    detected_run_information = run_information_detector.detect()
+
+    assert detected_run_information == run_information
