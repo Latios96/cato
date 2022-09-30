a3d88f7f7e8d94ba18629480a871516690d9c62d
Jan Honsbrok <jan.honsbrok@gmail.com>
2022-09-30 16:55:15 +0200

    feat: add own class to calculate run aggregration

---
+++ cato_server/usecases/aggregate_run.py
@@ -0,0 +1,48 @@
+from cato_common.domain.run import Run
+from cato_common.dtos.run_dto import RunDto
+from cato_common.storage.page import Page
+from cato_server.run_status_calculator import RunStatusCalculator
+from cato_server.storage.abstract.test_result_repository import TestResultRepository
+
+
+class AggregateRun:
+    def __init__(
+        self,
+        test_result_repository: TestResultRepository,
+        run_status_calculator: RunStatusCalculator,
+    ):
+        self._test_result_repository = test_result_repository
+        self._run_status_calculator = run_status_calculator
+
+    def aggregate_runs_by_project_id(
+        self, project_id: int, run_page: Page[Run]
+    ) -> Page[RunDto]:
+        status_by_run_id = self._test_result_repository.find_status_by_project_id(
+            project_id
+        )
+        duration_by_run_id = self._test_result_repository.duration_by_run_ids(
+            {run.id for run in run_page.entities}
+        )
+
+        dtos = []
+        for run in run_page.entities:
+            status = self._run_status_calculator.calculate(
+                status_by_run_id.get(run.id, set())
+            )
+            dtos.append(
+                RunDto(
+                    id=run.id,
+                    project_id=run.id,
+                    started_at=run.started_at,
+                    status=status,
+                    duration=duration_by_run_id[run.id],
+                    branch_name=run.branch_name,
+                )
+            )
+
+        return Page(
+            page_number=run_page.page_number,
+            page_size=run_page.page_size,
+            total_entity_count=run_page.total_entity_count,
+            entities=dtos,
+        )
---
+++ tests/unittests/cato_server/usecases/test_aggregate_run.py
@@ -0,0 +1,122 @@
+import datetime
+
+import pytest
+
+from cato_common.domain.branch_name import BranchName
+from cato_common.domain.run import Run
+from cato_common.domain.unified_test_status import UnifiedTestStatus
+from cato_common.dtos.run_dto import RunDto
+from cato_common.storage.page import Page
+from cato_server.domain.run_status import RunStatus
+from cato_server.run_status_calculator import RunStatusCalculator
+from cato_server.storage.abstract.test_result_repository import TestResultRepository
+from cato_server.usecases.aggregate_run import AggregateRun
+from tests.utils import mock_safe
+
+
+@pytest.fixture
+def test_context():
+    class TestContext:
+        def __init__(self):
+            self.mock_test_result_repository = mock_safe(TestResultRepository)
+            self.mock_run_status_calculator = mock_safe(RunStatusCalculator)
+            self.aggregate_run = AggregateRun(
+                self.mock_test_result_repository, self.mock_run_status_calculator
+            )
+
+    return TestContext()
+
+
+def test_aggregate_empty_page(test_context):
+    page = Page(
+        page_number=1,
+        page_size=10,
+        total_entity_count=0,
+        entities=[],
+    )
+
+    aggregated_page = test_context.aggregate_run.aggregate_runs_by_project_id(1, page)
+
+    assert aggregated_page == Page(
+        page_number=1,
+        page_size=10,
+        total_entity_count=0,
+        entities=[],
+    )
+
+
+def test_aggregate_page(test_context, local_computer_run_information):
+    test_context.mock_test_result_repository.find_status_by_project_id.return_value = {
+        1: {UnifiedTestStatus.SUCCESS},
+        2: {UnifiedTestStatus.FAILED},
+    }
+    test_context.mock_test_result_repository.duration_by_run_ids.return_value = {
+        1: 1,
+        2: 2,
+    }
+    test_context.mock_run_status_calculator.calculate.side_effect = (
+        lambda x: RunStatus.SUCCESS if x == 1 else RunStatus.FAILED
+    )
+    page = Page(
+        page_number=1,
+        page_size=10,
+        total_entity_count=0,
+        entities=[
+            Run(
+                id=1,
+                project_id=1,
+                run_batch_id=1,
+                started_at=datetime.datetime(year=2022, month=9, day=30),
+                branch_name=BranchName("main"),
+                previous_run_id=None,
+                run_information=local_computer_run_information,
+            ),
+            Run(
+                id=2,
+                project_id=1,
+                run_batch_id=1,
+                started_at=datetime.datetime(year=2022, month=9, day=30),
+                branch_name=BranchName("main"),
+                previous_run_id=None,
+                run_information=local_computer_run_information,
+            ),
+        ],
+    )
+
+    aggregated_page = test_context.aggregate_run.aggregate_runs_by_project_id(1, page)
+
+    assert aggregated_page == Page(
+        page_number=1,
+        page_size=10,
+        total_entity_count=0,
+        entities=[
+            RunDto(
+                id=1,
+                project_id=1,
+                started_at=datetime.datetime(year=2022, month=9, day=30),
+                status=RunStatus.FAILED,
+                duration=1,
+                branch_name=BranchName(name="main"),
+            ),
+            RunDto(
+                id=2,
+                project_id=2,
+                started_at=datetime.datetime(year=2022, month=9, day=30),
+                status=RunStatus.FAILED,
+                duration=2,
+                branch_name=BranchName(name="main"),
+            ),
+        ],
+    )
+    test_context.mock_test_result_repository.find_status_by_project_id.assert_called_with(
+        1
+    )
+    test_context.mock_test_result_repository.duration_by_run_ids.assert_called_with(
+        {1, 2}
+    )
+    test_context.mock_run_status_calculator.calculate.assert_any_call(
+        {UnifiedTestStatus.FAILED}
+    )
+    test_context.mock_run_status_calculator.calculate.assert_any_call(
+        {UnifiedTestStatus.SUCCESS}
+    )
