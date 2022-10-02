a833d4593270b10c61f6e36cc70b673da2f7d14c
Jan Honsbrok <jan.honsbrok@gmail.com>
2022-10-02 10:53:07 +0200

    chore: add RunBatchAggregate to TypeScript types

--- scripts/generate_typescript_models.py
+++ scripts/generate_typescript_models.py
@@ -14,6 +14,7 @@ from cato_common.domain.test_failure_reason import TestFailureReason
 from cato_common.domain.test_identifier import TestIdentifier
 from cato_common.dtos.create_full_run_dto import CreateFullRunDto
 from cato_common.dtos.run_aggregate import RunAggregate
+from cato_common.dtos.run_batch_aggregate import RunBatchAggregate
 from cato_common.dtos.suite_result_dto import SuiteResultDto
 from cato_common.dtos.test_result_dto import TestResultDto
 from cato_common.dtos.test_result_short_summary_dto import TestResultShortSummaryDto
@@ -36,6 +37,7 @@ if __name__ == "__main__":
             AuthUser,
             BasicRunInformation,
             RunAggregate,
+            RunBatchAggregate,
         ]
     ).with_type_overrides(
         {
