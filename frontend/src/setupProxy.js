f938b46133821c10b91e38b0263d0a9154b868d5
Jan Honsbrok <jan.honsbrok@gmail.com>
2023-03-04 16:12:32 +0100

    chore: fix proxy script

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -5,7 +5,7 @@ module.exports = function (app) {
     app.use(
       route,
       createProxyMiddleware({
-        target: "http://localhost:5000",
+        target: "http://127.0.0.1:5000/",
         changeOrigin: true,
       })
     );
