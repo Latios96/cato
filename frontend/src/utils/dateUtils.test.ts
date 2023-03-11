8a08143130ce4d8a9be7f0fd657135e46059726d
Jan Honsbrok <jan.honsbrok@gmail.com>
2023-03-11 17:17:31 +0100

    tests: add tests for toHoursAndMinutes

--- frontend/src/utils/dateUtils.test.ts
+++ frontend/src/utils/dateUtils.test.ts
@@ -1,5 +1,5 @@
 import each from "jest-each";
-import { formatDuration } from "./dateUtils";
+import { formatDuration, toHoursAndMinutes } from "./dateUtils";
 
 describe("format duration", () => {
   each([[undefined, null, "NaN"]]).it(
@@ -14,5 +14,27 @@ describe("format duration", () => {
 
   it('should format 0.1 seconds to "less than one second"', () => {
     expect(formatDuration(0.1)).toBe("less than one second");
+  });
+});
+
+describe("toHoursAndMinutes", () => {
+  it("should format 0 to empty", () => {
+    expect(toHoursAndMinutes(0)).toBe("");
+  });
+
+  it("should format 0.1 to 0.1 seconds", () => {
+    expect(toHoursAndMinutes(0.1)).toBe("0.1s");
+  });
+
+  it("should format 1.1 to 1 seconds", () => {
+    expect(toHoursAndMinutes(1.1)).toBe("1s");
+  });
+
+  it("should format minutes correctly", () => {
+    expect(toHoursAndMinutes(61)).toBe("1min 1s");
+  });
+
+  it("should format hours correctly", () => {
+    expect(toHoursAndMinutes(3661)).toBe("1h 1min 1s");
   });
 });
