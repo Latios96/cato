ce5f28593c3f2b4c6babbcd897693555bc394199
Jan Honsbrok <jan.honsbrok@gmail.com>
2023-05-11 17:51:22 +0200

    fix: missing file

--- tests/unittests/cato_server/usecases/test_create_reference_image_edit.py
+++ tests/unittests/cato_server/usecases/test_create_reference_image_edit.py
@@ -1,9 +1,13 @@
 import datetime
 
+from cato_common.domain.can_be_edited import CanBeEdited
+from cato_common.domain.comparison_settings import ComparisonSettings
 from cato_common.domain.result_status import ResultStatus
 from cato_common.domain.compare_image_result import CompareImageResult
 from cato_common.domain.image import Image
 from cato_common.domain.test_edit import ReferenceImageEdit, ReferenceImageEditValue
+from cato_common.domain.test_identifier import TestIdentifier
+from cato_common.domain.unified_test_status import UnifiedTestStatus
 from cato_server.storage.abstract.image_repository import ImageRepository
 from cato_server.storage.abstract.test_edit_repository import TestEditRepository
 from cato_server.storage.abstract.test_result_repository import TestResultRepository
@@ -21,7 +25,7 @@ def test_can_be_edited_should_return_ok(test_result_factory):
     mock_test_edit_repository = mock_safe(TestEditRepository)
     mock_test_result_repository = mock_safe(TestResultRepository)
     mock_test_result_repository.find_by_id.return_value = test_result_factory(
-        image_output=1
+        image_output=1, comparison_settings=ComparisonSettings.default()
     )
     mock_compare_image = mock_safe(CompareImage)
     mock_image_repository = mock_safe(ImageRepository)
@@ -35,7 +39,7 @@ def test_can_be_edited_should_return_ok(test_result_factory):
 
     result = create_reference_image_edit.can_be_edited(1)
 
-    assert result == (True, None)
+    assert result == CanBeEdited.yes()
 
 
 def test_can_be_edited_should_return_not_ok_no_test_result_found():
@@ -53,7 +57,7 @@ def test_can_be_edited_should_return_not_ok_no_test_result_found():
 
     result = create_reference_image_edit.can_be_edited(1)
 
-    assert result == (False, "Could not find a test result with id 1")
+    assert result == CanBeEdited(False, "Could not find a test result with id 1")
 
 
 def test_can_be_edited_should_return_not_ok_no_image_output(test_result_factory):
@@ -73,7 +77,9 @@ def test_can_be_edited_should_return_not_ok_no_image_output(test_result_factory)
 
     result = create_reference_image_edit.can_be_edited(1)
 
-    assert result == (False, "Can't edit a test result which has no image_output!")
+    assert result == CanBeEdited(
+        False, "Can't edit a test result which has no image_output!"
+    )
 
 
 def test_can_be_edited_should_return_not_ok_no_comparison_settings(
@@ -95,9 +101,8 @@ def test_can_be_edited_should_return_not_ok_no_comparison_settings(
 
     result = create_reference_image_edit.can_be_edited(1)
 
-    assert result == (
-        False,
-        "Can't edit a test result which has no comparison settings!",
+    assert result == CanBeEdited(
+        False, "Can't edit a test result which has no comparison settings!"
     )
 
 
@@ -109,11 +114,12 @@ def test_create_reference_image_edit_with_success(test_result_factory):
     mock_test_result_repository.find_by_id.return_value = test_result_factory(
         id=5,
         image_output=original_output_image_id,
-        status=ResultStatus.SUCCESS,
+        unified_test_status=UnifiedTestStatus.SUCCESS,
         message="success",
         diff_image=3,
         reference_image=original_reference_image_id,
         error_value=1,
+        comparison_settings=ComparisonSettings.default(),
     )
     mock_compare_image = mock_safe(CompareImage)
     mock_compare_image.compare_image_from_db.return_value = CompareImageResult(
@@ -135,11 +141,12 @@ def test_create_reference_image_edit_with_success(test_result_factory):
     created_at = datetime.datetime(year=2021, month=original_reference_image_id, day=17)
     create_reference_image_edit._get_created_at = lambda: created_at
 
-    reference_image_edit = create_reference_image_edit.create_edit(1)
+    reference_image_edit = create_reference_image_edit.create_edit(5)
 
     assert reference_image_edit == ReferenceImageEdit(
         id=0,
         test_id=5,
+        test_identifier=TestIdentifier(suite_name="my_suite", test_name="my_test_name"),
         created_at=created_at,
         old_value=ReferenceImageEditValue(
             status=ResultStatus.SUCCESS,
