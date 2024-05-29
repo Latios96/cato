58603acab9e4c714310b1052701429b961d9982c
Jan Honsbrok <jan.honsbrok@gmail.com>
2024-05-29 10:43:00 +0200

    fix: don't just use information from first run

--- frontend/src/pages/ProjectPage/internal/RunBatchList/RunBatchListEntry/RunBatchListEntry.tsx
+++ frontend/src/pages/ProjectPage/internal/RunBatchList/RunBatchListEntry/RunBatchListEntry.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { RunBatchAggregate } from "../../../../../catoapimodels/catoapimodels";
-import RunBatchListRow from "./RunBatchListRow";
+import RunBatchListRow, { RunLike } from "./RunBatchListRow";
 import { Link } from "react-router-dom";
 import { useToggle } from "rooks";
 
@@ -12,6 +12,26 @@ interface Props {
 function RunBatchListEntry(props: Props) {
   const [isExpanded, toggleExpanded] = useToggle(false);
   const hasSingleRun = props.runBatch.runs.length === 1;
+
+  let runLike: RunLike = props.runBatch;
+  if (hasSingleRun) {
+    runLike = {
+      runInformation: props.runBatch.runs[0].runInformation,
+      runBatchIdentifier: props.runBatch.runBatchIdentifier,
+      createdAt: props.runBatch.runs[0].createdAt,
+      status: props.runBatch.runs[0].status,
+      branchName: props.runBatch.runs[0].branchName,
+      duration: props.runBatch.runs[0].duration,
+      id: props.runBatch.runs[0].id,
+      progress: props.runBatch.progress,
+    };
+  } else {
+    runLike.duration = props.runBatch.runs.reduce(
+      (duration, aggregate) => duration + aggregate.duration,
+      0
+    );
+  }
+
   return (
     <>
       <RunBatchListRow
@@ -32,7 +52,7 @@ function RunBatchListEntry(props: Props) {
             <>{props.runBatch.runBatchIdentifier.runName}</>
           )
         }
-        runLike={{ ...props.runBatch, ...props.runBatch.runs[0] }}
+        runLike={runLike}
       />
       {props.runBatch.runs.length > 1 && isExpanded
         ? props.runBatch.runs.map((run) => {
